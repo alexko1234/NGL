d56445f6ca77c27495de09b4fd107f53e6927be4
Guillaume Albini <galbini@genoscope.cns.fr>
2013-01-03 10:55:54 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@398 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-asset/project/Build.scala
+++ app-ngl-asset/project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "resourcesServer"
+    val appName         = "ngl-asset"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
