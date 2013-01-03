e566ab2d8c9058bcbbbb660d5ab9be80b047c2e9
Guillaume Albini <galbini@genoscope.cns.fr>
2013-01-03 10:56:46 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@402 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/project/Build.scala
+++ lib-ngl-common/project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "NGL-Common"
+    val appName         = "ngl-common"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
@@ -12,12 +12,11 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      	organization := "fr.cea.ig",
-	credentials += Credentials(new File(sys.env.getOrElse("NEXUS_CREDENTIAL","") + "/nexus.credentials")),
+      	organization := "fr.cea.ig.ngl",
+	credentials += Credentials(new File(sys.env.getOrElse("NEXUS_CREDENTIALS","") + "/nexus.credentials")),
       	publishMavenStyle := true,
       	resolvers += "Nexus repository" at "https://gsphere.genoscope.cns.fr/nexus/content/groups/public/",
-	publishTo <<= version { (v: String) =>
-  				val nexus = "https://oss.sonatype.org/"
+	publishTo <<= version { (v: String) =>  				
 				if (v.trim.endsWith("SNAPSHOT")) 
 				    Some("snapshots" at "https://gsphere.genoscope.cns.fr/nexus/content/repositories/snapshots") 
 				else
