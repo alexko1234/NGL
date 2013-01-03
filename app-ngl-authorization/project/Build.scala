d7da6813b844dba125719855c0261501e2686a0a
Guillaume Albini <galbini@genoscope.cns.fr>
2013-01-03 10:56:05 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@399 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-authorization/project/Build.scala
+++ app-ngl-authorization/project/Build.scala
@@ -4,14 +4,14 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "authorisationAdministration"
+    val appName         = "ngl-authorization"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "fr.cea.ig" %% "playtbforms" % "1.0-SNAPSHOT",
-		"fr.cea.ig" %% "casplugin" % "1.0-SNAPSHOT",
-		 "fr.cea.ig" %% "ngl-common" % "1.0-SNAPSHOT",
-		 "mysql" % "mysql-connector-java" % "5.1.18"
+        "fr.cea.ig" %% "bootstrap" % "1.0-SNAPSHOT",
+	"fr.cea.ig" %% "casplugin" % "1.0-SNAPSHOT",
+	"fr.cea.ig.ngl" %% "ngl-common" % "1.0-SNAPSHOT",
+	"mysql" % "mysql-connector-java" % "5.1.18"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
