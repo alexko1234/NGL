6e43b4fe29ccd652a7790bf005ca508017a4d6c8
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-01-07 11:15:02 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@417 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-bi/test/utils/AbstractTests.java
+++ app-ngl-bi/test/utils/AbstractTests.java
@@ -2,6 +2,9 @@ package utils;
 
 import static play.test.Helpers.fakeApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Before;
 
@@ -12,13 +15,23 @@ public abstract class AbstractTests {
 	FakeApplication app;
 	@Before
 	public void start(){
-		 app = fakeApplication();
+		 app = fakeApplication(fakeConfiguration());
 		 Helpers.start(app);
 		 init();
 	}
+	
 	@After
 	public void stop(){
 		Helpers.stop(app);
+	}
+	
+	public Map<String,String> fakeConfiguration(){
+		Map<String,String> config = new HashMap<String,String>();
+		config.put("mongodb.database", "NGL-BI");
+		config.put("mongodb.credentials", "ngl-bi:NglBiPassW");
+		config.put("mongodb.servers", "gsphere.genoscope.cns.fr:27017");
+		return config;
+		
 	}
 	
 	public abstract void init();
