6e216a6ba4ee030339ab4e93ead52015d1cf3ac5
Guillaume Albini <galbini@genoscope.cns.fr>
2013-01-24 10:46:11 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@484 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ lib-ngl-common/test/utils/AbstractTests.java
@@ -0,0 +1,52 @@
+package utils;
+
+
+import static play.test.Helpers.fakeApplication;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+
+import play.test.FakeApplication;
+import play.test.Helpers;
+
+public abstract class AbstractTests {
+	FakeApplication app;
+	@Before
+	public void start(){
+		 app = getFakeApplication();
+		 Helpers.start(app);
+	}
+	
+	@After
+	public void stop(){
+		Helpers.stop(app);
+	}
+	
+	public FakeApplication getFakeApplication(){
+		return fakeApplication(fakeConfiguration());
+	}
+	
+	
+	public Map<String,String> fakeConfiguration(){
+		Map<String,String> config = new HashMap<String,String>();
+		
+		config.put("evolutionplugin", "disabled");
+		config.put("db.default.driver", "com.mysql.jdbc.Driver");
+		config.put("db.default.url", "jdbc:mysql://mysql.genoscope.cns.fr:3307/NGL");
+		config.put("db.default.user", "ngl");
+		config.put("db.default.password", "ngl@dmin");
+		config.put("db.default.partitionCount", "1");
+		config.put("db.default.maxConnectionsPerPartition", "6");
+		config.put("db.default.minConnectionsPerPartition", "1");
+		config.put("db.default.logStatements", "true");
+		config.put("db.default.jndiName", "ngl");	
+		return config;
+		
+	}
+	
+	
+}
