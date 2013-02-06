39214216b9d86dc3d3785627abb10d8948acf738
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-02-06 14:36:15 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@538 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-sq/public/javascripts/ngl/containers/app.js
@@ -0,0 +1,13 @@
+"use strict";
+ 
+var containerApp = angular.module('containerApp', ['nglServices','basketsServices'])
+.config(['$routeProvider', function($routeProvider) {
+$routeProvider
+.when('/', {
+templateUrl: 'tpl/containers',
+controller: 'MainCtrl'
+})
+.otherwise({
+redirectTo: '/test'
+});
+}]);
---
+++ app-ngl-sq/public/javascripts/ngl/containers/controllers.js
@@ -0,0 +1,82 @@
+"use strict";
+ containerApp.controller('MainCtrl', function($scope, $http, datatable, baskets) {
+ /*$scope.submitPost = function() {
+     $http.post('/api/main', {
+          post_title: "test",
+          post_body: "var=1"
+        }).success(function(data, status, headers, config) {
+          if(data.success){
+            $location.path('/');
+          }else {
+            //do something about the error
+          }
+        });
+ }*/
+	$scope.name = "test";
+	$scope.projects=[{"code":'1',"label":'P1'},{"code":'2',"label":'P2'},{"code":'3',"label":'P3'}];
+	$scope.experiments=[{"code":'1',"label":'E1'},{"code":'2',"label":'E2'},{"code":'3',"label":'E3'}];
+	$scope.states=[{"code":'1',"label":'S1'},{"code":'2',"label":'S2'},{"code":'3',"label":'S3'}];
+	
+	$scope.datatableConfig = {
+			edit: false,
+			orderReverse:false,
+			orderBy:undefined,
+			editColumn: {
+				all:undefined,
+				code:undefined,
+				name:undefined,
+				collectionName:undefined
+			},
+			updateColumn: {
+				code:undefined,
+				name:undefined,
+				collectionName:undefined
+			},
+			hideColumn: {
+				id:undefined,
+				code:undefined,
+				name:undefined,
+				objectType:{type:undefined},
+				collectionName:undefined
+			},
+			orderColumn:{
+				id:undefined,
+				code:undefined,
+				name:undefined,
+				objectType:{type:undefined},
+				collectionName:undefined
+			},
+			url:{
+				//save:"/admin/types?format=json",
+				remove:"",
+				search:'/api/containers'
+			},
+	};
+	
+	$scope.basketsConfig = {
+		idBtnModal:"Add",
+		titleModal:"Add to basket",
+		textModal:"",
+		modalId:"newBasketModal",
+		textCancelModal:"Cancel",
+		manualModal:false,
+		url:"/baskets",
+		urlList:"/basket/experimenttype"
+	};
+	
+	$scope.init = function(){
+		$scope.datatable = datatable($scope, $scope.datatableConfig);
+		$scope.baskets = baskets($scope, $scope.basketsConfig, $scope.datatable);
+	}
+	
+	/*$scope.init = function(){
+		$scope.datatable = datatable($scope.datatableConfig);
+		$scope.datatable.search = function() {
+		$http.get('/api/containers').
+			success(function(data, status, headers, config){
+			$scope.datatable.searchresult = data.containers;
+			$scope.datatable.searchresultMaster = angular.copy(data.containers);
+			});
+		}
+	}*/
+});
