cb3195ced51ea1a83768fa14bc3953e8696d70f3
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-03-11 14:54:06 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@641 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 import com.typesafe.sbteclipse.core.EclipsePlugin.EclipseKeys
 
 object ApplicationBuild extends Build {
@@ -40,20 +40,22 @@ object ApplicationBuild extends Build {
  
    object Dependencies {
    	val nglcommonDependencies = Seq(
+		javaCore, javaJdbc,javaEbean,
 		"fr.cea.ig" %% "mongodbplugin" % "1.0-SNAPSHOT",
-		"play" %% "spring" % "2.0",
+		"play" %% "play-spring-module" % "1.1-SNAPSHOT",
 		"org.springframework" % "spring-jdbc" % "3.0.7.RELEASE",
 		"mysql" % "mysql-connector-java" % "5.1.18",
 		"net.sourceforge.jtds" % "jtds" % "1.2.2",
         "net.sf.opencsv" % "opencsv" % "2.0",
 		"org.springframework" % "spring-test" % "3.0.7.RELEASE"
-    	)	 
+    	)	
    	
    	val nglbiDependencies = Seq(
 	        // Add your project dependencies here,
-	        "commons-collections" % "commons-collections" % "3.2.1",
+	      javaCore, javaJdbc,
+			"commons-collections" % "commons-collections" % "3.2.1",
 	        "org.apache.commons" % "commons-lang3" % "3.1",
-	        "play" %% "spring" % "2.0",
+	       "play" %% "play-spring-module" % "1.1-SNAPSHOT",
 	        "org.springframework" % "spring-jdbc" % "3.0.7.RELEASE",
 	        "mysql" % "mysql-connector-java" % "5.1.18",
 	        "postgresql" % "postgresql" % "8.3-603.jdbc4",
@@ -61,11 +63,12 @@ object ApplicationBuild extends Build {
 	        "fr.cea.ig" %% "bootstrap" % "1.0-SNAPSHOT",
 	        "fr.cea.ig" %% "casplugin" % "1.0-SNAPSHOT",
 	        "fr.cea.ig" %% "mongodbplugin" % "1.0-SNAPSHOT"
-    	)
+			)
     	
     	val nglsqDependencies = Seq(
+			javaCore, javaJdbc,
 	      // Add your project dependencies here,
-	      "play" %% "spring" % "2.0",
+		 "play" %% "play-spring-module" % "1.1-SNAPSHOT",
 	      "net.sourceforge.jtds" % "jtds" % "1.2.4",
 	      "org.springframework" % "spring-jdbc" % "3.0.7.RELEASE",
 	      "mysql" % "mysql-connector-java" % "5.1.18",
@@ -73,25 +76,26 @@ object ApplicationBuild extends Build {
 	      "fr.cea.ig" %% "bootstrap" % "1.0-SNAPSHOT",
 	      "fr.cea.ig" %% "casplugin" % "1.0-SNAPSHOT",
 	      "fr.cea.ig" %% "mongodbplugin" % "1.0-SNAPSHOT"
-    	)
+		 	)
     	
     	val nglauthDependencies = Seq(
-	        "fr.cea.ig" %% "bootstrap" % "1.0-SNAPSHOT",
+		javaCore, javaJdbc,javaEbean,
+	    "fr.cea.ig" %% "bootstrap" % "1.0-SNAPSHOT",
 	        "fr.cea.ig" %% "casplugin" % "1.0-SNAPSHOT",
 	        "mysql" % "mysql-connector-java" % "5.1.18"
-    	)
+		)
    }
    
     
  
-   val nglcommon = PlayProject(appName + "-common", appVersion, nglcommonDependencies, mainLang = JAVA, path = file("lib-ngl-common"),settings = buildSettings).settings(
+   val nglcommon = play.Project(appName + "-common", appVersion, nglcommonDependencies, path = file("lib-ngl-common"),settings = buildSettings).settings(
        // Add your own project settings here      
        resolvers := Seq(nexusig),
        publishTo := Some(nexusigpublish),       
        resourceDirectory in Test <<= baseDirectory / "conftest"
     )
    
-   val nglbi = PlayProject(appName + "-bi", appVersion, nglbiDependencies, mainLang = JAVA, path = file("app-ngl-bi"),settings = buildSettings).settings(
+   val nglbi = play.Project(appName + "-bi", appVersion, nglbiDependencies, path = file("app-ngl-bi"),settings = buildSettings).settings(
        // Add your own project settings here      
        resolvers := Seq(nexusig),
        publishArtifact := false,
@@ -100,7 +104,7 @@ object ApplicationBuild extends Build {
  
     ).dependsOn(nglcommon)
    
-   val nglsq = PlayProject(appName + "-sq", appVersion, nglsqDependencies, mainLang = JAVA, path = file("app-ngl-sq"),settings = buildSettings).settings(
+   val nglsq = play.Project(appName + "-sq", appVersion, nglsqDependencies, path = file("app-ngl-sq"),settings = buildSettings).settings(
           // Add your own project settings here      
           resolvers := Seq(nexusig),
           publishArtifact := false,
@@ -110,16 +114,16 @@ object ApplicationBuild extends Build {
     ).dependsOn(nglcommon)
    
    
-   val nglauth = PlayProject(appName + "-authorization", appVersion, nglauthDependencies, mainLang = JAVA, path = file("app-ngl-authorization"),settings = buildSettings).settings(
+   val nglauth = play.Project(appName + "-authorization", appVersion, nglauthDependencies, path = file("app-ngl-authorization"),settings = buildSettings).settings(
              // Add your own project settings here      
              resolvers := Seq(nexusig),
              publishArtifact := false,
              publishArtifact in makePom := false,
-             publishTo := Some(nexusigpublish) 
+             publishTo := Some(nexusigpublish)
        
    ).dependsOn(nglcommon)
    
-   val nglasset = PlayProject(appName + "-asset", appVersion, mainLang = JAVA, path = file("app-ngl-asset"),settings = buildSettings).settings(
+   val nglasset = play.Project(appName + "-asset", appVersion, path = file("app-ngl-asset"),settings = buildSettings).settings(
                 // Add your own project settings here      
                 resolvers := Seq(nexusig),
                 publishArtifact := false,
@@ -129,7 +133,7 @@ object ApplicationBuild extends Build {
       )
    
    
-   val main = PlayProject(appName, appVersion, settings = buildSettings).settings(
+   val main = play.Project(appName, appVersion, settings = buildSettings).settings(
       // Add your own project settings here      
       resolvers := Seq(nexusig),
       publishArtifact := false,
