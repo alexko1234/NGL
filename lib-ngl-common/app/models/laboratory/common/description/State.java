6bdf1f438efa752ee9ae40388f9aa7f082c367df
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-03-26 15:09:25 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@666 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/common/description/State.java
+++ lib-ngl-common/app/models/laboratory/common/description/State.java
@@ -1,8 +1,14 @@
 package models.laboratory.common.description;
 
 
+import java.util.List;
+
 import models.laboratory.common.description.dao.StateDAO;
+import models.laboratory.experiment.description.dao.ExperimentTypeDAO;
+import models.utils.ListObject;
 import models.utils.Model;
+import models.utils.dao.DAOException;
+import play.api.modules.spring.Spring;
 
 /**
  * Value of the possible state of type
@@ -22,6 +28,11 @@ public class State extends Model<State>{
 	public String level;
 	
 	public static Finder<State> find = new Finder<State>(StateDAO.class.getName()); 
+	
+	public static List<ListObject> findAllForContainerList() throws DAOException{
+		StateDAO stateDAO = Spring.getBeanOfType(StateDAO.class);
+		return stateDAO.findAllForContainerList();
+	}
 	
 	public State() {
 		super(StateDAO.class.getName());
