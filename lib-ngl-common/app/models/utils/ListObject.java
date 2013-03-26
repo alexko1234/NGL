aa0fe8e6bc0f8baf61f85019827ebf82f264ad5c
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-03-26 15:03:51 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@663 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/utils/ListObject.java
+++ lib-ngl-common/app/models/utils/ListObject.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import models.laboratory.project.instance.Project;
+import models.laboratory.sample.instance.Sample;
 
 public class ListObject{
 	public String name;
@@ -16,7 +17,18 @@ public class ListObject{
 	
 	public static List<ListObject> projectToJsonObject(List<Project> projects){
 		List<ListObject> jo = new ArrayList<ListObject>();
+		
 		for(Project p: projects){
+			jo.add(new ListObject(p.code, p.name));
+		}
+		
+		return jo;
+	}
+	
+	public static List<ListObject> sampleToJsonObject(List<Sample> samples){
+		List<ListObject> jo = new ArrayList<ListObject>();
+		
+		for(Sample p: samples){
 			jo.add(new ListObject(p.code, p.name));
 		}
 		
