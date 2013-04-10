acb25fb6975f50c1a5eabe8430b9d185a25a4419
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-04-10 12:44:01 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@714 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/public/javascripts/ngl/processus/app.js
+++ app-ngl-sq/public/javascripts/ngl/processus/app.js
@@ -1,9 +1,13 @@
 "use strict";
  
-angular.module('home', ['datatableServices','basketsServices'], function($routeProvider, $locationProvider) {
+angular.module('home', ['datatableServices','basketsServices','basketsLocalServices'], function($routeProvider, $locationProvider) {
 	$routeProvider.when('/processus/home', {
 		templateUrl : '/tpl/processus/search',
 		controller : 'SearchCtrl'
+	});
+	$routeProvider.when('/processus/list', {
+		templateUrl : '/tpl/processus/list',
+		controller : 'ListCtrl'
 	});
 	$routeProvider.otherwise({redirectTo: '/processus/home'});
 
--- app-ngl-sq/public/javascripts/ngl/processus/controllers.js
+++ app-ngl-sq/public/javascripts/ngl/processus/controllers.js
@@ -1,48 +1,17 @@
 "use strict";
-function SearchCtrl($scope, $http,datatable,baskets) {
+
+function SearchCtrl($scope, $http,$filter,datatable,baskets,basketsLocal) {
 	 
 	$scope.datatableConfig = {
-			edit: false,
-			orderReverse:false,
-			orderBy:undefined,
-			editColumn: {
-				all:undefined,
-				code:undefined,
-				categoryCode:undefined,
-				sampleCodes:undefined,
-				fromExperimentTypeCodes:undefined,
-				stateCode:undefined,
-				support:{name:undefined}
-				
-			},
-			updateColumn: {
-				code:undefined,
-				categoryCode:undefined,
-				sampleCodes:undefined,
-				fromExperimentTypeCodes:undefined,
-				stateCode:undefined,
-				support:{name:undefined}
-			},
-			hideColumn: {
-				code:undefined,
-				categoryCode:undefined,
-				sampleCodes:undefined,
-				fromExperimentTypeCodes:undefined,
-				stateCode:undefined,
-				support:{name:undefined}
-			},
-			orderColumn:{
-				code:undefined,
-				categoryCode:undefined,
-				sampleCodes:undefined,
-				fromExperimentTypeCodes:undefined,
-				stateCode:undefined,
-				support:{name:undefined}
-			},
+		pagination:{
+			mode:'local'
+		},
 			search:{
-				//save:"/admin/types?format=json",
 				url:'/api/containers'
 			},
+			otherButtons:{
+				active:true
+			}
 	};
 	
 	$scope.basketsConfig = {
@@ -56,13 +25,28 @@ function SearchCtrl($scope, $http,datatable,baskets) {
 		urlList:"/basket/experimenttype"
 	};
 	
-	$scope.createProcessus = function(processusType){	
+	$scope.basketsLocalConfig = {
+		transform: function(container){
+			var processus = {};
+			processus.projectCode = container.projectCodes[0];
+			processus.sampleCode = container.sampleCodes[0];
+			processus.containerInputCode = container.code;
+			processus.codeType = container.categoryCode;
+			
+			//var date = new Date();
+			//processus.code =  processus.codeType+"/"+processus.projectCode+"/"+processus.sampleCode+"/"+$filter('date')(date,"yyyyMMddHHMMss");//yyyyMMDDHHMMSS
+			
+			return processus;
+		}
+	};
+	
+	/*$scope.createProcessus = function(processusType){	
 		for(var i = 0; i < $scope.datatable.displayResult.length; i++){
 			if($scope.datatable.displayResult[i] && $scope.datatable.displayResult[i].selected){
 				$http({
 					method: 'POST',
 					url: '/api/processus',
-					data: {"type":processusType},
+					data: {"type":processusType,"container":$scope.datatable.displayResult[i].code},
 					headers: {'Content-Type': 'application/json'}
 				}).success(function(data) {
 					var process = data;
@@ -71,15 +55,6 @@ function SearchCtrl($scope, $http,datatable,baskets) {
 				});
 			}
 		}
-	}
-	
-	$scope.detectSelect = function(){
-		for(var i = 0; i < $scope.datatable.displayResult.length; i++){
-				if($scope.datatable.displayResult[i] && $scope.datatable.displayResult[i].selected){
-					return true;
-				}
-			}
-		return false;
 	}
 	
 	$scope.addContainer = function(code,container){
@@ -91,61 +66,130 @@ function SearchCtrl($scope, $http,datatable,baskets) {
 		}).success(function(data) {
 			//Call post for adding container
 		});
+	}*/
+	
+	$scope.initialiseProcessus = function(processusType){
+		for(var i = 0; i < $scope.datatable.displayResult.length; i++){
+			if($scope.datatable.displayResult[i] && $scope.datatable.displayResult[i].selected){
+				$scope.basketsLocal.add($scope.datatable.displayResult[i]);
+			}
+		}
 	}
 	
 	$scope.init = function(){
-		$scope.datatable = datatable($scope, $scope.datatableConfig);
-		$scope.baskets = baskets($scope, $scope.basketsConfig, $scope.datatable);
+		$scope.basketsLocal.setConfig($scope.basketsLocalConfig);
+		if(angular.isUndefined($scope.getDatatable())){
+			$scope.tabs.push({label:"Processus",href:jsRoutes.controllers.processus.tpl.Processus.home("list").url,remove:false});//$scope.tabs[1]
+			$scope.datatable = datatable($scope, $scope.datatableConfig);
+			$scope.baskets = baskets($scope, $scope.basketsConfig, $scope.datatable);
+			
+			$scope.setDatatable($scope.datatable);	
+		}else{
+			$scope.datatable = $scope.getDatatable();
+		}
 		
-		$http.get('/tpl/projects/list').
-			success(function(data, status, headers, config){
-				$scope.projects = data;
-			});
+		if(angular.isUndefined($scope.getForm())){
+			$scope.form = {};
+			$http.get('/tpl/projects/list').
+				success(function(data, status, headers, config){
+					$scope.projects = data;
+					$scope.form.projects = data;
+				});
+				
+			$http.get('/tpl/experimenttypes/list').
+				success(function(data, status, headers, config){
+					$scope.experiments = data;
+					$scope.form.experiments = data;
+				});
+				
+			$http.get('/tpl/samples/list').
+				success(function(data, status, headers, config){
+					$scope.samples = data;
+					$scope.form.samples = data;
+				});
 			
-		$http.get('/tpl/experimenttypes/list').
-			success(function(data, status, headers, config){
-				$scope.experiments = data;
-			});
+			$http.get('/tpl/processType/list').
+				success(function(data, status, headers, config){
+					$scope.process = data;
+					$scope.form.process = data;
+				});
+				
+				$scope.setForm($scope.form);
+		}else{
+		
+			//loading the data
+			$scope.projects = $scope.form.projects;
+			$scope.experiments = $scope.form.experiments;
+			$scope.samples = $scope.form.samples;
+			$scope.process = $scope.form.process;
 			
-		$http.get('/tpl/samples/list').
-			success(function(data, status, headers, config){
-				$scope.samples = data;
-			});
-		$http.get('/tpl/processType/list').
-			success(function(data, status, headers, config){
-				$scope.process = data;
-			});
+			//selecting the previous values selected
+			$scope.project = $scope.form.projects.selected;
+			$scope.sample = $scope.form.samples.selected;
+			$scope.experiment = $scope.form.experiments.selected;
+			$scope.processus = $scope.form.process.selected;
+		}
 	}
 	
 	$scope.search = function(){
 		if($scope.processus==undefined && ($scope.project!=undefined || $scope.experiment!=undefined || $scope.sample!=undefined)){ 
 			$('#processus').addClass('error');
-			$('#processusHelp').html("Error: this information is required  ");
+			$('#processus').popover({content:"Information needed",trigger:"manual",placement:"bottom"}).popover('show');
+			
 		}else{
+			$('#processus').removeClass('error');
+			$('#processus').popover('destroy');
+			
 			var jsonSearch = {};
-			jsonSearch['containerState'] = 'N';
+			
+			jsonSearch.containerState = 'N';
 			
 			if($scope.project != undefined){
-				jsonSearch['projectCode'] = $scope.project;
+				jsonSearch.projectCode = $scope.project.code;
 			}
 			
 			if($scope.experiment != undefined){
-				jsonSearch['experimentCode'] = $scope.experiment
-			;
+				jsonSearch.experimentCode = $scope.experiment.code;
 			}
 			if($scope.sample != undefined){
-				jsonSearch['containerSample'] = $scope.sample;
+				jsonSearch.containerSample = $scope.sample.code;
 			}
 			
-			if($scope.processus.code != undefined){
-				jsonSearch['containerProcess'] = $scope.processus.code;
+			if($scope.processus != undefined){
+				jsonSearch.containerProcess = $scope.processus.code;
 			}
-
-			//$scope.datatable.search(JSON.parse("{projectCode:$scope.project,experimentCode:$scope.experiment,containerState:'N',containerSample:$scope.sample,containerProcess:$scope.processus.code}"));//State have to be IWP
+			
 			$scope.datatable.search(jsonSearch);
-			$('#processus').removeClass('error');
-			$('#processusHelp').html("");
 		}
+		
+		//Saving the selected data
+		$scope.form.projects.selected = $scope.project;
+		$scope.form.experiments.selected = $scope.experiment;
+		$scope.form.samples.selected = $scope.sample;
+		$scope.form.process.selected = $scope.processus;
+		
 	}
 }
-SearchCtrl.$inject = ['$scope','$http', 'datatable','baskets'];
+SearchCtrl.$inject = ['$scope','$http','$filter', 'datatable','baskets'];
+
+function ListCtrl($scope, $http, $routeParams,datatable,baskets,basketsLocal) {
+	$scope.datatableConfig = {
+		pagination:{
+			mode:'local',
+			numberRecordsPerPage:30
+		},
+		hide:{
+			active:true
+		}
+	};
+	
+	$scope.init = function(){
+		$(".popover").remove();
+		$scope.datatable = datatable($scope, $scope.datatableConfig);
+		$scope.baskets = baskets($scope, $scope.basketsConfig, $scope.datatable);
+		alert(JSON.stringify($scope.basketsLocal.get()));
+		$scope.datatable.setData($scope.basketsLocal.get(),$scope.basketsLocal.get().length);
+	}
+	
+};
+ListCtrl.$inject = ['$scope', '$http', '$routeParams','datatable','baskets'];
