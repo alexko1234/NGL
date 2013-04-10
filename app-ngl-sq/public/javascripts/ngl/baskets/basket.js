6a6fbe9d410a992400b25cef5dc7efd8b5442d07
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-04-10 12:43:49 +0000

    Local basket angular service
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@713 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-sq/public/javascripts/ngl/baskets/basket.js
@@ -0,0 +1,53 @@
+"use strict";
+
+angular.module('basketsLocalServices', []).
+factory('basketsLocal', ['$http', function($http){ //service to manage baskets local
+	var constructor = function($scope, iConfig){
+		var baskets = {
+			config : undefined,
+			configMaster : undefined,
+			basket : [],
+			
+			
+			configDefault:{
+				transform: function(element){
+					return element;
+				}
+			},
+			/**
+			 * function to keep the basket when we switch views
+			 */
+			get: function(){
+				return this.basket;
+			},
+			
+			/**
+			 * function to add an element to the basket
+			 */
+			add: function(element){
+				this.basket.push(this.config.transform(element));
+			},
+			
+			/**
+			 * function to remove one element in the basket
+			 */
+			remove: function(index){
+				this.basket.splice(index, 1);
+			},
+			/**
+			 * function to change the configuration
+			 */
+			setConfig: function(newConfig){
+				var settings = $.extend(true, {}, this.configDefault, newConfig);
+			    this.config = angular.copy(settings);
+			    this.configMaster = angular.copy(settings);    
+			}
+	
+	};
+	    var settings = $.extend(true, {}, baskets.configDefault, iConfig);
+	    baskets.config = angular.copy(settings);
+	    baskets.configMaster = angular.copy(settings);    
+	    return baskets;
+	}
+	return constructor;
+}]);
