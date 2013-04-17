e7b3c9fc52352c27a7a1f3cb78a3efedcd75a333
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-04-17 13:02:43 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@771 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-plaques/app/controllers/manips/tpl/Manips.java
+++ app-ngl-plaques/app/controllers/manips/tpl/Manips.java
@@ -20,7 +20,7 @@ public class Manips extends Controller {
 		return ok(views.html.manips.home.render());
 	}
 	
-	
+	//TODO messages Code,Nom
 	public static Result search() {
 		List<DatatableColumn> columns = new ArrayList<DatatableColumn>();
 		columns.add(DatatableHelpers.getColumn("matmaco", "Code"));
@@ -30,14 +30,22 @@ public class Manips extends Controller {
 		return ok(views.html.manips.search.render(config));
 	}
 	
+	//TODO messages Code,Nom
 	public static Result list(){
 		List<DatatableColumn> columns = new ArrayList<DatatableColumn>();
+		columns.add(DatatableHelpers.getColumn("x", "Position Plaque"));
 		columns.add(DatatableHelpers.getColumn("matmaco", "Code"));
 		columns.add(DatatableHelpers.getColumn("matmanom", "Nom"));
-		DatatableConfig config = new DatatableConfig(columns,true, true);
-		config.button = Boolean.TRUE;
+		DatatableConfig config = new DatatableConfig(columns);
+		config.save = Boolean.TRUE;
+		config.button=true;
 		return ok(views.html.manips.list.render(config));
 	}
+	
+	/*
+	public static Result plaque96(){
+		return ok(views.html.manips.plaque.render());
+	}*/
 	
 	public static Result javascriptRoutes() {
   	    response().setContentType("text/javascript");
@@ -46,6 +54,7 @@ public class Manips extends Controller {
   	        // Routes
   	    		controllers.manips.tpl.routes.javascript.Manips.home() ,
   	    		controllers.manips.api.routes.javascript.Manips.list(),
+  	  //  		controllers.manips.api.routes.javascript.Manips.plaque(),
   	    		controllers.routes.javascript.Lists.projects(),
   	    		controllers.routes.javascript.Lists.etmateriels(),
   	    		controllers.routes.javascript.Lists.etmanips()
