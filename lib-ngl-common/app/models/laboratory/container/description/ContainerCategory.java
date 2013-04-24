8d9219d366efdcd53939698f43a32599e4826e21
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-04-24 13:48:44 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@816 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/container/description/ContainerCategory.java
+++ lib-ngl-common/app/models/laboratory/container/description/ContainerCategory.java
@@ -1,7 +1,13 @@
 package models.laboratory.container.description;
 
+import java.util.List;
+
+import play.api.modules.spring.Spring;
 import models.laboratory.common.description.AbstractCategory;
 import models.laboratory.container.description.dao.ContainerCategoryDAO;
+import models.laboratory.processes.description.dao.ProcessTypeDAO;
+import models.utils.ListObject;
+import models.utils.dao.DAOException;
 
 public class ContainerCategory extends AbstractCategory{
 
@@ -11,4 +17,13 @@ public class ContainerCategory extends AbstractCategory{
 		super(ContainerCategoryDAO.class.getName());
 	}
 
+	
+	/**
+	 * Return a list of ListObject that help populating the <select> input
+	 * @return List<ListObject>
+	 */
+	public static List<ListObject> findAllForList() throws DAOException{
+		ContainerCategoryDAO containerCategoryDAO = Spring.getBeanOfType(ContainerCategoryDAO.class);
+		return containerCategoryDAO.findAllForList();
+	}
 }
