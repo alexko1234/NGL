0ea1ad97418f0dfc9850313b10a0b88bcdf063a1
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-04-24 11:47:31 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@811 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/public/ngl-sq/javascripts/processes/search.js
+++ app-ngl-sq/public/ngl-sq/javascripts/processes/search.js
@@ -28,9 +28,8 @@ function SearchCtrl($scope, datatable, comboLists) {
 		}else{
 			$scope.form.samples.options = [];
 		}	
-		if($scope.form.typeCodes.selected){
-			$scope.search();
-		}
+		
+		$scope.search();
 	}
 	
 	$scope.init = function(){
@@ -46,18 +45,17 @@ function SearchCtrl($scope, datatable, comboLists) {
 	
 	$scope.search = function(){		
 			var jsonSearch = {};			
+
+			if($scope.form.projects.selected){
+				jsonSearch.projectCode = $scope.form.projects.selected.code;
+			}			
+			if($scope.form.samples.selected){
+				jsonSearch.sampleCode = $scope.form.samples.selected.code;
+			}			
 			if($scope.form.typeCodes.selected){
-				if($scope.form.projects.selected){
-					jsonSearch.projectCode = $scope.form.projects.selected.code;
-				}			
-				if($scope.form.samples.selected){
-					jsonSearch.sampleCode = $scope.form.samples.selected.code;
-				}			
-				if($scope.form.typeCodes.selected){
-					jsonSearch.typeCode = $scope.form.typeCodes.selected.code;
-				}			
-				$scope.datatable.search(jsonSearch);
-			}
+				jsonSearch.typeCode = $scope.form.typeCodes.selected.code;
+			}			
+			$scope.datatable.search(jsonSearch);							
 	}
 }
 
