6730ed6ae4e0b4ff6b3fee476bbc24cae581b1e3
Guillaume Albini <galbini@genoscope.cns.fr>
2013-04-25 09:51:06 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@827 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/controllers/CommonController.java
+++ lib-ngl-common/app/controllers/CommonController.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import fr.cea.ig.MongoDBResult.Sort;
 import play.data.Form;
+import play.libs.Json;
 import play.mvc.Controller;
 import views.components.datatable.DatatableHelpers;
 
@@ -17,5 +20,17 @@ public class CommonController extends Controller{
 		}else{
 			return Sort.ASC;
 		}
+	}
+	/**
+	 * Fill a form in json mode
+	 * @param form
+	 * @param clazz
+	 * @return
+	 */
+	protected static <T> Form<T> getFilledForm(Form<T> form, Class<T> clazz) {
+		JsonNode json = request().body().asJson();
+		//T input = Json.fromJson(json, clazz);
+		Form<T> filledForm = form.bind(json); 
+		return filledForm;
 	}
 }
