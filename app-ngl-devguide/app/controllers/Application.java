0e17cdfd2b0264f59bab8e63315aa3c83d0b59be
Guillaume Albini <galbini@genoscope.cns.fr>
2013-05-16 13:04:37 +0000

    new application dev-guide
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@860 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-devguide/app/controllers/Application.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+  
+	 public static Result index() {
+		 return ok(index.render("home"));	       
+	 }
+	 
+	 public static Result namingRules(String submenu) {
+		 return ok(namingRules.render("namingrules"));	       
+	 }
+	 
+	 public static Result technologies() {
+		 return ok(technologies.render("technologies"));	       
+	 }
+}
