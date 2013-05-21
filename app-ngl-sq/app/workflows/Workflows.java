c85778b8871ddd37faabfb40330045e8a1e5dfd9
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-05-21 12:31:04 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@877 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-sq/app/workflows/Workflows.java
@@ -0,0 +1,20 @@
+package workflows;
+
+import models.laboratory.container.instance.Container;
+import fr.cea.ig.MongoDBDAO;
+
+public class Workflows {
+	private static final String CONTAINER_COLL_NAME = "Container";
+
+	/**
+	 * Set a state of a container to A (Available)
+	 * @param containerCode: the code of the container
+	 */
+	public static void setAvailable(String containerCode){
+		Container container = MongoDBDAO.findByCode(CONTAINER_COLL_NAME, Container.class, containerCode);
+		
+		if(container != null && (container.stateCode.equals("IWP") || container.stateCode.equals("N"))){
+			MongoDBDAO.updateSet(CONTAINER_COLL_NAME, container,"stateCode", "A");
+		}
+	}
+}
