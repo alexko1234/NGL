1758fd01a35debc84d78e334d5203f974c161e63
Guillaume Albini <galbini@genoscope.cns.fr>
2013-05-28 09:36:40 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@905 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-plaques/app/controllers/Main.java
+++ app-ngl-plaques/app/controllers/Main.java
@@ -10,7 +10,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
-public class Application extends Controller {
+public class Main extends Controller {
   
    public static Result index() {
         return ok(index.render("Your new application is ready.","home"));
