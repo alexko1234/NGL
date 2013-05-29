1408fcc5b2f15bced64bcd44524a058d96564033
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-05-29 14:11:32 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@924 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/controllers/Constants.java
+++ app-ngl-sq/app/controllers/Constants.java
@@ -2,6 +2,7 @@ package controllers;
 
 public interface Constants {
 	
+	public static final String CONTAINER_COLL_NAME = "Container";
 	public static final String PROCESS_COLL_NAME = "Process";
 
 }
