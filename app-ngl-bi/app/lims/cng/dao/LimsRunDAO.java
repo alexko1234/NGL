9a20c5c37843d9c804b8cff8cd50522ee6be7909
Guillaume Albini <galbini@genoscope.cns.fr>
2013-07-01 15:17:11 +0000

    CNGR-10
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1004 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-bi/app/lims/cng/dao/LimsRunDAO.java
+++ app-ngl-bi/app/lims/cng/dao/LimsRunDAO.java
@@ -27,32 +27,40 @@ public class LimsRunDAO {
      * Return the list of active sequencer
      * @return
      */
+    
     public List<Instrument> getInstruments(){
-    	String sql = "SELECT DISTINCT m.pc_name as code, m.run_path as path, 'HISEQ2000' as categoryCode, 1 as active  FROM t_machine m JOIN t_machine_type mt ON m.type_id=mt.id WHERE mt.type='HS' AND m.model='2000' AND (m.status=2 OR m.status=1) ORDER BY m.pc_name";
+    	
+    	
+    	String sql = "SELECT DISTINCT m.pc_name as code, m.run_path as path, mt.name as categoryCode, 1 as active  " +
+    			"FROM t_machine m JOIN t_machine_type mt ON m.type_id=mt.id WHERE mt.type in ('HS','H2') AND (m.status=2 OR m.status=1) " +
+    			"ORDER BY m.pc_name";
     	BeanPropertyRowMapper<Instrument> mapper = new BeanPropertyRowMapper<Instrument>(Instrument.class);
     	return this.jdbcTemplate.query(sql, mapper);
     }
     
     public List<LimsExperiment> getExperiments(Experiment experiment){
+
     	if(null != experiment.date){
-	    	String sql = "SELECT m.pc_name as code, w.start_date as date, 'HISEQ2000' as categoryCode, f.nb_cycles" //, mt.type as seq_type, m.model as seq_model
+	    	String sql = "SELECT m.pc_name as code, min(w.start_date) as date, mt.name as categoryCode, f.nb_cycles" //, mt.type as seq_type, m.model as seq_model
 	    				+" FROM t_flowcell f"
 	    				+" JOIN t_workflow w on w.flowcell_id=f.id"
 	    				+" JOIN t_stage s on w.stage_id=s.id and ( s.workflow='SEQ' and s.name='Read1')"
 	    				+" JOIN t_machine m on w.machine_id=m.id"
-	    				+" JOIN t_machine_type mt on m.type_id=mt.id"
-	    				+" WHERE f.barcode=? and w.start_date between ? and ?";
+	    				+" JOIN t_machine_type mt on ( m.type_id=mt.id AND mt.type IN ('GA','HS','H2','MS') )"
+	    				+" WHERE f.barcode=? and w.start_date between ? and ?"
+	    				+" GROUP BY m.pc_name,  mt.name, f.nb_cycles";
 	    	BeanPropertyRowMapper<LimsExperiment> mapper = new BeanPropertyRowMapper<LimsExperiment>(LimsExperiment.class);
 	    	return this.jdbcTemplate.query(sql, mapper, experiment.containerSupportCode, minus(experiment.date,5), add(experiment.date,5));    	
 	    	
     	}else{
-    		String sql = "SELECT m.pc_name as code, w.start_date as date, 'HISEQ2000' as categoryCode, f.nb_cycles" //, mt.type as seq_type, m.model as seq_model
+    		String sql = "SELECT m.pc_name as code,  min(w.start_date) as date, mt.name as categoryCode, f.nb_cycles" //, mt.type as seq_type, m.model as seq_model
     				+" FROM t_flowcell f"
     				+" JOIN t_workflow w on w.flowcell_id=f.id"
     				+" JOIN t_stage s on w.stage_id=s.id and ( s.workflow='SEQ' and s.name='Read1')"
     				+" JOIN t_machine m on w.machine_id=m.id"
-    				+" JOIN t_machine_type mt on m.type_id=mt.id"
-    				+" WHERE f.barcode=?";
+    				+" JOIN t_machine_type mt on  ( m.type_id=mt.id AND mt.type IN ('GA','HS','H2','MS') )"
+    				+" WHERE f.barcode=?"
+    				+" GROUP BY m.pc_name,  mt.name, f.nb_cycles";
     		BeanPropertyRowMapper<LimsExperiment> mapper = new BeanPropertyRowMapper<LimsExperiment>(LimsExperiment.class);
         	return this.jdbcTemplate.query(sql, mapper, experiment.containerSupportCode);    	
     	}
@@ -91,6 +99,22 @@ ORDER BY l.number
     
     
 	public List<LimsLibrary> geContainerSupport(String barCode){
+		/*
+		 SELECT l.number as lane_number, et.name as exp_name, s.barcode as aliquot_barcode, s.stock_barcode, i.short_name as index_short,
+			i.sequence as index_sequence, i.type as index_type
+			FROM t_flowcell f
+			JOIN t_lane l ON l.flowcell_id=f.id
+			JOIN t_sample_lane sl ON sl.lane_id=l.id
+			JOIN t_sample s ON sl.sample_id=s.id
+			JOIN t_exp_type et on sl.exp_type_id=et.id
+			JOIN t_individual ind on s.individual_id=ind.id
+			JOIN t_index i ON ((sl.index=i.cng_name OR sl.index=i.short_name) AND i.type !=3 )
+			WHERE f.barcode='$fcbarcode'
+			ORDER BY l.number"; 
+
+		 */
+		
+		
     	String sql = "SELECT l.number as laneNumber, et.short_name as experimentTypeCode, s.stock_barcode as sampleBarCode," +
     			" i.short_name as indexName, i.type as indexTypeCode, i.sequence as indexSequence ,ind.name as sampleCode, sl.size as insertLength, fn_getsampleid_projects(s.id) as projectCode"
     			+ " FROM t_flowcell f"
@@ -99,7 +123,7 @@ ORDER BY l.number
     			+ " JOIN t_sample s ON sl.sample_id=s.id"
     			+ " JOIN t_exp_type et on sl.exp_type_id=et.id"
     			+ " JOIN t_individual ind on s.individual_id=ind.id"
-    			+ " LEFT OUTER JOIN t_index i ON sl.index=i.cng_name"
+    			+ " LEFT OUTER JOIN t_index i ON (sl.index=i.cng_name OR sl.index=i.short_name)"
     			+ " WHERE f.barcode=?"
     			+ " ORDER BY l.number";
     	BeanPropertyRowMapper<LimsLibrary> mapper = new BeanPropertyRowMapper<LimsLibrary>(LimsLibrary.class);
