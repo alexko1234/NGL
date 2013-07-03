1d24d8a06dd0e92fe6746bef5a0a9801a7e3d186
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-07-03 14:46:42 +0000

    -Move import data files from ngl-sq to ngl-data
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1017 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-data/app/models/TaraDAO.java
@@ -0,0 +1,86 @@
+package models;
+
+import static validation.utils.ConstraintsHelper.addErrors;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import models.laboratory.common.instance.PropertyValue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import play.Logger;
+import play.data.validation.ValidationError;
+
+@Repository
+public class TaraDAO {
+
+	private JdbcTemplate jdbcTemplate;
+
+
+	@Autowired
+	@Qualifier("tara")
+	public void setDataSourceTara(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);              
+	}
+
+	public Map<String,PropertyValue> findTaraSample(Integer limsCode,Map<String,List<ValidationError>> errors){
+
+		List<Map<String,PropertyValue>> results =  this.jdbcTemplate.query("   SELECT"+
+				"  cast(AREA.AREA_CODE as SIGNED INT) as station ,"+
+				"  AREA.AREA_NAME,"+
+				"  FRACTION.FRACTION_CODE,"+
+				"  FRACTION.FRACTION_NAME as filtre,"+
+				"  ITERATION.ITERATION_CODE as iteration,"+
+				"  ITERATION.ITERATION_NAME,"+
+				"  LOCUS.LOCUS_CODE ,"+
+				"  LOCUS.LOCUS_NAME as profondeur,"+
+				"  MATERIAL.MATERIAL_CODE,"+
+				"  MATERIAL.MATERIAL_NAME as materiel,"+
+				"  CROSS_REF.REF_ID as ref_id"+
+				" FROM "+
+				" AREA INNER JOIN SAMPLE ON (AREA.AREA_ID=SAMPLE.AREA_ID)"+
+				"  INNER JOIN CROSS_REF ON (SAMPLE.SAMPLE_ID=CROSS_REF.SAMPLE_ID)"+
+				"  INNER JOIN FRACTION ON (SAMPLE.FRACTION_ID=FRACTION.FRACTION_ID)"+
+				"  INNER JOIN ITERATION ON (ITERATION.ITERATION_ID=SAMPLE.ITERATION_ID)"+
+				"  INNER JOIN LOCUS ON (SAMPLE.LOCUS_ID=LOCUS.LOCUS_ID)"+
+				"  INNER JOIN MATERIAL ON (MATERIAL.MATERIAL_ID=SAMPLE.MATERIAL_ID)" +
+				"  WHERE CROSS_REF.REF_ID=? ", 
+				new Object[]{limsCode},new RowMapper<Map<String,PropertyValue>>() {
+
+			public Map<String,PropertyValue> mapRow(ResultSet rs, int rowNum) throws SQLException {
+				
+				Logger.debug("Tara :"+rs.getInt("ref_id"));
+				
+				//TODO manque materiel origine
+				Map<String,PropertyValue> properMap=new HashMap<String, PropertyValue>();
+				properMap.put("taraStation", new PropertyValue(rs.getInt("station")));
+				properMap.put("taraDepth", new PropertyValue(rs.getString("profondeur")));
+				properMap.put("taraFilter", new PropertyValue(rs.getString("filtre")));
+				properMap.put("taraIteration", new PropertyValue(rs.getString("iteration")));
+				properMap.put("taraSample", new PropertyValue(rs.getString("materiel")));
+				return properMap;
+			}
+
+		});     
+		
+		if(results.size()==1){
+			return results.get(0);
+		} else {
+			addErrors(errors,"taraRefId","limsdao.error.code.notexist","Tara Reference Id", limsCode);
+			return null;
+		}
+
+
+	}
+
+}
---
+++ app-ngl-data/app/services/instance/ImportDataRun.java
@@ -0,0 +1,141 @@
+package services.instance;
+
+import static validation.utils.ConstraintsHelper.addErrors;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import models.LimsDAO;
+import models.laboratory.common.instance.PropertyValue;
+import models.laboratory.container.instance.Container;
+import models.laboratory.container.instance.Content;
+import models.laboratory.project.description.ProjectType;
+import models.laboratory.project.instance.Project;
+import models.laboratory.sample.instance.Sample;
+import models.utils.HelperObjects;
+import models.utils.InstanceConstants;
+import models.utils.InstanceHelpers;
+import models.utils.ListObject;
+import models.utils.dao.DAOException;
+import models.utils.instance.ContainerHelper;
+import play.Logger;
+import play.api.modules.spring.Spring;
+import play.data.validation.ValidationError;
+import sun.security.jca.GetInstance.Instance;
+import validation.utils.ConstraintsHelper;
+import fr.cea.ig.MongoDBDAO;
+
+public class ImportDataRun implements Runnable {
+
+	static Map<String, List<ValidationError>> errors = new HashMap<String, List<ValidationError>>();
+	static LimsDAO  limsServices = Spring.getBeanOfType(LimsDAO.class);
+
+	@Override
+	public void run() {
+		errors.clear();
+		Logger.info("ImportDataRun execution");
+		try{
+			createProjectFromLims();
+			createContainersSamples();
+
+		}catch (Exception e) {
+			Logger.debug("",e);
+		}
+		/* Display error messages */
+		Iterator entries = errors.entrySet().iterator();
+		while (entries.hasNext()) {
+			Entry thisEntry = (Entry) entries.next();
+			String key = (String) thisEntry.getKey();
+			List<ValidationError> value = (List<ValidationError>) thisEntry.getValue();	  
+
+			for(ValidationError validationError:value){
+				Logger.debug(key+ " : "+validationError.message() + " "+validationError.arguments());
+			}
+
+		}
+	}
+
+
+
+	public static List<Project> createProjectFromLims() throws SQLException, DAOException{
+
+		List<Project> projects = limsServices.findProjectToCreate(errors) ;
+		
+		for(Project project:projects){
+			
+			if(project.exist(errors)){
+				MongoDBDAO.deleteByCode(InstanceConstants.PROJECT_COLL_NAME, Project.class, project.code);
+				Logger.debug("Project to create :"+project.code);
+			}
+			
+		}
+		
+		return InstanceHelpers.save(InstanceConstants.PROJECT_COLL_NAME,projects,errors);
+		
+
+	}
+
+
+
+	public	static void createContainersSamples() throws SQLException, DAOException{
+		
+		List<Container> containers = limsServices.findContainersToCreate(errors); 
+		List<Container> listContainers = new ArrayList<Container>(containers);
+		
+		Sample sample =null;
+		Sample newSample =null;
+		for(Container container :listContainers){
+
+			Content content= container.contents.get(0);
+			sample = new HelperObjects<Sample>().getObject(Sample.class, content.sampleUsed.sampleCode);
+		
+			/* Sample content not in MongoDB */
+			if(sample==null){
+				/* Find sample in Mongodb */
+				sample = limsServices.findSampleToCreate(errors,container.contents.get(0).sampleUsed.sampleCode);
+				newSample =(Sample) InstanceHelpers.save(InstanceConstants.SAMPLE_COLL_NAME,sample,errors);
+				
+			}else {	newSample = sample;}
+			
+			if(newSample==null){
+				/* Error : No sample, remove container from list to create */
+				containers.remove(container);
+				//Logger.debug("Remove container"+container.code );
+				addErrors(errors, "container","initialdata.container.samplenotexist", container.support.barCode,content.sampleUsed.sampleCode);
+			}
+			else{
+				Map<String,PropertyValue> properties=container.contents.get(0).properties;
+				container.contents.clear();
+				ContainerHelper.addContent(container,newSample);
+				container.contents.get(0).properties.putAll(properties);
+			}
+
+		}
+
+		List<Container> newContainers=InstanceHelpers.save(InstanceConstants.CONTAINER_COLL_NAME,containers,errors);
+
+		Logger.debug("Nb containers créés :"+newContainers.size());
+		
+		limsServices.updateTubeLims(newContainers,errors);
+
+		Logger.info("Maj des tubes du Lims");
+	}
+
+	//TODO
+	//Maj volume, conc, quantity
+	public static void updateContainerFromLims() throws SQLException, DAOException{
+		
+	}
+
+	//TODO
+	//Maj referenceCollab and resolution ???	
+	public static void updateSampleFromLims() throws SQLException, DAOException{
+			
+	
+	}
+}
