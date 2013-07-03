1cd905a890133d45e0e3c4094fb30588808df81f
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-07-03 14:55:30 +0000

    Move files in ngl-data project
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1019 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/Global.java
+++ app-ngl-sq/app/Global.java
@@ -1,40 +1,22 @@
 
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
-
-import org.joda.time.DateTime;
-import org.joda.time.Seconds;
 
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
-import play.libs.Akka;
 import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Http.Request;
-import scala.concurrent.duration.Duration;
-import data.ImportDataRun;
 
 public class Global extends GlobalSettings {
 
 	@Override
 	public void onStart(Application app) {
 		Logger.info("NGL has started");
-		
-		Akka.system().scheduler().schedule(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.SECONDS)
-                , new ImportDataRun(), Akka.system().dispatcher()
-				); 
-		
-		Akka.system().scheduler().schedule(
-				Duration.create(nextExecutionInSeconds(22,13), TimeUnit.SECONDS),
-                Duration.create(24, TimeUnit.HOURS)
-                , new ImportDataRun(), Akka.system().dispatcher()
-				); 
 	}
 
 	@Override
 	public Action onRequest(Request request, Method actionMethod) {
-
 		//call CAS module
 		return new play.modules.cas.CasAuthentication();
 	}
@@ -49,24 +31,5 @@ public class Global extends GlobalSettings {
 		Logger.info("NGL shutdown...");
 	}
 
-	
-	
-	 public static int nextExecutionInSeconds(int hour, int minute){
-	        return Seconds.secondsBetween(
-	                new DateTime(),
-	                nextExecution(hour, minute)
-	        ).getSeconds();
-	    }
 
-	    public static DateTime nextExecution(int hour, int minute){
-	        DateTime next = new DateTime()
-	                .withHourOfDay(hour)
-	                .withMinuteOfHour(minute)
-	                .withSecondOfMinute(0)
-	                .withMillisOfSecond(0);
-
-	        return (next.isBeforeNow())
-	                ? next.plusHours(24)
-	                : next;
-	    }
 }
