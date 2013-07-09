b0674eaa970b2dc285a28a79f8d40868c5e5f45e
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-07-09 09:46:47 +0000

    add find in Instrument model class
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1045 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/instrument/description/Instrument.java
+++ lib-ngl-common/app/models/laboratory/instrument/description/Instrument.java
@@ -8,6 +8,9 @@ public class Instrument extends Model<Instrument>{
 
 	public String name;
 	
+	public static Finder<Instrument> find = new Finder<Instrument>(InstrumentDAO.class.getName()); 
+
+	
 	public Instrument() {
 		super(InstrumentDAO.class.getName());
 	}
