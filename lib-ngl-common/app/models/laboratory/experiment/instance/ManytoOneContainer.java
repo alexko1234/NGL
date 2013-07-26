f9d7ae7a98a31fd37320e01be96c895200634aec
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-07-26 08:58:00 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1075 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/experiment/instance/ManytoOneContainer.java
+++ lib-ngl-common/app/models/laboratory/experiment/instance/ManytoOneContainer.java
@@ -1,28 +1,53 @@
 package models.laboratory.experiment.instance;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import models.laboratory.common.description.PropertyDefinition;
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.container.instance.Container;
-
-import org.codehaus.jackson.annotate.JsonIgnore;
-
+import models.laboratory.experiment.description.ExperimentType;
+import models.utils.InstanceConstants;
+import models.utils.instance.ContainerHelper;
+import net.vz.mongodb.jackson.DBQuery;
 import play.data.validation.ValidationError;
+import fr.cea.ig.MongoDBDAO;
 
 public class ManytoOneContainer extends AtomicTransfertMethod{
 
 	public int inputNumber;
-	
+
 	public List<ContainerUsed> inputContainerUseds;
 	public ContainerUsed outputContainerUsed;
 
 	@Override
 	public List<Container> createOutputContainerUsed(
 			Map<String, PropertyDefinition> propertyDefinitions,
-			Map<String, PropertyValue> propertyValues) {
-		return null;
+			Map<String, PropertyValue> propertyValues,Experiment experiment) {
+
+
+		List<String> containerCodes= new ArrayList<String>();
+		for(int i=0;i<inputContainerUseds.size();i++){
+			containerCodes.add(inputContainerUseds.get(i).containerCode);
+		}
+
+		List<Container> inputContainers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.in("code", containerCodes)).toList();
+
+		List<Container> containers = new ArrayList<Container>();
+		Container container = new Container();
+
+		for(int i=0;i<inputContainers.size();i++){
+			ContainerHelper.addContent(inputContainers.get(i), container,experiment);
+		}
+
+		ContainerHelper.copyProperties(propertyDefinitions,propertyValues,container);			
+		ContainerHelper.addContainerSupport(container, experiment);
+		ContainerHelper.generateCode(container);
+		
+		outputContainerUsed=new ContainerUsed(container);
+		containers.add(container);
+		return containers;
 	}
 
 	@Override
