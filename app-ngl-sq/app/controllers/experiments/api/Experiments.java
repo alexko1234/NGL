5311f5cc4d060be6945ade96dc3ccfb7800242d9
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-08-06 14:56:16 +0000

    Simplify the creation of containerOut
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1112 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/controllers/experiments/api/Experiments.java
+++ app-ngl-sq/app/controllers/experiments/api/Experiments.java
@@ -5,6 +5,7 @@ import static play.data.Form.form;
 import java.util.List;
 import java.util.Map;
 
+import models.laboratory.common.description.Level;
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.common.instance.TraceInformation;
 import models.laboratory.container.instance.Container;
@@ -34,200 +35,182 @@ import fr.cea.ig.MongoDBDAO;
 public class Experiments extends CommonController{
 
 	final static Form<Experiment> experimentForm = form(Experiment.class);
-	
+
 	public static Result updateExperimentInformations(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	Workflows.setExperimentStateCode(exp,experimentFilledForm.errors());	
-	 	
-	 	if (!experimentFilledForm.hasErrors()) {
-		 	
+
+		exp = traceInformation(exp);
+
+		Workflows.setExperimentStateCode(exp,experimentFilledForm.errors());	
+
+		if (!experimentFilledForm.hasErrors()) {
+
 			Builder builder = new DBUpdate.Builder();
 			builder=builder.set("typeCode",exp.typeCode);
 			builder=builder.set("resolutionCode",exp.resolutionCode);
 			builder=builder.set("protocolCode",exp.protocolCode);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result generateOutput(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	List<Container> containers = null;
-	 	if (!experimentFilledForm.hasErrors()) {
-	 		for(int i=0;i<exp.atomicTransfertMethods.size();i++){
-	 			Map<String,PropertyValue> propertiesOutput = null;
-	 			Map<String,PropertyValue> propertiesInput = null;
-	 			
-	 			for(ContainerUsed c : exp.atomicTransfertMethods.get(i).getInputContainers()){
-	 				if(c.experimentProperties !=null && c.instrumentProperties!=null){
-	 					propertiesInput = c.experimentProperties;
-	 					propertiesInput.putAll(c.instrumentProperties);
-	 					models.utils.InstanceHelpers.copyPropertyValueFromLevel(exp.getExperimentType().getMapPropertyDefinition(),"container", propertiesInput,propertiesOutput);		 			
-	 				}
-	 			}
-	 			
-	 			if(exp.experimentProperties !=null && exp.instrumentProperties!=null){
-	 				propertiesInput = exp.experimentProperties;
-	 				propertiesInput.putAll(exp.instrumentProperties);
-	 			
-	 				models.utils.InstanceHelpers.copyPropertyValueFromLevel(exp.getExperimentType().getMapPropertyDefinition(),"experiment", propertiesInput,propertiesOutput);		 				
-	 			}
-	 			
-	 			containers = exp.atomicTransfertMethods.get(i).createOutputContainerUsed(exp.getExperimentType().getMapPropertyDefinition(), propertiesOutput,exp);
-	 	}
-	 		
-	 		
+
+		exp = traceInformation(exp);
+		List<Container> containers = null;
+		if (!experimentFilledForm.hasErrors()) {
+			for(int i=0;i<exp.atomicTransfertMethods.size();i++){
+				containers = exp.atomicTransfertMethods.get(i).createOutputContainerUsed(exp);
+			}
+
+
 			Builder builder = new DBUpdate.Builder();
 			builder=builder.set("atomicTransfertMethods",exp.atomicTransfertMethods);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
-			
+
 			InstanceHelpers.save(Constants.EXPERIMENT_COLL_NAME, containers,new ContextValidation( experimentFilledForm.errors()));
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result updateInstrumentInformations(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	if (!experimentFilledForm.hasErrors()) {
-		 	
+
+		exp = traceInformation(exp);
+
+		if (!experimentFilledForm.hasErrors()) {
+
 			Builder builder = new DBUpdate.Builder();
 			builder = builder.set("instrument",exp.instrument);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result updateExperimentProperties(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	if (!experimentFilledForm.hasErrors()) {
-		 	
+
+		exp = traceInformation(exp);
+
+		if (!experimentFilledForm.hasErrors()) {
+
 			Builder builder = new DBUpdate.Builder();
 			builder = builder.set("experimentProperties",exp.experimentProperties);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result updateInstrumentProperties(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	if (!experimentFilledForm.hasErrors()) {
-		 	
+
+		exp = traceInformation(exp);
+
+		if (!experimentFilledForm.hasErrors()) {
+
 			Builder builder = new DBUpdate.Builder();
 			builder=builder.set("instrumentProperties",exp.instrumentProperties);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result getInstrumentProperties(String instrumentUsedTypeCode){
 		InstrumentUsedTypeDAO instrumentUsedTypesDAO = Spring.getBeanOfType(InstrumentUsedTypeDAO.class);
 		InstrumentUsedType instrumentUsedType = null;
-		
+
 		try {
 			instrumentUsedType = instrumentUsedTypesDAO.findByCode(instrumentUsedTypeCode);
 		} catch (DAOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return ok(Json.toJson(instrumentUsedType.propertiesDefinitions));
 	}
-	
+
 	public static Result updateComments(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	if (!experimentFilledForm.hasErrors()) {
-		 	
+
+		exp = traceInformation(exp);
+
+		if (!experimentFilledForm.hasErrors()) {
+
 			Builder builder = new DBUpdate.Builder();
 			builder=builder.set("comments",exp.comments);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result updateContainers(String code){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
 		Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
 
-	 	
+		exp = traceInformation(exp);
+
+
 		if (!experimentFilledForm.hasErrors()) {
 			for(int i=0;i<exp.atomicTransfertMethods.size();i++){
 				Workflows.setInWaitingExperiment(exp.atomicTransfertMethods.get(i).getInputContainers());
 			}
-			
+
 			Builder builder = new DBUpdate.Builder();
 			builder=builder.set("atomicTransfertMethods",exp.atomicTransfertMethods);
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
+
 	public static Result updateStateCode(String code){
-	 	Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
-	 	Experiment exp = experimentFilledForm.get();
-	 	
-	 	exp = traceInformation(exp);
-	 	
-	 	
-	 		//TODO if first experiment in the processus then processus state to IP
-	 	Workflows.setExperimentStateCode(exp,experimentFilledForm.errors());
-	 	if (!experimentFilledForm.hasErrors()) {	 	
+		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
+		Experiment exp = experimentFilledForm.get();
+
+		exp = traceInformation(exp);
+
+
+		//TODO if first experiment in the processus then processus state to IP
+		Workflows.setExperimentStateCode(exp,experimentFilledForm.errors());
+		if (!experimentFilledForm.hasErrors()) {	 	
 			Builder builder = new DBUpdate.Builder();
 			builder = builder.set("stateCode",exp.stateCode);//TODO: validation? buisness validation
-			
+
 			MongoDBDAO.updateSetArray(Constants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("code", code),builder);
 			return ok(Json.toJson(exp));
-	 	}
-	 	
-	 	return badRequest(experimentFilledForm.errorsAsJson());
+		}
+
+		return badRequest(experimentFilledForm.errorsAsJson());
 	}
-	
-	
+
+
 	/**
 	 * Save or update an Experiment object	
 	 * @param experimentType
@@ -235,24 +218,24 @@ public class Experiments extends CommonController{
 	 */
 	public static Result save(String experimentType){
 		Form<Experiment> experimentFilledForm = getFilledForm(experimentForm,Experiment.class);
-	 	Experiment exp = experimentFilledForm.get();
-	 	
-	 	if(exp._id == null){
+		Experiment exp = experimentFilledForm.get();
+
+		if(exp._id == null){
 			exp.code = CodeHelper.generateExperiementCode(exp);
 			exp.typeCode = experimentType;
 			exp.stateCode = "N";
-	 	}
-	 	
+		}
+
 		exp = traceInformation(exp);
-	 	
+
 		if (!experimentFilledForm.hasErrors()) {	 	
 			exp = (Experiment) InstanceHelpers.save(Constants.EXPERIMENT_COLL_NAME, exp, new ContextValidation(experimentFilledForm.errors()));
-	 		return ok(Json.toJson(exp));
-	 	} else {
-	 		return badRequest(experimentFilledForm.errorsAsJson());
-	 	}
+			return ok(Json.toJson(exp));
+		} else {
+			return badRequest(experimentFilledForm.errorsAsJson());
+		}
 	}
-	
+
 	/**
 	 * Add/Create trace informations to the experiment object
 	 * @param exp: the Experiment object
@@ -266,7 +249,7 @@ public class Experiments extends CommonController{
 		} else {
 			exp.traceInformation.setTraceInformation(PermissionHelper.getCurrentUser(session()));
 		}
-		
+
 		return exp;
 	}
 }
