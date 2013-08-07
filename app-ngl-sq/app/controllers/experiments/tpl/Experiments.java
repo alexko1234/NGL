bf3aece581615af857849757cfd095b987a72d0a
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-08-07 15:10:59 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1119 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/controllers/experiments/tpl/Experiments.java
+++ app-ngl-sq/app/controllers/experiments/tpl/Experiments.java
@@ -1,12 +1,13 @@
 package controllers.experiments.tpl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.laboratory.common.description.PropertyDefinition;
 import models.laboratory.experiment.description.ExperimentType;
 import models.utils.dao.DAOException;
-import play.Logger;
 import play.Routes;
 import play.i18n.Messages;
 import play.libs.Json;
@@ -58,7 +59,7 @@ public class Experiments extends Controller{
 			 
 		//TODO: Multi header datatable
 		//TODO: Datatable with tubes
-		List<DatatableColumn> columns = new ArrayList<DatatableColumn>();
+		/*List<DatatableColumn> columns = new ArrayList<DatatableColumn>();
 		columns.add(DatatableHelpers.getColumn("code", Messages.get("containers.table.code"), true, false, false));	
 		columns.add(DatatableHelpers.getColumn("projectCodes", Messages.get("containers.table.projectCodes"), true, false, false));	
 		columns.add(DatatableHelpers.getColumn("mesuredVolume.value", Messages.get("experiments.table.volume.value"),true, true, false));	
@@ -71,11 +72,23 @@ public class Experiments extends Controller{
 		List<PropertyDefinition> props = experimentProperties(experimentTypeCode);
 		
 		DatatableConfig config = new DatatableConfig(columns);
-		config.remove = true;
 		config.button = true;
-		config.save = true;
 		config.edit = true;
 		
+		List<String> ids = new ArrayList<String>();
+		ids.add("experimentProperties");
+		List<String> ids2 = new ArrayList<String>();
+		ids2.add("instrumentProperties");
+		List<String> ids3 = new ArrayList<String>();
+		ids3.add("inputContainer");
+		List<String> ids4 = new ArrayList<String>();
+		ids4.add("outputContainer");
+		
+		DatatableHelpers.addExtraHeaders(config, 1,DatatableHelpers.getDynamicHeader("Instrument", ids2));
+		DatatableHelpers.addExtraHeaders(config, 1,DatatableHelpers.getDynamicHeader("Experiment", ids));
+		DatatableHelpers.addExtraHeaders(config, 0,DatatableHelpers.getDynamicHeader("Inputs", ids3));
+		DatatableHelpers.addExtraHeaders(config, 0,DatatableHelpers.getDynamicHeader("Outputs", ids4));
+		*/
 		ExperimentType experimentType = null;
 		try{
 			experimentType = ExperimentType.find.findByCode(experimentTypeCode);
@@ -83,8 +96,33 @@ public class Experiments extends Controller{
 			
 		}
 		
+		List<PropertyDefinition> props = experimentProperties(experimentTypeCode);
+		
+		DatatableConfig config = new DatatableConfig();
+		config.button = true;
+		config.edit = true;
+		
 		return ok(createExperiments.render(Json.toJson(experimentType),config,Json.toJson(props)));
 	}
+	
+	public static Result getEditExperimentColumns(){
+		
+		List<DatatableColumn> columns = new ArrayList<DatatableColumn>();
+		Map<Integer,String> extraHeaders = new HashMap<Integer, String>();
+		extraHeaders.put(0, "Inputs");
+		
+		columns.add(DatatableHelpers.getColumn("code", Messages.get("containers.table.code"), true, false, true,false,extraHeaders));	
+		columns.add(DatatableHelpers.getColumn("projectCodes", Messages.get("containers.table.projectCodes"), true, false, true,false,extraHeaders));	
+		columns.add(DatatableHelpers.getColumn("mesuredVolume.value", Messages.get("experiments.table.volume.value"),true, true, true,false,extraHeaders));	
+		columns.add(DatatableHelpers.getColumn("sampleCodes", Messages.get("containers.table.sampleCodes"), true, false, true,false,extraHeaders));		
+		columns.add(DatatableHelpers.getColumn("valid", Messages.get("containers.table.valid"), true, false, true,false,extraHeaders));
+		columns.add(DatatableHelpers.getColumn("stateCode", Messages.get("containers.table.stateCode"), true, false, true,false,extraHeaders));
+		columns.add(DatatableHelpers.getColumn("categoryCode", Messages.get("containers.table.categoryCode"), true, false, true,false,extraHeaders));
+		columns.add(DatatableHelpers.getColumn("fromExperimentTypeCodes", Messages.get("containers.table.fromExperimentTypeCodes"), true, false, true,false,extraHeaders));
+		
+		return ok(Json.toJson(columns));
+	}
+	
 	
 	public static Result javascriptRoutes() {
   	    response().setContentType("text/javascript");
@@ -113,7 +151,8 @@ public class Experiments extends Controller{
   	    		controllers.experiments.api.routes.javascript.Experiments.save(),
   	    		controllers.experiments.api.routes.javascript.Experiments.updateContainers(),
   	    		controllers.lists.api.routes.javascript.Lists.protocols(),
-  	    		controllers.lists.api.routes.javascript.Lists.resolutions()
+  	    		controllers.lists.api.routes.javascript.Lists.resolutions(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.getEditExperimentColumns()
   	      )	  	      
   	    );
   	}
