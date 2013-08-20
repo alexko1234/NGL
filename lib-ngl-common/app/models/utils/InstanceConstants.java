e92a595b9b6e5522e826d55b3b541c1c8a105da9
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-08-20 07:59:17 +0000

    DNO 20-08-2013 - maj des constantes de validation
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1145 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/utils/InstanceConstants.java
+++ lib-ngl-common/app/models/utils/InstanceConstants.java
@@ -9,6 +9,7 @@ public interface InstanceConstants {
 	public static final String SAMPLE_COLL_NAME = "Sample";
 	public static final String STOCK_COLL_NAME = "Stock";
 	public static final String REAGENT_INSTANCE_COLL_NAME = "ReagentInstance";
-
+	public static final String RUN_ILLUMINA_COLL_NAME = "run.illumina";
+	
 
 }
