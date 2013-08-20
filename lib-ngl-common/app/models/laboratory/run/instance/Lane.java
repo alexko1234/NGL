33ca57d9168aec96c45a98306a76776c76086a2b
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-08-20 07:57:57 +0000

    DNO 20-08-2013 - uniformisation du code
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1142 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/instance/Lane.java
+++ lib-ngl-common/app/models/laboratory/run/instance/Lane.java
@@ -1,14 +1,30 @@
 package models.laboratory.run.instance;
 
+import static validation.utils.ConstraintsHelper.getKey;
+import static validation.utils.ConstraintsHelper.required;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.bson.types.ObjectId;
+
+import net.vz.mongodb.jackson.DBQuery;
+
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.common.instance.TBoolean;
+import models.utils.IValidation;
+import models.utils.InstanceConstants;
+import validation.utils.ValidationConstants;
 import models.utils.InstanceHelpers;
+import validation.InstanceValidationHelper;
+import validation.utils.ConstraintsHelper;
+import validation.utils.ContextValidation;
+import validation.utils.RunPropertyDefinitionHelper;
+import fr.cea.ig.DBObject;
+import fr.cea.ig.MongoDBDAO;
 
-public class Lane {
+public class Lane implements IValidation {
 	
 	public Integer number;
 	public TBoolean abort = TBoolean.UNSET;
@@ -32,4 +48,30 @@ public class Lane {
 	phasing
 	prephasing		
 	 */
+	
+	@Override
+	public void validate(ContextValidation contextValidation) {
+		
+		Run run = (Run) contextValidation.contextObjects.get("run");
+		
+		if(required(contextValidation.errors, this.number, getKey(contextValidation.rootKeyName,"number"))){
+			//Validate unique lane.number if run already exist
+			if(null != run._id ){
+				System.out.println("number :" +  this.number + " code :" + run.toString());
+				Run runExist = MongoDBDAO.findOne(InstanceConstants.RUN_ILLUMINA_COLL_NAME, Run.class, DBQuery.is("code", run.code).is("lanes.number", this.number));
+				//TODO : update case not managed
+				if (runExist != null) {
+					System.out.println("erreur integrite");
+					ConstraintsHelper.addErrors(contextValidation.errors,getKey(contextValidation.rootKeyName,"code"),ValidationConstants.ERROR_NOTUNIQUE,this.number);
+				}
+			}
+		}
+		
+		String rootKeyNameProp = getKey(contextValidation.rootKeyName,"properties");
+		ConstraintsHelper.validateProperties(contextValidation.errors, this.properties, RunPropertyDefinitionHelper.getLanePropertyDefinitions(), rootKeyNameProp);
+		
+		contextValidation.contextObjects.put("lane", this);
+		InstanceValidationHelper.validationReadSets(this.readsets, contextValidation);
+
+	}
 }
