68edeb835e4cb8d8d83e6564d0a0cc6b062bc15b
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-08-20 07:58:20 +0000

    DNO 20-08-2013 - uniformisation du code
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1144 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/instance/Run.java
+++ lib-ngl-common/app/models/laboratory/run/instance/Run.java
@@ -1,16 +1,31 @@
 package models.laboratory.run.instance;
 
+import static validation.utils.ConstraintsHelper.getKey;
+import static validation.utils.ConstraintsHelper.required;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import models.laboratory.common.description.PropertyDefinition;
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.common.instance.TBoolean;
 import models.laboratory.common.instance.TraceInformation;
-import models.laboratory.instrument.instance.InstrumentUsed;
+import models.laboratory.run.instance.InstrumentUsed;
+import models.utils.IValidation;
+import models.utils.InstanceConstants;
 import models.utils.InstanceHelpers;
+import validation.DescriptionValidationHelper;
+import validation.InstanceValidationHelper;
+import validation.utils.BusinessValidationHelper;
+import validation.utils.ConstraintsHelper;
+import validation.utils.ContextValidation;
+import validation.utils.RunPropertyDefinitionHelper;
 import fr.cea.ig.DBObject;
-public class Run extends DBObject {
+
+
+public class Run extends DBObject implements IValidation {
 		
 	public TraceInformation traceInformation;
 	public String typeCode;
@@ -28,6 +43,35 @@ public class Run extends DBObject {
 
 	public InstrumentUsed instrumentUsed;
 	public List<Lane> lanes;
+	
+	
+
+	@Override
+	public void validate(ContextValidation contextValidation) {
+			
+			contextValidation.contextObjects.put("_id",this._id);
+			
+			if(required(contextValidation.errors, this.code, getKey(contextValidation.rootKeyName,"code"))){
+				BusinessValidationHelper.validateUniqueInstanceCode(contextValidation, this.code, Run.class, InstanceConstants.RUN_ILLUMINA_COLL_NAME);		
+			}
+
+			traceInformation.validate(contextValidation);
+			
+			DescriptionValidationHelper.validationRunTypeCode(this.typeCode, contextValidation); 
+			DescriptionValidationHelper.validationContainerSupportCode(this.containerSupportCode, contextValidation); 
+			
+			this.instrumentUsed.validate(contextValidation); 
+				
+			contextValidation.contextObjects.put("run", this);
+			contextValidation.rootKeyName = "lanes";
+			InstanceValidationHelper.validationLanes(this.lanes, contextValidation);
+			
+
+			String rootKeyNameProp = getKey(contextValidation.rootKeyName,"properties");
+			ConstraintsHelper.validateProperties(contextValidation.errors, this.properties, RunPropertyDefinitionHelper.getRunPropertyDefinitions(), rootKeyNameProp);
+				
+		}		
+	}
 
 	/*
 	 	nbClusterIlluminaFilter
@@ -41,9 +85,3 @@ public class Run extends DBObject {
 	 	mismatch
 	 */
 	
-	/*
-	id du depot flowcell ???
-	id du type de sequençage ???
-	*/
-
-}
