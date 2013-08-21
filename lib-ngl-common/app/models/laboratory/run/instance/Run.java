de006d359c6231e5372f0186da94cc3525943198
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-08-21 13:42:12 +0000

    DNO 21-08-2013 correction bug message d'erreurs
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1160 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/instance/Run.java
+++ lib-ngl-common/app/models/laboratory/run/instance/Run.java
@@ -1,88 +1,100 @@
 package models.laboratory.run.instance;
 
+import static validation.utils.ConstraintsHelper.getKey;
+import static validation.utils.ConstraintsHelper.required;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import models.laboratory.common.description.PropertyDefinition;
 import validation.DescriptionValidationHelper;
 import validation.InstanceValidationHelper;
 import validation.utils.BusinessValidationHelper;
 import validation.utils.ContextValidation;
 import validation.utils.RunPropertyDefinitionHelper;
 import validation.utils.ValidationHelper;
-
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.common.instance.TBoolean;
 import models.laboratory.common.instance.TraceInformation;
 import models.laboratory.run.instance.InstrumentUsed;
 import models.utils.IValidation;
 import models.utils.InstanceConstants;
+
 import fr.cea.ig.DBObject;
 
 
 public class Run extends DBObject implements IValidation {
-		
-	public TraceInformation traceInformation;
-	public String typeCode;
-	
-	public Date transfertStartDate;
-	public Date transfertEndDate;
-	public Boolean dispatch = Boolean.FALSE;
-	
-	public String containerSupportCode; //id flowcell
-	
-	public TBoolean abort = TBoolean.UNSET;
-	public Date abortDate;
-	
-	public Map<String, PropertyValue> properties = new HashMap<String, PropertyValue>();
+        
+    public TraceInformation traceInformation;
+    public String typeCode;
+    
+    public Date transfertStartDate;
+    public Date transfertEndDate;
+    public Boolean dispatch = Boolean.FALSE;
+    
+    public String containerSupportCode; //id flowcell
+    
+    public TBoolean abort = TBoolean.UNSET;
+    public Date abortDate;
+    
 
-	public InstrumentUsed instrumentUsed;
-	public List<Lane> lanes;
-	
-	
+    public Map<String, PropertyValue> properties = new HashMap<String, PropertyValue>();
 
-	@Override
-	public void validate(ContextValidation contextValidation) {
-			
-		contextValidation.putObject("_id",this._id);
-		
-		if(ValidationHelper.required(contextValidation.errors, this.code, ValidationHelper.getKey(contextValidation.rootKeyName,"code"))){
-			BusinessValidationHelper.validateUniqueInstanceCode(contextValidation, this.code, Run.class, InstanceConstants.RUN_ILLUMINA_COLL_NAME);		
-		}
+    public InstrumentUsed instrumentUsed;
+    public List<Lane> lanes;
+    
+    
 
-		traceInformation.validate(contextValidation);
-		
-		DescriptionValidationHelper.validationRunTypeCode(this.typeCode, contextValidation); 
-		DescriptionValidationHelper.validationContainerSupportCode(this.containerSupportCode, contextValidation); 
-		
-		this.instrumentUsed.validate(contextValidation); 
-			
-		contextValidation.putObject("run", this);
-		contextValidation.rootKeyName = "lanes";
-		InstanceValidationHelper.validationLanes(this.lanes, contextValidation);
-		
+    @Override
+    public void validate(ContextValidation contextValidation) {
+            
 
-		String rootKeyNameProp = ValidationHelper.getKey(contextValidation.rootKeyName,"properties");
-		ValidationHelper.validateProperties(contextValidation, this.properties, RunPropertyDefinitionHelper.getRunPropertyDefinitions(), rootKeyNameProp);
-			
-	}		
-}
+        contextValidation.putObject("_id",this._id);
+            
 
-	/*
-	 	nbClusterIlluminaFilter
-	 	nbCycle
-	 	nbClusterTotal
-	 	nbBase
-	 	flowcellPosition
-	 	rtaVersion
-	 	flowcellVersion
-	 	controlLane
-	 	mismatch
-	 */
-	
-	/*
-	id du depot flowcell ???
-	id du type de sequençage ???
-	*/
+        if(ValidationHelper.required(contextValidation.errors, this.code, ValidationHelper.getKey(contextValidation.rootKeyName,"code"))){        if(ValidationHelper.required(contextValidation.errors, this.code, ValidationHelper.getKey(contextValidation.rootKeyName,"code"))){
+                BusinessValidationHelper.validateUniqueInstanceCode(contextValidation, this.code, Run.class, InstanceConstants.RUN_ILLUMINA_COLL_NAME);		
+            }
+
+            traceInformation.validate(contextValidation);
+            
+            DescriptionValidationHelper.validationRunTypeCode(this.typeCode, contextValidation); 
+            DescriptionValidationHelper.validationContainerSupportCode(this.containerSupportCode, contextValidation); 
+            
+            this.instrumentUsed.validate(contextValidation); 
+                
+
+        	contextValidation.putObject("run", this);
+
+
+            InstanceValidationHelper.validationLanes(this.lanes, contextValidation);
+            
+
+
+
+            String rootKeyNameProp = getKey("",".properties");
+            ConstraintsHelper.validateProperties(contextValidation.errors, this.properties, RunPropertyDefinitionHelper.getRunPropertyDefinitions(), rootKeyNameProp);
+                
+        }		
+    }
+
+    /*
+        nbClusterIlluminaFilter
+        nbCycle
+        nbClusterTotal
+        nbBase
+        flowcellPosition
+        rtaVersion
+        flowcellVersion
+        controlLane
+        mismatch
+     */
+    
+    /*
+    id du depot flowcell ???
+    id du type de sequençage ???
+    */
 
