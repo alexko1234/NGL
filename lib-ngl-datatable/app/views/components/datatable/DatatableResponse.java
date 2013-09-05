f39a94aee5fc0bf5a387d6ab990cb9ba8c42348e
Guillaume Albini <galbini@genoscope.cns.fr>
2013-09-05 11:57:56 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1189 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-datatable/app/views/components/datatable/DatatableResponse.java
+++ lib-ngl-datatable/app/views/components/datatable/DatatableResponse.java
@@ -4,24 +4,19 @@ import java.util.List;
 
 public class DatatableResponse<T> {
 	
-	public List<? extends T> data;
+	public List<T> data;
 	public Integer recordsNumber;
 	
-	
-	
-	public DatatableResponse(List<? extends T> data) {
+	public DatatableResponse(List<T> data) {
 		super();
 		this.data = data;
 		this.recordsNumber = data.size();
 	}
 
-	public DatatableResponse(List<? extends T> data, Integer recordsNumber) {
+	public DatatableResponse(List<T> data, Integer recordsNumber) {
 		super();
 		this.data = data;
 		this.recordsNumber = recordsNumber;
 	}
-
-	
-	
 
 }
