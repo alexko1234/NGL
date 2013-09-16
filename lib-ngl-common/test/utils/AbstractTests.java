2b2adca18309e721af2fbd300cad91ce57298b12
Guillaume Albini <galbini@genoscope.cns.fr>
2013-09-16 06:35:35 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1248 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/test/utils/AbstractTests.java
+++ lib-ngl-common/test/utils/AbstractTests.java
@@ -48,7 +48,7 @@ public abstract class AbstractTests {
 				
 		config.put("mongodb.database","NGL-TESTU");
 		config.put("mongodb.credentials","testu:testu");
-		config.put("mongodb.servers","mongodev:27018");
+		config.put("mongodb.servers","mongodev.genoscope.cns.fr:27017");
 		config.put("ehcacheplugin", "disabled");
 	
 		return config;
