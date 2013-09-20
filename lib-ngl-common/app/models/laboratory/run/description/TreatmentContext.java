114517d8bc04d98d503eced0f2f988df8ff98372
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-09-20 11:51:50 +0000

    dnoisett 20092013
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1342 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/description/TreatmentContext.java
+++ lib-ngl-common/app/models/laboratory/run/description/TreatmentContext.java
@@ -1,15 +1,20 @@
 package models.laboratory.run.description;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 import models.laboratory.run.description.dao.TreatmentContextDAO;
 import models.utils.Model;
 
 public class TreatmentContext extends Model<TreatmentContext> {
 	
-	public String name;
-	
-	public static Finder<TreatmentContext> find = new Finder<TreatmentContext>(TreatmentContextDAO.class.getName());
-	
 	public TreatmentContext() {
 		super(TreatmentContextDAO.class.getName());
 	}
+	
+	public String name;
+
+    
+	public static Finder<TreatmentContext> find = new Finder<TreatmentContext>(TreatmentContextDAO.class.getName());
+	
+
 }
