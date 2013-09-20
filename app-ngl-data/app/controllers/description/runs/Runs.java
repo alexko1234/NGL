a133ef58d78b8b3c1fdc88de81c95b64c76f63cb
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-09-20 07:36:07 +0000

    dnoisett 20092013
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1334 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-data/app/controllers/description/runs/Runs.java
@@ -0,0 +1,30 @@
+package controllers.description.runs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import play.Logger;
+import play.data.validation.ValidationError;
+import play.libs.Json;
+import play.mvc.Result;
+import services.description.run.RunService;
+
+import controllers.CommonController;
+
+public class Runs extends CommonController {
+	public static Result save(){
+		try {
+			Map<String,List<ValidationError>> errors = new HashMap<String, List<ValidationError>>();
+			RunService.main(errors);
+			if (errors.size() > 0) {
+				return badRequest(Json.toJson(errors));
+			} else {
+				return ok();
+			}
+		} catch (Exception e) {
+			Logger.error(e.getMessage(), e);
+			return internalServerError(e.getMessage());
+		}				
+	}
+}
