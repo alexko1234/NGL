bf25ac383362a3373470aea2af8d1daa21974161
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-09-24 12:34:55 +0000

    dnoisett 24092013
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1359 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/description/run/RunService.java
+++ app-ngl-data/app/services/description/run/RunService.java
@@ -25,9 +25,7 @@ public class RunService {
 
 		DAOHelpers.removeAll(RunType.class, RunType.find);
 		DAOHelpers.removeAll(RunCategory.class, RunCategory.find);
-		
 		DAOHelpers.removeAll(TreatmentContext.class, TreatmentContext.find);
-		
 		DAOHelpers.removeAll(TreatmentType.class, TreatmentType.find);
 		DAOHelpers.removeAll(TreatmentCategory.class, TreatmentCategory.find);
 				
@@ -39,9 +37,7 @@ public class RunService {
 		
 		saveTreatmentCategory(errors);
 		
-		
 		saveTreatmentContext(errors);
-		
 		
 		saveTreatmentType(errors);
 		
@@ -49,11 +45,11 @@ public class RunService {
 	
 	
 	
-	//@SuppressWarnings("unchecked")
 	public static void saveRunCategories(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<RunCategory> l = new ArrayList<RunCategory>();
-		l.add(DescriptionFactory.newSimpleCategory(RunCategory.class, "RHS", "rhs"));
-		l.add(DescriptionFactory.newSimpleCategory(RunCategory.class, "RX", "rx"));
+		// name, code
+		l.add(DescriptionFactory.newSimpleCategory(RunCategory.class, "Illumina", "rhs"));
+		//l.add(DescriptionFactory.newSimpleCategory(RunCategory.class, "Myseq", "rx"));
 		
 		DAOHelpers.saveModels(RunCategory.class, l, errors);
 	}
@@ -61,14 +57,13 @@ public class RunService {
 	public static void saveRunType(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<RunType> l = new ArrayList<RunType>();
 		//newRunType(String name, String code, RunCategory category, List<PropertyDefinition> propertiesDefinitions)
-		l.add(DescriptionFactory.newRunType("RHS200","RHS200", RunCategory.find.findByCode("rhs"), null));
-		l.add(DescriptionFactory.newRunType("RHS400","RHS400", RunCategory.find.findByCode("rhs"), null));
-		l.add(DescriptionFactory.newRunType("RX","rx",RunCategory.find.findByCode("rx"),null ));
+		l.add(DescriptionFactory.newRunType("RHS2000","RHS2000", RunCategory.find.findByCode("rhs"), getRunRGropertyDefinitions()));
+		l.add(DescriptionFactory.newRunType("RHS2005","RHS2005", RunCategory.find.findByCode("rhs"), null));
+		//l.add(DescriptionFactory.newRunType("RX2","rx2",RunCategory.find.findByCode("rx"),null ));
 		
 		DAOHelpers.saveModels(RunType.class, l, errors);
 	}
 	
-	//@SuppressWarnings("unchecked")
 	public static void saveTreatmentCategory(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<TreatmentCategory> l = new ArrayList<TreatmentCategory>();
 		
@@ -82,9 +77,11 @@ public class RunService {
 	
 	public static void saveTreatmentContext(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<TreatmentContext> l = new ArrayList<TreatmentContext>();
-		l.add(DescriptionFactory.newTreatmentContext("context1","c1"));
-		l.add(DescriptionFactory.newTreatmentContext("context2","c2"));
-		l.add(DescriptionFactory.newTreatmentContext("context3","c3"));
+		l.add(DescriptionFactory.newTreatmentContext("Default","D"));
+		l.add(DescriptionFactory.newTreatmentContext("Read1","Read1"));
+		l.add(DescriptionFactory.newTreatmentContext("Read2","Read2"));
+		l.add(DescriptionFactory.newTreatmentContext("Pairs","Pairs"));
+		l.add(DescriptionFactory.newTreatmentContext("Single","Single"));
 		
 		DAOHelpers.saveModels(TreatmentContext.class, l, errors);
 	}
@@ -92,9 +89,10 @@ public class RunService {
 	
 	public static void saveTreatmentType(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<TreatmentType> l = new ArrayList<TreatmentType>();
-		//newTreatmentType(String name, String code, TreatmentCategory category, String names, List<PropertyDefinition> propertiesDefinitions, List<TreatmentContext>  contexts)
-		l.add(DescriptionFactory.newTreatmentType("QualityAnalysis","QA", TreatmentCategory.find.findByCode(TreatmentCategory.CODE.QC.name()), "names", getNGSRGropertyDefinitions(), getTreatmentContexts("c1"))); 
 		
+		//newTreatmentType(String name, String code, TreatmentCategory category, String names, List<PropertyDefinition> propertiesDefinitions, List<TreatmentContext>  contexts)
+		l.add(DescriptionFactory.newTreatmentType("ngs_rg","ngs_rg", TreatmentCategory.find.findByCode(TreatmentCategory.CODE.Quality_QC.name()), "ngs_rg", getNGSRGropertyDefinitions(), getTreatmentContexts("Read1", "Read2")));
+
 		DAOHelpers.saveModels(TreatmentType.class, l, errors);
 	}
 	
@@ -111,22 +109,57 @@ public class RunService {
 		return DAOHelpers.getModelByCodes(TreatmentType.class, TreatmentType.find, codes);
 	}
 */
-	
-	
-	//Data Test
 	public static List<PropertyDefinition> getNGSRGropertyDefinitions() throws DAOException {
 		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
 		//String name, String code, List<Level> levels, Class<?> type, Boolean required
-        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("flowcellPosition","flowcellPosition", LevelService.getLevels(Level.CODE.Run), String.class, true));
+        //Run level
+		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("flowcellPosition","flowcellPosition", LevelService.getLevels(Level.CODE.Run), String.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCycle","nbCycle", LevelService.getLevels(Level.CODE.Run), Integer.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("flowcellVersion","flowcellVersion", LevelService.getLevels(Level.CODE.Run), String.class, true));
-        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterIlluminaFilter","nbClusterIlluminaFilter", LevelService.getLevels(Level.CODE.Run), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterIlluminaFilter","nbClusterIlluminaFilter", LevelService.getLevels(Level.CODE.Run, Level.CODE.Lane), Long.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbBase","nbBase", LevelService.getLevels(Level.CODE.Run), Integer.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("mismatch","mismatch", LevelService.getLevels(Level.CODE.Run), Boolean.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("controlLane","controlLane", LevelService.getLevels(Level.CODE.Run), Integer.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("rtaVersion","rtaVersion", LevelService.getLevels(Level.CODE.Run), String.class, true));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterTotal","nbClusterTotal", LevelService.getLevels(Level.CODE.Run), Long.class, true));
-		return propertyDefinitions;
+		
+        // Lane level
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("prephasing","prephasing", LevelService.getLevels(Level.CODE.Lane), String.class, true));
+        //propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterIlluminaFilter","nbClusterIlluminaFilter", LevelService.getLevels(Level.CODE.Lane), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("percentClusterInternalAndIlluminaFilter","percentClusterInternalAndIlluminaFilter", LevelService.getLevels(Level.CODE.Lane), Double.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("phasing","phasing", LevelService.getLevels(Level.CODE.Lane), String.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCycleReadIndex2","nbCycleReadIndex2", LevelService.getLevels(Level.CODE.Lane), Integer.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCycleRead2","nbCycleRead2", LevelService.getLevels(Level.CODE.Lane), Integer.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCycleRead1","nbCycleRead1", LevelService.getLevels(Level.CODE.Lane), Integer.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCluster","nbCluster", LevelService.getLevels(Level.CODE.Lane), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbCycleReadIndex1","nbCycleReadIndex1", LevelService.getLevels(Level.CODE.Lane), Integer.class, true));
+        
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("percentClusterIlluminaFilter","percentClusterIlluminaFilter", LevelService.getLevels(Level.CODE.Lane), Double.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbBaseInternalAndIlluminaFilter","nbBaseInternalAndIlluminaFilter", LevelService.getLevels(Level.CODE.Lane, Level.CODE.ReadSet), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterInternalAndIlluminaFilter","nbClusterInternalAndIlluminaFilter", LevelService.getLevels(Level.CODE.Lane, Level.CODE.ReadSet), Long.class, true));
+        
+        // ReadSet level
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("q30","q30", LevelService.getLevels(Level.CODE.ReadSet), Double.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbUsableCluster","nbUsableCluster", LevelService.getLevels(Level.CODE.ReadSet), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("insertLength","insertLength", LevelService.getLevels(Level.CODE.ReadSet), Integer.class, true));
+        //propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbBaseInternalAndIlluminaFilter","nbBaseInternalAndIlluminaFilter", LevelService.getLevels(Level.CODE.ReadSet), Long.class, true));
+        //propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbClusterInternalAndIlluminaFilter","nbClusterInternalAndIlluminaFilter", LevelService.getLevels(Level.CODE.ReadSet), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbRead","nbRead", LevelService.getLevels(Level.CODE.ReadSet), Integer.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("score","score", LevelService.getLevels(Level.CODE.ReadSet), Double.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("nbUsableBase","nbUsableBase", LevelService.getLevels(Level.CODE.ReadSet), Long.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("fraction","fraction", LevelService.getLevels(Level.CODE.ReadSet), Double.class, true));
+        
+        // file level
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("label","label", LevelService.getLevels(Level.CODE.File), String.class, true));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("asciiEncoding","asciiEncoding", LevelService.getLevels(Level.CODE.File), String.class, true));
+        
+        return propertyDefinitions;
+	}
+	
+	//Data Test
+	public static List<PropertyDefinition> getRunRGropertyDefinitions() throws DAOException {
+
+        return null;
 	}
 	
 	
