a635db763823e3da1eb9599f41782a37090f9d20
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-10-07 15:11:58 +0000

    Deprecated interface replace by InstanceConstants in ngl-common
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1495 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/controllers/Constants.java
+++ app-ngl-sq/app/controllers/Constants.java
@@ -1,9 +1,12 @@
 package controllers;
 
+@Deprecated
 public interface Constants {
-	
+
 	public static final String CONTAINER_COLL_NAME = "Container";
+
 	public static final String PROCESS_COLL_NAME = "Process";
+	
 	public static final String EXPERIMENT_COLL_NAME = "Experiment";
 
 }
