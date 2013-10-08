39d5c2bfc3cbddf64fe5bf6e9580c4b0b14e2e69
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-08 11:08:54 +0000

    dnoisett
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1500 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/validation/utils/RunPropertyDefinitionHelper.java
+++ lib-ngl-common/app/validation/utils/RunPropertyDefinitionHelper.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import play.i18n.Messages;
 
+import models.laboratory.common.description.Level.CODE;
+import models.laboratory.common.description.Level;
 import models.laboratory.common.description.PropertyDefinition;
 
 public class RunPropertyDefinitionHelper {
@@ -22,7 +24,7 @@ public class RunPropertyDefinitionHelper {
 
 	
 	
-	public static List<PropertyDefinition> getRunPropertyDefinitions(){
+	public static List<PropertyDefinition> getRunNGSRGPropertyDefinitions(){
 		/*
 		 * properties.nbClusterTotal
 		   properties.nbBase
@@ -46,23 +48,25 @@ public class RunPropertyDefinitionHelper {
 		return propertyDefinitions;
 	}
 	
-	
-	
+	public static List<PropertyDefinition> getRunPropertyDefinitions(){
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		return propertyDefinitions;
+	}
 
-	public static List<PropertyDefinition> getLanePropertyDefinitions(){
+	public static List<PropertyDefinition> getLaneNGSRGPropertyDefinitions(){
 		/*
 			properties.nbCycleRead1
-properties.nbCycleReadIndex1
-properties.nbCycleRead2
-properties.nbCycleReadIndex2
-properties.nbCluster
-properties.nbBaseInternalAndIlluminaFilter
-properties.phasing
-properties.prephasing
-properties.nbClusterInternalAndIlluminaFilter
-properties.percentClusterInternalAndIlluminaFilter
-properties.nbClusterIlluminaFilter
-properties.percentClusterIlluminaFilter
+			properties.nbCycleReadIndex1
+			properties.nbCycleRead2
+			properties.nbCycleReadIndex2
+			properties.nbCluster
+			properties.nbBaseInternalAndIlluminaFilter
+			properties.phasing
+			properties.prephasing
+			properties.nbClusterInternalAndIlluminaFilter
+			properties.percentClusterInternalAndIlluminaFilter
+			properties.nbClusterIlluminaFilter
+			properties.percentClusterIlluminaFilter
 
 		 */
 		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
@@ -82,7 +86,12 @@ properties.percentClusterIlluminaFilter
 		return propertyDefinitions;
 	}
 	
-	public static List<PropertyDefinition> getReadSetPropertyDefinitions(){
+	public static List<PropertyDefinition> getLanePropertyDefinitions(){
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		return propertyDefinitions;
+	}
+	
+	public static List<PropertyDefinition> getReadSetNGSRGPropertyDefinitions(){
 		/*
 		
 			properties.nbClusterInternalAndIlluminaFilter
@@ -97,19 +106,44 @@ properties.percentClusterIlluminaFilter
 
 
 		 */
+		/*
 		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
 		propertyDefinitions.add(getPropertyDefinition("nbClusterInternalAndIlluminaFilter","nbClusterInternalAndIlluminaFilter",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
 		propertyDefinitions.add(getPropertyDefinition("nbBaseInternalAndIlluminaFilter","nbBaseInternalAndIlluminaFilter",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
 		propertyDefinitions.add(getPropertyDefinition("fraction","fraction",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));		
 		//propertyDefinitions.add(getPropertyDefinition("insertLengthTh","insertLengthTh",Boolean.FALSE, Boolean.TRUE, Boolean.FALSE, String.class)); //String because lims not valid
-		propertyDefinitions.add(getPropertyDefinition("nbUsableBase","nbUsableBase",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
-		propertyDefinitions.add(getPropertyDefinition("nbUsableCluster","nbUsableCluster",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		//propertyDefinitions.add(getPropertyDefinition("nbUsableBase","nbUsableBase",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		//propertyDefinitions.add(getPropertyDefinition("nbUsableCluster","nbUsableCluster",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
 		propertyDefinitions.add(getPropertyDefinition("q30","q30",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));
 		propertyDefinitions.add(getPropertyDefinition("score","score",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));
-		propertyDefinitions.add(getPropertyDefinition("nbRead",Messages.get("property.nbRead.label"),Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Integer.class));
-
+		propertyDefinitions.add(getPropertyDefinition("nbRead","nbRead",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Integer.class));
+		 */
+		
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		propertyDefinitions.add(getPropertyDefinition("nbCluster","nbCluster",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("nbBases","nbBases",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("fraction","fraction",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));		
+		//propertyDefinitions.add(getPropertyDefinition("insertLengthTh","insertLengthTh",Boolean.FALSE, Boolean.TRUE, Boolean.FALSE, String.class)); //String because lims not valid
+		//propertyDefinitions.add(getPropertyDefinition("nbUsableBase","nbUsableBase",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		//propertyDefinitions.add(getPropertyDefinition("nbUsableCluster","nbUsableCluster",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("Q30","Q30",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));
+		propertyDefinitions.add(getPropertyDefinition("qualityScore","qualityScore",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Double.class));
+		propertyDefinitions.add(getPropertyDefinition("nbReadIllumina","nbReadIllumina",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Integer.class));
+		
 		return propertyDefinitions;
 		
+	}
+	
+	public static List<PropertyDefinition> getReadSetGlobalPropertyDefinitions(){
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		propertyDefinitions.add(getPropertyDefinition("usefulSequences","usefulSequences",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("usefulBases","usefulBases",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		return propertyDefinitions;		
+	}
+	
+	public static List<PropertyDefinition> getReadSetPropertyDefinitions(){
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		return propertyDefinitions;
 	}
 	
 	public static List<PropertyDefinition> getFilePropertyDefinitions(){
@@ -123,4 +157,154 @@ properties.percentClusterIlluminaFilter
 		propertyDefinitions.add(getPropertyDefinition("label",Messages.get("property.label.label"),Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, String.class));
 		return propertyDefinitions;
 	}
+
+	public static List<PropertyDefinition> getLaneSAVPropertyDefinitions(){
+		
+		
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+	
+		propertyDefinitions.add(getPropertyDefinition("clusterDensity","clusterDensity",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("clusterDensityStd","clusterDensityStd",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Long.class));
+		propertyDefinitions.add(getPropertyDefinition("clusterPFPerc","clusterPFPerc",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("clusterPFPercStd","clusterPFPercStd",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("phasing","phasing",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("prephasing","prephasing",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("reads","reads",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("readsPF","readsPF",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("greaterQ30Perc","greaterQ30Perc",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("cyclesErrRated","cyclesErrRated",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Integer.class));
+		propertyDefinitions.add(getPropertyDefinition("alignedPerc","alignedPerc",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("alignedPercStd","alignedPercStd",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePerc","errorRatePerc",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercStd","errorRatePercStd",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle35","errorRatePercCycle35",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle35Std","errorRatePercCycle35Std",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle75","errorRatePercCycle75",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle75Std","errorRatePercCycle75Std",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle100","errorRatePercCycle100",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("errorRatePercCycle100Std","errorRatePercCycle100Std",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("intensityCycle1","intensityCycle1",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Integer.class));
+		propertyDefinitions.add(getPropertyDefinition("intensityCycle1Std","IntensityCycle1Std",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("intensityCycle20Perc","intensityCycle20Perc",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		propertyDefinitions.add(getPropertyDefinition("intensityCycle20PercStd","intensityCycle20PercStd",Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Float.class));
+		
+		
+		return propertyDefinitions;
+		
+	}
+
+	public static List<PropertyDefinition> getTreatmentPropertyDefinitions(String treatmentCode, CODE levelCode) {
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
+		
+		if(Level.CODE.ReadSet.equals(levelCode)){
+			if(treatmentCode.equalsIgnoreCase("ngsrg")){
+				propertyDefinitions = getReadSetNGSRGPropertyDefinitions();
+			}else if(treatmentCode.equalsIgnoreCase("global")){
+				propertyDefinitions = getReadSetGlobalPropertyDefinitions();
+			}
+		}else if(Level.CODE.Lane.equals(levelCode)){
+			if(treatmentCode.equalsIgnoreCase("ngsrg")){
+				propertyDefinitions = getLaneNGSRGPropertyDefinitions();
+			}else if(treatmentCode.equalsIgnoreCase("sav")){
+				propertyDefinitions =  getLaneSAVPropertyDefinitions();
+			}
+		}else if(Level.CODE.Run.equals(levelCode)){
+			if(treatmentCode.equalsIgnoreCase("ngsrg")){
+				propertyDefinitions = getRunNGSRGPropertyDefinitions();
+			}
+		}
+		return propertyDefinitions;
+	}
+
+
+	static List<String> treatmentCodes = null;
+
+	public static List<String> getTreatmentCodes() {
+		if(null == treatmentCodes){
+			treatmentCodes = new ArrayList<String>();
+			treatmentCodes.add("ngsrg");
+			treatmentCodes.add("global");
+			treatmentCodes.add("sav");			
+		}
+		return treatmentCodes;
+	}
+
+	
+	static List<String> treatmentTypeCodes = null;
+
+	public static List<String> getTreatmentTypeCodes() {
+		if(null == treatmentTypeCodes){
+			treatmentTypeCodes = new ArrayList<String>();
+			treatmentTypeCodes.add("ngsrg-illumina");
+			treatmentTypeCodes.add("global");
+			treatmentTypeCodes.add("sav");			
+		}
+		return treatmentTypeCodes;
+	}
+	
+	
+	static List<String> treatmentCatTypeCodes = null;
+
+	public static List<String> getTreatmentCatTypeCodes() {
+		if(null == treatmentCatTypeCodes){
+			treatmentCatTypeCodes = new ArrayList<String>();
+			treatmentCatTypeCodes.add("ngsrg");
+			treatmentCatTypeCodes.add("global");
+			treatmentCatTypeCodes.add("sequencing");			
+		}
+		return treatmentCatTypeCodes;
+	}
+	
+	static List<String> treatmentContextCodes = null;
+	public static List<String> getTreatmentContextCodes() {
+		if(null == treatmentContextCodes){
+			treatmentContextCodes = new ArrayList<String>();
+			treatmentContextCodes.add("default");
+			treatmentContextCodes.add("read1");
+			treatmentContextCodes.add("read2");
+			treatmentContextCodes.add("pairs");
+			treatmentContextCodes.add("single");
+			
+		}
+		return treatmentContextCodes;
+	}
+	
+	
+	static List<String> runStatesCode = null;
+	public static List<String> getRunStateCodes() {
+		if(null == runStatesCode){
+			runStatesCode = new ArrayList<String>();
+			runStatesCode.add("IP_S");
+			runStatesCode.add("IP_RG");
+			runStatesCode.add("F_RG");
+			runStatesCode.add("F");			
+		}
+		return runStatesCode;
+	}
+	
+	static List<String> readSetStatesCode = null;
+	public static List<String> getReadSetStateCodes() {
+		if(null == readSetStatesCode){
+			readSetStatesCode = new ArrayList<String>();
+			readSetStatesCode.add("IP_RG");
+			readSetStatesCode.add("F_RG");
+			readSetStatesCode.add("IW_QC");
+			readSetStatesCode.add("F_QC");
+			readSetStatesCode.add("A");
+			readSetStatesCode.add("UA");
+		}
+		return readSetStatesCode;
+	}
+	
+	static List<String> runTypeCode = null;
+	public static List<String> getRunTypeCodes() {
+		if(null == runTypeCode){
+			runTypeCode = new ArrayList<String>();
+			runTypeCode.add("RHS2000");
+			runTypeCode.add("RHS2500");
+			runTypeCode.add("RHS2500R");			
+		}
+		return runTypeCode;
+	}
+	
 }
