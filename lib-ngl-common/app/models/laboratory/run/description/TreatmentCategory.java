9cd9af52312675632f9c18add260e192fd0c59e5
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-08 11:09:30 +0000

    dnoisett rename codes
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1501 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/description/TreatmentCategory.java
+++ lib-ngl-common/app/models/laboratory/run/description/TreatmentCategory.java
@@ -7,7 +7,7 @@ import models.laboratory.run.description.dao.TreatmentCategoryDAO;
 
 public class TreatmentCategory extends AbstractCategory<TreatmentCategory>{
 	
-	public static enum CODE {Quality_QC, Quality_SAV};
+	public static enum CODE {ngsrg, global, sequencing};
 
 	public static Finder<TreatmentCategory> find = new Finder<TreatmentCategory>(TreatmentCategoryDAO.class.getName());
 	
