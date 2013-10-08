58c95e1e0dee62bf027f2a0062fa91fbe22e2ff1
Maud Haquelle <mhaquell@genoscope.cns.fr>
2013-10-08 06:22:44 +0000

    Renommage des collections Mongo
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1497 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/utils/InstanceConstants.java
+++ lib-ngl-common/app/models/utils/InstanceConstants.java
@@ -2,14 +2,14 @@ package models.utils;
 
 public interface InstanceConstants {
 	
-	public static final String PROJECT_COLL_NAME = "Project";
-	public static final String CONTAINER_COLL_NAME = "Container";
-	public static final String PROCESS_COLL_NAME = "Process";
-	public static final String EXPERIMENT_COLL_NAME = "Experiment";
-	public static final String SAMPLE_COLL_NAME = "Sample";
-	public static final String STOCK_COLL_NAME = "Stock";
-	public static final String REAGENT_INSTANCE_COLL_NAME = "ReagentInstance";
-	public static final String RUN_ILLUMINA_COLL_NAME = "run.illumina";
+	public static final String PROJECT_COLL_NAME = "ngl_project.Project";
+	public static final String CONTAINER_COLL_NAME = "ngl_sq.Container";
+	public static final String PROCESS_COLL_NAME = "ngl_sq.Process";
+	public static final String EXPERIMENT_COLL_NAME = "ngl_sq.Experiment";
+	public static final String SAMPLE_COLL_NAME = "ngl_bq.Sample";
+	public static final String STOCK_COLL_NAME = "ngl_sq.Stock";
+	public static final String REAGENT_INSTANCE_COLL_NAME = "ngl_sq.ReagentInstance";
+	public static final String RUN_ILLUMINA_COLL_NAME = "ngl_bi.run.illumina";
 	
 
 }
