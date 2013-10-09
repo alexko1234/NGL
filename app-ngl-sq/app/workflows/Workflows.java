b5217c6c8caef5e6ca85e6763aa50ea79ba9e0c1
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-10-09 13:18:21 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1533 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/workflows/Workflows.java
+++ app-ngl-sq/app/workflows/Workflows.java
@@ -9,41 +9,96 @@ import java.util.List;
 import java.util.Map;
 
 import models.laboratory.container.instance.Container;
+import models.laboratory.experiment.instance.AtomicTransfertMethod;
 import models.laboratory.experiment.instance.ContainerUsed;
 import models.laboratory.experiment.instance.Experiment;
+import models.utils.InstanceConstants;
 import play.Logger;
 import play.data.validation.ValidationError;
 import validation.ContextValidation;
-import controllers.Constants;
 import fr.cea.ig.MongoDBDAO;
 
 public class Workflows {
 
 	/**
 	 * Set a state of a container to A (Available)
-	 * @param containerCode: the code of the container
+	 * @param containerCode: the code of the container,processTypeCode: the code of the processType
 	 */
 	public static void setAvailable(String containerCode,String processTypeCode){
-		Container container = MongoDBDAO.findByCode(Constants.CONTAINER_COLL_NAME, Container.class, containerCode);
+		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerCode);
 		
 		if(container != null && (container.stateCode.equals("IWP") || container.stateCode.equals("N"))){
-			MongoDBDAO.updateSet(Constants.CONTAINER_COLL_NAME, container,"stateCode", "A");
-			MongoDBDAO.updateSet(Constants.CONTAINER_COLL_NAME, container,"processTypeCode", processTypeCode);
+			MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "A");
+			MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"processTypeCode", processTypeCode);
 		}
 	}
 	
 	/**
-	 * Set a state of a container to IU (In Use)
-	 * @param containerCode: the code of the container
+	 * Set a state of a container to A (Available)
+	 * @param containerCode: the code of the container,processTypeCode: the code of the processType
+	 */
+	public static void setAvailable(ContainerUsed containerUsed){
+		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
+		
+		if(container != null && (container.stateCode.equals("IWP") || container.stateCode.equals("N"))){
+			MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "A");
+		}
+	}
+	
+	/**
+	 * Set a state of a list of containerUsed to IU (In Use)
+	 * @param List<ContainerUsed> inputContainers: the list of container
 	 */
 	public static void setInUse(List<ContainerUsed> inputContainers){
 		for(ContainerUsed containerUsed:inputContainers){
-			Container container = MongoDBDAO.findByCode(Constants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
+			Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
 			
 			if(container != null && (container.stateCode.equals("IWE"))){
-				MongoDBDAO.updateSet(Constants.CONTAINER_COLL_NAME, container,"stateCode", "IU");
+				MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "IU");
 			}
 		}	
+	}
+	
+	/**
+	 * Set final state of list if containerUsed to IS or UN (In Stock or Unavailable)
+	 * @param List<ContainerUsed> inputContainers: the list of container
+	 */
+	public static void setFinalState(List<ContainerUsed> inputContainers){
+		for(ContainerUsed containerUsed:inputContainers){
+			if(containerUsed != null && containerUsed.resolutionCode!= null){
+				if(containerUsed.resolutionCode.equals("IS")){
+					setInStock(containerUsed);
+				}else if(containerUsed.resolutionCode.equals("UN")){
+					setUnavailable(containerUsed);
+				}else if(containerUsed.resolutionCode.equals("A")){
+					setAvailable(containerUsed);
+				}
+			}	
+		}
+	}
+	
+	/**
+	 * Set a state of a container to IS (In Stock)
+	 * @param containerCode: the code of the container
+	 */
+	public static void setInStock(ContainerUsed containerUsed){
+		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
+			
+		if(container != null && (container.stateCode.equals("IU"))){
+				MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "IS");
+		}
+	}
+	
+	/**
+	 * Set a state of a container to UA (Unavailable)
+	 * @param containerCode: the code of the container
+	 */
+	public static void setUnavailable(ContainerUsed containerUsed){
+		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
+		
+		if(container != null && (container.stateCode.equals("IU"))){
+			MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "UN");
+		}
 	}
 	
 	/**
@@ -52,10 +107,10 @@ public class Workflows {
 	 */
 	public static void setInWaitingExperiment(List<ContainerUsed> inputContainers){
 		for(ContainerUsed containerUsed:inputContainers){
-			Container container = MongoDBDAO.findByCode(Constants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
+			Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerUsed.containerCode);
 			
 			if(container != null && (container.stateCode.equals("A"))){
-				MongoDBDAO.updateSet(Constants.CONTAINER_COLL_NAME, container,"stateCode", "IWE");
+				MongoDBDAO.updateSet(InstanceConstants.CONTAINER_COLL_NAME, container,"stateCode", "IWE");
 			}
 		}	
 	}
@@ -79,6 +134,11 @@ public class Workflows {
 			required(contextValidation, experiment.protocolCode, "protocolCode");
 			required(contextValidation, experiment.instrument.code, "instrument");
 			required(contextValidation, experiment.atomicTransfertMethods, "atomicTransfertMethods");
+
+			for(int i=0;i<experiment.atomicTransfertMethods.size();i++){
+				required(contextValidation, experiment.atomicTransfertMethods.get(i).getOutputContainers(), "outputContainer");
+			}
+			
 			contextValidation.setRootKeyName("experimentProperties");
 			validateProperties(contextValidation, experiment.experimentProperties, experiment.getExperimentType().propertiesDefinitions);
 			contextValidation.removeKeyFromRootKeyName("experimentProperties");
