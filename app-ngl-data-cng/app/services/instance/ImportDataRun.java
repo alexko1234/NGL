d7f568f27e44ed87165399e4f10d02385e616454
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-15 14:18:27 +0000

    dnoisett : import containers
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1588 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data-cng/app/services/instance/ImportDataRun.java
+++ app-ngl-data-cng/app/services/instance/ImportDataRun.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import models.LimsDAO;
+import models.laboratory.container.instance.Container;
 import models.laboratory.project.instance.Project;
 import models.laboratory.sample.instance.Sample;
 import models.utils.InstanceConstants;
@@ -28,10 +29,10 @@ public class ImportDataRun implements Runnable {
 	public void run() {
 		contextError.clear();
 		contextError.addKeyToRootKeyName("import");
-		Logger.info("ImportData execution");
+		Logger.info("ImportData execution : ");
 		try{
-			Logger.info(" Import Samples ");
-			testTri();
+			Logger.info(" Import Containers ... ");
+			createContainersFromLims();
 		}catch (Exception e) {
 			Logger.debug("",e);
 		}
@@ -47,7 +48,7 @@ public class ImportDataRun implements Runnable {
 				Logger.error( key+ " : "+Messages.get(validationError.message(),validationError.arguments()));
 			}
 		}
-	    Logger.info("ImportData End");
+	    Logger.info("ImportData End !");
 	}
 
 
@@ -59,35 +60,18 @@ public class ImportDataRun implements Runnable {
      * @throws DAOException
      */
 	public static List<Project> createProjectsFromLims() throws SQLException, DAOException{
-
 		List<Project> projects = limsServices.findProjectsToCreate(contextError) ;
-		
 		for(Project project:projects){
-			
 			if(MongoDBDAO.checkObjectExistByCode(InstanceConstants.PROJECT_COLL_NAME, Project.class, project.code)){
 				MongoDBDAO.deleteByCode(InstanceConstants.PROJECT_COLL_NAME, Project.class, project.code);
 				//Logger.debug("Project to create :"+project.code);
 			}
 		}
-		
 		List<Project> projs=InstanceHelpers.save(InstanceConstants.PROJECT_COLL_NAME,projects,contextError);
 		return projs;
 	}
 	
-	public void testTri() {
-		List<Sample> samples = limsServices.testTri();
-		for (Sample sample : samples) {
-			Logger.info("Sample created :"+ sample.code);
-			if (sample.projectCodes != null) {
-				Logger.info("Nb Projects for the sample :"+ sample.projectCodes.size());
-			}
-			else {
-				Logger.info("No projects associated with the sample"); 
-			}
-				
-		}
-	}
-	
+
 	public static void deleteSamplesFromLims() throws SQLException, DAOException{
 		List<Sample> samples = limsServices.findSamplesToCreate(contextError) ;
 		for(Sample sample:samples){
@@ -100,19 +84,27 @@ public class ImportDataRun implements Runnable {
 	
 	
 	public static List<Sample> createSamplesFromLims() throws SQLException, DAOException{
-
 		List<Sample> samples = limsServices.findSamplesToCreate(contextError) ;
-		
 		for(Sample sample:samples){
-			
 			if(MongoDBDAO.checkObjectExistByCode(InstanceConstants.SAMPLE_COLL_NAME, Sample.class, sample.code)){
 				MongoDBDAO.deleteByCode(InstanceConstants.SAMPLE_COLL_NAME, Sample.class, sample.code);
 				//Logger.debug("Sample to create :"+sample.code);
 			}
 		}
-		
 		List<Sample> samps=InstanceHelpers.save(InstanceConstants.SAMPLE_COLL_NAME, samples, contextError);
 		return samps;
+	}
+	
+	public static List<Container> createContainersFromLims() throws SQLException, DAOException{
+		List<Container> containers = limsServices.findContainersToCreate(contextError) ;
+		for(Container container:containers){
+			if(MongoDBDAO.checkObjectExistByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, container.code)){
+				MongoDBDAO.deleteByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, container.code);
+				//Logger.debug("Container to create :"+container.code);
+			}
+		}
+		List<Container> ctrs=InstanceHelpers.save(InstanceConstants.SAMPLE_COLL_NAME, containers, contextError);
+		return ctrs;
 	}
 	
 
