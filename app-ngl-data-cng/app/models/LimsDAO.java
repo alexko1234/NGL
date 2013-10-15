e532fe168546d34f60eced1a85f5ae4be850a6aa
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-15 14:38:07 +0000

    dnoisett use common function
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1589 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data-cng/app/models/LimsDAO.java
+++ app-ngl-data-cng/app/models/LimsDAO.java
@@ -16,6 +16,7 @@ import models.laboratory.common.description.MeasureUnit;
 import models.laboratory.common.description.PropertyDefinition;
 import models.laboratory.common.instance.Comment;
 import models.laboratory.common.instance.PropertyValue;
+import models.laboratory.common.instance.TBoolean;
 import models.laboratory.common.instance.property.PropertySingleValue;
 import models.laboratory.container.instance.Container;
 import models.laboratory.container.instance.ContainerSupport;
@@ -27,7 +28,7 @@ import models.laboratory.sample.instance.Sample;
 import models.utils.InstanceHelpers;
 import models.utils.ListObject;
 import models.utils.dao.DAOException;
-import models.utils.instance.ContainerHelper;
+import models.utils.instance.ContainerSupportHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -102,7 +103,7 @@ public class LimsDAO {
 				InstanceHelpers.updateTraceInformation(project.traceInformation);
 				
 				project.comments = new ArrayList<Comment>(); 
-				
+				// just one comment for one project
 				InstanceHelpers.addComment(rs.getString("comments"), project.comments);
 				
 				return project;
@@ -128,8 +129,9 @@ public class LimsDAO {
 				sample.traceInformation.setTraceInformation(InstanceHelpers.getUser());
 
 				sample.code=rs.getString("code"); //barcode
+				//Logger.debug("Sample code :"+sample.code);
 				
-				String sampleTypeCode=getSampleTypeFromLims();
+				String sampleTypeCode=SAMPLE_TYPE_CODE_DEFAULT;
 				//Logger.debug("Sample Type :"+sampleTypeCode);
 
 				if(sampleTypeCode==null){
@@ -150,6 +152,7 @@ public class LimsDAO {
 				}
 				
 				sample.typeCode=sampleTypeCode;
+				sample.categoryCode=sampleType.category.code;
 			
 				sample.projectCodes=new ArrayList<String>();
 				sample.projectCodes.add(rs.getString("project_code")); // jointure t_project.name
@@ -160,24 +163,13 @@ public class LimsDAO {
 
 				sample.comments=new ArrayList<Comment>(); // comments
 				sample.comments.add(new Comment(rs.getString("comments")));
-				
-				sample.categoryCode=sampleType.category.code;
 
 				//pb : column with different types (not only varchar) !
-				
 				//for(PropertyDefinition propertyDefinition :sampleType.propertiesDefinitions) {
-					
 				//	String code=null;
 				//	try{
 				//		value=rs.getString(propertyDefinition.code.toLowerCase());
-						
-				//		Logger.info("code of property : " + code);
-						/*
-						propertyDefinitions.add(newPropertiesDefinition("Taille associée au taxon", "taxonSize", LevelService.getLevels(Level.CODE.Content,Level.CODE.Sample),Double.class, true,MeasureCategory.find.findByCode(MeasureService.MEASURE_CAT_CODE_SIZE), MeasureUnit.find.findByCode("Mb"), MeasureUnit.find.findByCode("Mb")));
-						propertyDefinitions.add(newPropertiesDefinition("Fragmenté", "isFragmented", LevelService.getLevels(Level.CODE.Sample),Boolean.class, true));
-						propertyDefinitions.add(newPropertiesDefinition("Adaptateurs", "isAdapters", LevelService.getLevels(Level.CODE.Sample),Boolean.class, true));
-						propertyDefinitions.add(newPropertiesDefinition("Code LIMS", "limsCode", LevelService.getLevels(Level.CODE.Sample),Integer.class, false));
-						*/
+				//		Logger.info("code of property : " + value);
 						
 						if(sample.properties==null){ 
 							sample.properties=new HashMap<String, PropertyValue>();
@@ -204,138 +196,26 @@ public class LimsDAO {
 		/// required to have an ordered list (see ORDER BY clause in the sql of the view)
 		int pos = 0;
 		int x=1;
-		int posNext = 0;
 		int listSize  =  results.size(); 
 		while (pos < listSize-1    )   {
-			posNext = pos+x;
-			while (  results.get(pos).code.equals( results.get(posNext).code ) ) {
+			while ( (pos < listSize-1) && (results.get(pos).code.equals( results.get(pos+x).code ))   ) {
 				// difference between the two project codes
-				if (! results.get(pos).projectCodes.get(0).equals(results.get(posNext).projectCodes.get(0))) {
-					results.get(pos).projectCodes.add( results.get(posNext).projectCodes.get(0) ); 
+				if (! results.get(pos).projectCodes.get(0).equals(results.get(pos+x).projectCodes.get(0))) {
+					results.get(pos).projectCodes.add( results.get(pos+x).projectCodes.get(0) ); 
 				}
 				// difference between the two comments
-				if (! results.get(pos).comments.get(0).equals(results.get(posNext).comments.get(0))) {
-					results.get(pos).comments.add( results.get(posNext).comments.get(0) ); 
+				if (! results.get(pos).comments.get(0).equals(results.get(pos+x).comments.get(0))) {
+					results.get(pos).comments.add( results.get(pos+x).comments.get(0) ); 
 				}
 				// all the difference have been reported on the first sample found (at the position pos)
 				// so we can delete the sample at the position (posNext)
-				results.remove(posNext);
+				results.remove(pos+x);
 				listSize--;
 			}
 			pos++;
 		}
-		
 		return results;
 	}
-	
-	
-	
-	public List<Sample> testTri() { 
-		List<Sample> results = new ArrayList<Sample>();
-		
-		Sample s0 = new Sample();
-		s0.code = "c0";
-		s0.projectCodes = null;
-		Sample s1 = new Sample();
-		s1.code = "c1";
-		s1.projectCodes = null;
-		Sample s2 = new Sample();
-		s2.code = "c2";
-		s2.projectCodes=new ArrayList<String>();
-		s2.projectCodes.add("p0");
-		Sample s3 = new Sample();
-		s3.code = "c2";
-		s3.projectCodes=new ArrayList<String>();
-		s3.projectCodes.add("p1");
-		Sample s4 = new Sample();
-		s4.code = "c2";
-		s4.projectCodes=new ArrayList<String>();
-		s4.projectCodes.add("p2");
-		Sample s5 = new Sample();
-		s5.code = "c5";
-		
-		results.add(s0);
-		results.add(s1);
-		results.add(s2);
-		results.add(s3);
-		results.add(s4);
-		results.add(s5);
-	
-		//affect all the project codes to a same sample 
-		/// required to have an ordered list (see ORDER BY clause in the sql of the view)
-		int pos = 0;
-		int x=1;
-		int posNext = 0;
-		int listSize  =  results.size(); 
-		while (pos < listSize-1    )   {
-			System.out.println("pos = "+ pos); 
-			posNext = pos+x;
-			System.out.println("posNext = "+ posNext); 
-			
-			while (  results.get(pos).code.equals( results.get(posNext).code ) ) {
-				// difference between the two project codes
-				if (! results.get(pos).projectCodes.get(0).equals(results.get(posNext).projectCodes.get(0))) {
-					results.get(pos).projectCodes.add( results.get(posNext).projectCodes.get(0) ); 
-				}
-				// difference between the two comments
-				//if (! results.get(pos).comments.get(0).equals(results.get(posNext).comments.get(0))) {
-				//	results.get(pos).comments.add( results.get(posNext).comments.get(0) ); 
-				//}
-				// all the difference have been reported on the first sample found (at the position pos)
-				// so we can delete the sample at the position (posNext)
-				results.remove(posNext);
-				listSize--;
-				System.out.println("listSize = " + listSize); 
-				//x++;
-			}
-			//x=1;
-			pos++;
-			System.out.println("pos = "+ pos); 
-		}
-		return results;
-	}
-	
-	
-	
-	
-	
-	public List<ContainerSupport> findFlowcellsToCreate(final ContextValidation contextError) throws SQLException, DAOException {
-		List<ContainerSupport> results = this.jdbcTemplate.query("select * from v_flowcelltongl;", new Object[]{} 
-		, new RowMapper<ContainerSupport>() {
-
-			@SuppressWarnings("rawtypes")
-			public ContainerSupport mapRow(ResultSet rs, int rowNum) throws SQLException {
-				
-				// String name, String barCode, String categoryCode, String stockCode /* code frigo */, String x, String y
-
-				ContainerSupport cs = new ContainerSupport();
-
-				cs.name= rs.getString("name1").trim();
-				
-				cs.barCode = rs.getString("barcode");
-				
-				cs.categoryCode = "flowcell " + rs.getString("y");
-				
-				cs.stockCode = null;
-				
-				cs.x = "1";
-				
-				cs.y = rs.getString("y");
-				
-				
-				// rs contains "idx" column (index) && "code_sample" (code du sample)
-				
-				return cs;
-			}
-		});
-		
-		return results;
-	}
-
-
-
-
-	
 	
 	
 	
@@ -347,91 +227,129 @@ public class LimsDAO {
 	 */
 	public List<Container> findContainersToCreate(ContextValidation contextError){
 
-		List<Container> results = this.jdbcTemplate.query("select * from v_flowcelltongl;",new Object[]{} 
+		List<Container> results = this.jdbcTemplate.query("select distinct * from v_flowcelltongl;",new Object[]{} 
 		,new RowMapper<Container>() {
 
 			@SuppressWarnings("rawtypes")
 			public Container mapRow(ResultSet rs, int rowNum) throws SQLException {
+				
+				/* flowcell_id, lane_id, name1, code, barcode, y, code_sample, idx*/ 
 
 				Container container = new Container();
-				container.traceInformation.setTraceInformation(InstanceHelpers.getUser());
-				//Logger.debug("Container :"+rs.getString("code"));
+				
 				container.code=String.valueOf(rs.getInt("lane_id"));
+				Logger.debug("Container :"+rs.getString("code"));
 				
 				container.categoryCode=CONTAINER_CATEGORY_CODE;
-				container.projectCodes=new ArrayList<String>();				
-				// a voir
+				
+				container.processTypeCode = null;
+				
+				container.projectCodes=new ArrayList<String>();
+				// TODO : change test value 
 				//container.projectCodes.add(rs.getString("project_code"));
-
+				container.projectCodes.add("UNKNOWN"); 
+				
 				container.sampleCodes=new ArrayList<String>();
 				container.sampleCodes.add(rs.getString("code_sample"));
-
-				container.comments=new ArrayList<Comment>();				
-				//container.comments.add(new Comment(rs.getString("comments")));
-				container.stateCode=CONTAINER_STATE_CODE;
-				container.valid=null;
-
-				container.support=ContainerHelper.getContainerSupportLane(rs.getString("barcode"));
 				
-				container.support.y = rs.getString("y");
+				//container.fromExperimentTypeCodes = new ArrayList<String>(); //not required
+				
+				container.fromPurifingCode = null; // not required
+				
+				
+				container.resolutionCode = null; // not required
+				
+				container.stateCode=CONTAINER_STATE_CODE; // required
+				
+				//define content
+				Content content = new Content();
+				content.sampleUsed=new SampleUsed();
+				content.sampleUsed.sampleCode=rs.getString("code_sample");
+				
+				//TODO : change default value
+				content.sampleUsed.categoryCode = "unknown"; // required
+				content.sampleUsed.typeCode = "unknown"; // required
+				
+				container.contents=new ArrayList<Content>();
+				container.contents.add(content);
+				
+				content.properties = new HashMap<String, PropertyValue>();
+				content.properties.put("index",new PropertySingleValue(rs.getString("idx")));
+				
+				// define container support attributes
+				try {
+					container.support=ContainerSupportHelper.getContainerSupport("lane", rs.getInt("nb_lanes"),rs.getString("barcode"),"1",rs.getString("y")); 
+				}
+				catch(DAOException e) {
+					Logger.info("Can't get container support !"); 
+				}
+				
+				container.traceInformation.setTraceInformation(InstanceHelpers.getUser());
+				
+
+				container.comments=new ArrayList<Comment>();	
+				//just one comment for one lane (container)
+				container.comments.add(new Comment(rs.getString("comments")));
+				
+				
+				
+				// TODO : to verify
+				/*
+				switch (rs.getInt("control")) {
+				case 0: 
+					container.valid= TBoolean.FALSE;
+					break; 
+				case 1: 
+					container.valid= TBoolean.TRUE;
+					break; 
+				case 2: 
+					container.valid= TBoolean.UNSET;
+					break; 
+				default: 
+					container.valid= TBoolean.UNSET;
+					break; 
+				}
+				*/
 
 				container.properties= new HashMap<String, PropertyValue>();
 				container.properties.put(LIMS_CODE,new PropertySingleValue(rs.getInt("lane_id")));
 
-				container.mesuredConcentration=new PropertySingleValue((float) 0); //new PropertySingleValue(rs.getFloat("tubconcr"), "ng/µl");
-				container.mesuredVolume=new PropertySingleValue((float) 0); //new PropertySingleValue(rs.getFloat("tubvolr"), "µl");
-				container.mesuredQuantity=new PropertySingleValue((float) 0); // new PropertySingleValue(rs.getFloat("tubqtr"), "ng");
+				//TODO: get measures;
+				//container.mesuredConcentration=new PropertySingleValue((float) 0);
+				//container.mesuredVolume=new PropertySingleValue((float) 0);
+				//container.mesuredQuantity=new PropertySingleValue((float) 0); 
+				//container.calculedVolume =new PropertySingleValue((float) 0);  
 
-				Content content = new Content();
-				content.sampleUsed=new SampleUsed();
-				content.sampleUsed.sampleCode=rs.getString("code_sample");
-				container.contents=new ArrayList<Content>();
-				container.contents.add(content);
 
-				content.properties = new HashMap<String, PropertyValue>();
-				content.properties.put("index",new PropertySingleValue(rs.getString("idx")));
 			
-
 				return container;
 			}
 
-		});        
-
-		return results;
-	}
-	
-	
-
-	
-
+		});       
 		
-	private String getSampleTypeFromLims() {
-	/*	
-		if(tadnco.equals("15")) return "fosmid";
-		else
-		if(tadnco.equals("8")) return "plasmid";
-		else
-		if(tadnco.equals("2")) return "BAC";
-		else
-		if(tadnco.equals("1") && !tprco.equals("11")) return "gDNA";
-		else
-		if(tadnco.equals("1") && tprco.equals("11")) return "MeTa-DNA";
-		else
-		if(tadnco.equals("19") || tadnco.equals("6")) return "amplicon";
-		else
-		if(tadnco.equals("12")) return "cDNA";
-		else
-		if( tadnco.equals("11")) return "total-RNA";
-		else 
-		if(tadnco.equals("18")) return "sRNA";
-		else
-		if(tadnco.equals("10")) return "mRNA";
-		else
-		if(tadnco.equals("17")) return "chIP";
-		//Logger.debug("Erreur mapping Type materiel ("+tadnco+")/Type projet ("+tprco+") et Sample Type");
-		return null;
-	*/
-		return SAMPLE_TYPE_CODE_DEFAULT; 
+		//affect all the project codes /samples to the first container 
+		/// required to have an ordered list (see ORDER BY clause in the sql of the view)
+		int pos = 0;
+		int x=1;
+		int listSize  =  results.size(); 
+		while (pos < listSize-1    )   {
+			while ( (pos < listSize-1) && (results.get(pos).code.equals( results.get(pos+x).code ))   ) {
+				// difference between the two project codes
+				if (! results.get(pos).projectCodes.get(0).equals(results.get(pos+x).projectCodes.get(0))) {
+					results.get(pos).projectCodes.add( results.get(pos+x).projectCodes.get(0) ); 
+				}
+				// difference between the two comments
+				if (! results.get(pos).sampleCodes.get(0).equals(results.get(pos+x).sampleCodes.get(0))) {
+					results.get(pos).sampleCodes.add( results.get(pos+x).sampleCodes.get(0) ); 
+				}
+				// all the difference have been reported on the first sample found (at the position pos)
+				// so we can delete the sample at the position (posNext)
+				results.remove(pos+x);
+				listSize--;
+			}
+			pos++;
+		}
+		return results;
 	}
 
 
