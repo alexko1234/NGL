e6bbc10f2e51a5df3c8c1a42a7821c807789a425
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-23 12:00:44 +0000

    new management of states
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1642 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/common/description/ObjectType.java
+++ lib-ngl-common/app/models/laboratory/common/description/ObjectType.java
@@ -1,16 +1,21 @@
 package models.laboratory.common.description;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import models.laboratory.common.description.dao.ObjectTypeDAO;
 import models.utils.Model;
 
 /**
  * Type definition
  * @author ejacoby
- *
+ * dnoisett : add attribute List<State> states+ update codes
  */
 public class ObjectType extends Model<ObjectType>{
 	
-	public enum CODE {Project,Process,Sample,Instrument,Reagent,Experiment,Import, Run, Treatment, ReadSet}; 
+	public List<State> states = new ArrayList<State>();
+	
+	public enum CODE {Project, Process, Sample, Instrument, Container, Reagent, Experiment, Import, Run, Treatment, ReadSet, Lane, File}; 
 	
 	public static Finder<ObjectType> find = new Finder<ObjectType>(ObjectTypeDAO.class.getName()); 
 
