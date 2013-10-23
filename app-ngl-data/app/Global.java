a1b20ed1b349a0dabca918f8bc33925f2f103af6
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-23 13:15:39 +0000

    merge
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1662 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/Global.java
+++ app-ngl-data/app/Global.java
@@ -1,3 +1,4 @@
+<<<<<<< .mine
 
 import java.util.concurrent.TimeUnit;
 
@@ -57,4 +58,65 @@ public class Global extends GlobalSettings {
 	                ? next.plusHours(24)
 	                : next;
 	    }
+=======
+
+import java.util.concurrent.TimeUnit;
+
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.libs.Akka;
+import scala.concurrent.duration.Duration;
+import services.instance.ImportDataCNS;
+import services.instance.ImportDataFactory;
+
+public class Global extends GlobalSettings {
+
+	@Override
+	public void onStart(Application app) {
+		Logger.info("NGL has started");
+		
+		/*Akka.system().scheduler().schedule(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES)
+                , (new ImportDataFactory()).getImportData(), Akka.system().dispatcher()
+				); 
+		
+		
+		Akka.system().scheduler().schedule(
+
+				Duration.create(nextExecutionInSeconds(22,13), TimeUnit.SECONDS),
+                Duration.create(24, TimeUnit.HOURS)
+                , new ImportDataRun(), Akka.system().dispatcher()
+
+				);*/ 
+	}
+
+	@Override
+	public void onStop(Application app) {
+		Logger.info("NGL shutdown...");
+	}
+
+	
+	
+	 public static int nextExecutionInSeconds(int hour, int minute){
+	        return Seconds.secondsBetween(
+	                new DateTime(),
+	                nextExecution(hour, minute)
+	        ).getSeconds();
+	    }
+
+	    public static DateTime nextExecution(int hour, int minute){
+	        DateTime next = new DateTime()
+	                .withHourOfDay(hour)
+	                .withMinuteOfHour(minute)
+	                .withSecondOfMinute(0)
+	                .withMillisOfSecond(0);
+
+	        return (next.isBeforeNow())
+	                ? next.plusHours(24)
+	                : next;
+	    }
+>>>>>>> .r1660
 }
