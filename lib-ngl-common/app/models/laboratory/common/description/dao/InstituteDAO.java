a1db991ddc14878398499887ba2efc3aa1d67725
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-24 08:47:48 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1671 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ lib-ngl-common/app/models/laboratory/common/description/dao/InstituteDAO.java
@@ -0,0 +1,61 @@
+package models.laboratory.common.description.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.laboratory.common.description.Institute;
+import models.utils.dao.AbstractDAOMapping;
+import models.utils.dao.DAOException;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InstituteDAO extends AbstractDAOMapping<Institute>{
+
+	protected InstituteDAO() {
+		super("institute", Institute.class, InstituteMappingQuery.class,
+				"SELECT t.id, t.name, t.code FROM institute as t ",
+				true);
+	}
+	
+	@Override
+	public void remove(Institute institute) throws DAOException {
+		//Remove list institute for common_info_type
+		String sqlState = "DELETE FROM common_info_type_institute WHERE fk_institute=?";
+		jdbcTemplate.update(sqlState, institute.id);
+		//remove institute itself
+		super.remove(institute);
+	}
+
+	@Override
+	public long save(Institute institute) throws DAOException {
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("code", institute.code);
+		parameters.put("name", institute.name);
+
+		Long newId = (Long) jdbcInsert.executeAndReturnKey(parameters);
+		institute.id = newId;
+		return institute.id;
+	}
+
+	@Override
+	public void update(Institute institute) throws DAOException {
+		String sql = "UPDATE institute SET code=?, name=? WHERE id=?";
+		jdbcTemplate.update(sql, institute.code, institute.name);
+		
+	}
+	
+	public List<Institute> findByCommonInfoType(long idCommonInfoType) {
+		String sql = "SELECT i.id, i.name, i.code "+
+				"FROM institute i "+
+				"JOIN common_info_type_institute ci ON fk_institute= i.id "+
+				"WHERE ci.fk_common_info_type=?";
+		BeanPropertyRowMapper<Institute> mapper = new BeanPropertyRowMapper<Institute>(Institute.class);
+		return this.jdbcTemplate.query(sql, mapper, idCommonInfoType);
+	}
+
+
+}
+
