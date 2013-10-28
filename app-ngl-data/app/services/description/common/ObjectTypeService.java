de4009cded54944deb80a8e69a98a07e4530e113
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-10-28 17:25:23 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1718 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/description/common/ObjectTypeService.java
+++ app-ngl-data/app/services/description/common/ObjectTypeService.java
@@ -1,7 +1,6 @@
 package services.description.common;
 
-import static services.description.DescriptionFactory.newObjectType;
-import static services.description.DescriptionFactory.setStatesToObjectType;
+import services.description.DescriptionFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,21 +27,21 @@ public class ObjectTypeService {
 	public static void saveObjectTypes(Map<String,List<ValidationError>> errors) throws DAOException{
 		List<ObjectType> l = new ArrayList<ObjectType>();
 		
-		l.add(newObjectType(ObjectType.CODE.Container.name(), ObjectType.CODE.Container.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "N", "IW-P", "IW-E", "IU","IS" ) ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Container.name(), ObjectType.CODE.Container.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "N", "IW-P", "IW-E", "IU","IS" ) ));
 		
-		l.add(newObjectType(ObjectType.CODE.Project.name(), ObjectType.CODE.Project.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Project.name(), ObjectType.CODE.Project.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(newObjectType(ObjectType.CODE.Experiment.name(), ObjectType.CODE.Experiment.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Experiment.name(), ObjectType.CODE.Experiment.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(newObjectType(ObjectType.CODE.Process.name(), ObjectType.CODE.Process.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Process.name(), ObjectType.CODE.Process.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(newObjectType(ObjectType.CODE.Run.name(), ObjectType.CODE.Run.name(), getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Run.name(), ObjectType.CODE.Run.name(), DescriptionFactory.getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
 		
-		l.add(newObjectType(ObjectType.CODE.Lane.name(), ObjectType.CODE.Lane.name(), getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.Lane.name(), ObjectType.CODE.Lane.name(), DescriptionFactory.getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
 		
-		l.add(newObjectType(ObjectType.CODE.ReadSet.name(), ObjectType.CODE.ReadSet.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.ReadSet.name(), ObjectType.CODE.ReadSet.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
 		
-		l.add(newObjectType(ObjectType.CODE.File.name(), ObjectType.CODE.File.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.newObjectType(ObjectType.CODE.File.name(), ObjectType.CODE.File.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
 		
 		
 		DAOHelpers.saveModels(ObjectType.class, l, errors);
@@ -53,45 +52,38 @@ public class ObjectTypeService {
 	public static void updateStatesObjectTypes(Map<String,List<ValidationError>> errors) throws DAOException{
 		List<ObjectType> l = new ArrayList<ObjectType>();
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Container.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "N", "IW-P", "IW-E", "IU","IS" ) ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Container.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "N", "IW-P", "IW-E", "IU","IS" ) ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Project.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Project.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Experiment.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Experiment.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Process.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Process.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Run.name(), getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Run.name(), DescriptionFactory.getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Lane.name(), getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Lane.name(), DescriptionFactory.getStates("F", "E","IW-V", "IP-V", "F-V", "IP-S", "IP-RG", "F-RG") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.ReadSet.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.ReadSet.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.File.name(), getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.File.name(), DescriptionFactory.getStates("A","UA", "IW-QC", "IP-QC", "F-QC", "IW-V", "IP-V", "F-V", "IP-RG", "F-RG") ));
 		
 		//default values
-		l.add(setStatesToObjectType(ObjectType.CODE.Sample.name(), getStates("F", "N", "IP") ));
+		//TODO : define values
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Sample.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Instrument.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Instrument.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Reagent.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Reagent.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Import.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Import.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
-		l.add(setStatesToObjectType(ObjectType.CODE.Treatment.name(), getStates("F", "N", "IP") ));
+		l.add(DescriptionFactory.setStatesToObjectType(ObjectType.CODE.Treatment.name(), DescriptionFactory.getStates("F", "N", "IP") ));
 		
 		
 		DAOHelpers.updateModels(ObjectType.class, l, errors);	
 	}
 	
-	
-	private static List<State> getStates(String...codes) throws DAOException {
-		List<State> states = new ArrayList<State>();
-		for(String code: codes){
-			states.add(State.find.findByCode(code));
-		}
-		return states;
-	}
 	
 
 
