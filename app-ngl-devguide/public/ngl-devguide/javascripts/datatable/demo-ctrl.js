f0862edf9418cb62344cb98906f663ab14b97dc1
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-11-05 15:37:29 +0000

    ADD: Multi select (listStyle:"multiselect")
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1766 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-devguide/public/ngl-devguide/javascripts/datatable/demo-ctrl.js
+++ app-ngl-devguide/public/ngl-devguide/javascripts/datatable/demo-ctrl.js
@@ -2,45 +2,117 @@
 
 function DemoCtrl($scope,datatable) {
 
-	
 	$scope.datatableConfig = {
 			columns : [{	header:"Code", //the title
 							property:"code", //the property to bind
-							id:'test', //the column id
-							edit:false, //can be edited or not
+							edit:true, //can be edited or not
 							hide:true, //can be hidden or not
 							order:true, //can be ordered or not
 							type:"String", //the column type
 							choiceInList:false, //when the column is in edit mode, the edition is a list of choices or not
 							extraHeaders:{"0":"Inputs","1":"prop2"}, //the extraHeaders list
 						},
-						{	header:"test 1", //the title
-							property:"test", //the property to bind
-							id:'test2', //the column id
-							edit:false, //can be edited or not
+						{	header:"Etat", //the title
+							property:"etat", //the property to bind
+							edit:true, //can be edited or not
+							hide:true, //can be hidden or not
+							order:true, //can be ordered or not
+							type:"String", //the column type
+							choiceInList:true, //when the column is in edit mode, the edition is a list of choices or not
+							possibleValues:[{"code":"N","name":"Nouveau"},{"code":"IWP","name":"En attente de processus"},{"code":"IWE","name":"En attente d'experience"}],
+							extraHeaders:{"0":"Inputs","1":"prop"}, //the extraHeaders list
+						},{	header:"Volume", //the title
+							property:"volume", //the property to bind
+							edit:true, //can be edited or not
 							hide:true, //can be hidden or not
 							order:true, //can be ordered or not
 							type:"String", //the column type
 							choiceInList:false, //when the column is in edit mode, the edition is a list of choices or not
 							extraHeaders:{"0":"Inputs","1":"prop"}, //the extraHeaders list
 						}],
-			compact:false,
+			compact:true,
 			pagination:{
-				active:true
+				active:false,
+				mode:'local'
 			},		
 			search:{
 				url:"/datatable/get-examples"
 			},
 			order:{
 				mode:'local', //or 
-				active:true,
+				active:false,
 				by:'ContainerInputCode'
 			},
 			remove:{
-				active:false,
+				active:true,
 			},
 			save:{
+				active:false,
+				mode:'local',
+			},
+			hide:{
+				active:true
+			},
+			edit:{
+				active:false
+			},
+			messages:{
+				active:true
+			},
+			extraHeaders:{
+				number:2,
+				dynamic:true,
+			},
+			name:"datatable"
+	};
+	
+	$scope.datatableConfig2 = {
+			columns : [{	header:"Code", //the title
+							property:"code", //the property to bind
+							edit:true, //can be edited or not
+							hide:true, //can be hidden or not
+							order:true, //can be ordered or not
+							type:"String", //the column type
+							choiceInList:false, //when the column is in edit mode, the edition is a list of choices or not
+							extraHeaders:{"0":"Inputs","1":"prop2"}, //the extraHeaders list
+						},
+						{	header:"Etat", //the title
+							property:"etat", //the property to bind
+							edit:true, //can be edited or not
+							hide:true, //can be hidden or not
+							order:true, //can be ordered or not
+							type:"String", //the column type
+							choiceInList:true, //when the column is in edit mode, the edition is a list of choices or not
+							listStyle:"multiselect",
+							possibleValues:[{"code":"N","name":"Nouveau"},{"code":"IWP","name":"En attente de processus"},{"code":"IWE","name":"En attente d'experience"}],
+							extraHeaders:{"0":"Inputs","1":"prop"}, //the extraHeaders list
+						},{	header:"Volume", //the title
+							property:"volume", //the property to bind
+							edit:true, //can be edited or not
+							hide:true, //can be hidden or not
+							order:true, //can be ordered or not
+							type:"String", //the column type
+							choiceInList:false, //when the column is in edit mode, the edition is a list of choices or not
+							extraHeaders:{"0":"Inputs","1":"prop"}, //the extraHeaders list
+						}],
+			compact:true,
+			pagination:{
+				active:false,
+				mode:'local'
+			},		
+			search:{
+				url:"/datatable/get-examples"
+			},
+			order:{
+				mode:'local', //or 
+				active:false,
+				by:'ContainerInputCode'
+			},
+			remove:{
 				active:true,
+			},
+			save:{
+				active:false,
 				mode:'local',
 			},
 			hide:{
@@ -56,13 +128,13 @@ function DemoCtrl($scope,datatable) {
 				number:2,
 				dynamic:true,
 			},
-			name:"datatable"
+			name:"datatable2"
 	};
-
 	
 	$scope.init = function(){
 		$scope.datatable = new datatable($scope, $scope.datatableConfig);
-		$scope.config = JSON.stringify($scope.datatable.config);
+		$scope.datatable2 = new datatable($scope, $scope.datatableConfig2);
+		//$scope.config = JSON.stringify($scope.datatable.config);
 	};
 	
 	
@@ -76,6 +148,7 @@ function DemoCtrl($scope,datatable) {
 	
 	$scope.search = function(){
 		$scope.datatable.search();
+		$scope.datatable2.search();
 		$scope.config = JSON.stringify($scope.datatable.config);
 	};
 	
