3fd6e2eae13adc505c174224b9ab044e3e6328b1
Guillaume Albini <galbini@genoscope.cns.fr>
2013-11-18 15:12:15 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1919 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ lib-ngl-common/public/ngl-common/javascripts/services/messages.js
@@ -0,0 +1,104 @@
+"use strict";
+
+angular.module('messagesServices', []).
+    	factory('messages', function(){ //service to manage datatable
+    		var constructor = function($scope, iConfig){
+				var messages = {
+						
+						configDefault : {
+							errorClass:'alert alert-error',
+							successClass: 'alert alert-success',							
+							errorKey:{save:'msg.error.save',remove:'msg.error.remove'},
+							successKey:{save:'msg.success.save',remove:'msg.success.remove'}
+						},
+						config:undefined,
+    					configMaster:undefined,
+						
+						clazz : undefined, 
+						text : undefined, 
+						showDetails : false, 
+						isDetails : false, 
+						details : [],
+						opening : false,
+						getConfig: function(){
+		    				return this.config;		    				
+		    			},
+		    			setConfig: function(config){
+		    				var settings = $.extend(true, {}, this.configDefault, config);
+		    	    		this.config = angular.copy(settings);
+		    	    		this.configMaster = angular.copy(settings);		    	    		
+		    			},
+						clear : function() {
+							this.clazz = undefined;
+							this.text = undefined; 
+							this.showDetails = false; 
+							this.isDetails = false;
+							this.details = [];
+							this.opening = false;
+						},
+						setDetails : function(details){
+							this.isDetails = true;
+							this.details = details;
+						},
+						setSuccess : function(type){
+							this.clazz=this.config.successClass;
+							this.text=this.transformKey(this.config.successKey[type]);
+							this.open();
+						},
+						setError : function(type){
+							this.clazz=this.config.errorClass;
+							this.text=this.transformKey(this.config.errorKey[type]);
+							
+							this.open();
+						},
+						open : function(){
+							this.opening = true;
+						},
+						close : function(){
+							this.opening = false;
+						},
+						isOpen : function(){
+							return this.opening;
+						},
+						transformKey : function(key, args){
+							return Messages(key, args);
+						}
+				};
+    			messages.setConfig(iConfig)
+				return messages;
+    		}
+    		return constructor;
+    	}).directive('messages', function() {
+    		return {
+    			restrict: 'A',
+    			scope: {
+    				  messages: '=messages'
+    				},
+    			template: '<div ng-class="messages.clazz" ng-show="messages.isOpen()">'+
+    				'<button class="close" ng-click="messages.close()" type="button">×</button>'+
+    				'<strong>{{messages.text}}</strong><button class="btn btn-link" ng-click="messages.showDetails=!messages.showDetails" ng-show="messages.isDetails">{{messages.transformKey("msg.details")}}</button>'+
+    				'<div ng-show="messages.showDetails">'+
+    				'    <ul>'+
+    				'		<li ng-repeat="(key1, value1) in messages.details">{{key1}}'+
+    				'		<ul>'+
+    				'			<li ng-repeat="(key2, value2) in value1"> {{key2}} == {{value2}} </li>'+
+    			    '		</ul>'+
+    			    '		</li>'+
+    			    '	</ul>'	+
+    			    '</div>'+
+    			    '</div>'
+    			};
+    	}).directive('codes', function() {
+    		return {
+    			restrict: 'A',
+    			require: 'ngModel',
+    			link: function(scope, element, attrs, ngModel) {
+    				 if(!ngModel) return;
+    				 var type = attrs.codes;
+    				 ngModel.$render = function() {
+    					 element.html(Codes(type+"."+ngModel.$viewValue));
+    				 };
+    			}    					
+    			};
+    				
+    	});
