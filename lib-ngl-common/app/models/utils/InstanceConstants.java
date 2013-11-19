f1f72d11686d3d7a6fcd3c280fce529dae12d08e
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-11-19 11:53:12 +0000

    dnoisett, rename collection SAMPLE_COLL_NAME
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1933 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/utils/InstanceConstants.java
+++ lib-ngl-common/app/models/utils/InstanceConstants.java
@@ -6,7 +6,7 @@ public interface InstanceConstants {
 	public static final String CONTAINER_COLL_NAME = "ngl_sq.Container";
 	public static final String PROCESS_COLL_NAME = "ngl_sq.Process";
 	public static final String EXPERIMENT_COLL_NAME = "ngl_sq.Experiment";
-	public static final String SAMPLE_COLL_NAME = "ngl_sq.Sample";
+	public static final String SAMPLE_COLL_NAME = "ngl_bq.Sample";
 	public static final String STOCK_COLL_NAME = "ngl_sq.Stock";
 	public static final String REAGENT_INSTANCE_COLL_NAME = "ngl_sq.ReagentInstance";
 	
