762bc27cbd6c4b74366f21b692e65da0eca3a98f
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-11-20 14:46:28 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@1982 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/ImportDataCNG.java
+++ app-ngl-data/app/services/instance/ImportDataCNG.java
@@ -38,16 +38,16 @@ public class ImportDataCNG extends AbstractImportData {
 			Logger.info(" Import Projects ... ");
 			createProjectsFromLims(contextError);
 			Logger.info("End Import Projects !");
-			*/
+			
 			Logger.info(" Import Samples ... ");
 			//createSamplesFromLims(contextError, "26136024");
 			createSamplesFromLims(contextError, null);
 			Logger.info("End Import Samples !");
-			/*
+			*/
 			Logger.info(" Import Containers ... ");
 			createContainersFromLims(contextError);
 			Logger.info("End Import Containers !");
-			*/ 
+			 
 		}catch (Exception e) {
 			Logger.debug("",e);
 		}
