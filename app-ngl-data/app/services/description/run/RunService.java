f9f4e794335a8f03485cc0e38d34891c048595fb
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-11-22 10:24:27 +0000

    dnoisett, new version of the RunService for manage validation criteria.
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2031 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/description/run/RunService.java
+++ app-ngl-data/app/services/description/run/RunService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import models.laboratory.common.description.Institute;
 import models.laboratory.common.description.Level;
 import models.laboratory.common.description.PropertyDefinition;
+import models.laboratory.common.description.ValidationCriteria;
 import models.laboratory.run.description.ReadSetType;
 import models.laboratory.run.description.RunCategory;
 import models.laboratory.run.description.RunType;
@@ -18,14 +19,16 @@ import services.description.common.LevelService;
 
 
 public class RunService {
-
-
 	
 	public static void main(Map<String, List<ValidationError>> errors)  throws DAOException{
+		
+		DAOHelpers.removeAll(ValidationCriteria.class, ValidationCriteria.find);
 		
 		DAOHelpers.removeAll(ReadSetType.class, ReadSetType.find);
 		DAOHelpers.removeAll(RunType.class, RunType.find);
 		DAOHelpers.removeAll(RunCategory.class, RunCategory.find);
+		
+		saveValidationCriteria(errors);
 		
 		saveReadSetType(errors);
 		saveRunCategories(errors);
@@ -33,10 +36,18 @@ public class RunService {
 	}
 	
 	
+	public static void saveValidationCriteria(Map<String, List<ValidationError>> errors) throws DAOException {
+		List<ValidationCriteria> l = new ArrayList<ValidationCriteria>();
+		l.add(DescriptionFactory.newValidationCriteria("Default", "default-criteria",  "specDefaultCriteriaCNGforSAVQualityControl_1" ));
+		l.add(DescriptionFactory.newValidationCriteria("High", "criteria-high",  "specDefaultCriteriaCNGforSAVQualityControl_2" ));
+		l.add(DescriptionFactory.newValidationCriteria("Low", "criteria-low",  "specDefaultCriteriaCNGforSAVQualityControl_3" ));
+		DAOHelpers.saveModels(ValidationCriteria.class, l, errors);
+	}
+	
 	
 	public static void saveReadSetType(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<ReadSetType> l = new ArrayList<ReadSetType>();
-		l.add(DescriptionFactory.newReadSetType("Default","default-readset",  getReadSetPropertyDefinitions(),  DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
+		l.add(DescriptionFactory.newReadSetType("Default","default-readset",  getReadSetPropertyDefinitions(), getValidationCriterias("default-criteria"), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
 		DAOHelpers.saveModels(ReadSetType.class, l, errors);
 	}
 
@@ -50,9 +61,9 @@ public class RunService {
 	
 	public static void saveRunType(Map<String, List<ValidationError>> errors) throws DAOException {
 		List<RunType> l = new ArrayList<RunType>();
-		l.add(DescriptionFactory.newRunType("RHS2000","RHS2000", 8, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
-		l.add(DescriptionFactory.newRunType("RHS2500","RHS2500", 8, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
-		l.add(DescriptionFactory.newRunType("RHS2500R","RHS2500R", 2, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS)));
+		l.add(DescriptionFactory.newRunType("RHS2000","RHS2000", 8, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), getValidationCriterias("default-criteria","criteria-high","criteria-low"), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
+		l.add(DescriptionFactory.newRunType("RHS2500","RHS2500", 8, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), getValidationCriterias("default-criteria"), DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS) ));
+		l.add(DescriptionFactory.newRunType("RHS2500R","RHS2500R", 2, RunCategory.find.findByCode("illumina"), getRunPropertyDefinitions(), getValidationCriterias("default-criteria"),  DescriptionFactory.getInstitutes(Institute.CODE.CNG, Institute.CODE.CNS)));
 		DAOHelpers.saveModels(RunType.class, l, errors);
 	}
 	
@@ -68,6 +79,16 @@ public class RunService {
 		return propertyDefinitions;
 	}
 	
+	
+	private static List<ValidationCriteria> getValidationCriterias(String...codes) throws DAOException {
+		List<ValidationCriteria> lvc = new ArrayList<ValidationCriteria>();
+		ValidationCriteria vc = null;
+		for(String code : codes){
+			vc = DAOHelpers.getModelByCode(ValidationCriteria.class, ValidationCriteria.find, code);
+			lvc.add(vc);
+		}	
+		return lvc;
+	}
 
 	
 }
