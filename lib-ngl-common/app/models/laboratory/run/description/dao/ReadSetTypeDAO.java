604f1c2033284c39db4448b0e4749458e9406dbc
Didier Noisette <dnoisett@genoscope.cns.fr>
2013-11-22 10:16:49 +0000

    dnoisett, new version for ReadSetTypeDAO
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2026 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/run/description/dao/ReadSetTypeDAO.java
+++ lib-ngl-common/app/models/laboratory/run/description/dao/ReadSetTypeDAO.java
@@ -1,13 +1,16 @@
 package models.laboratory.run.description.dao;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import models.laboratory.common.description.ValidationCriteria;
 import models.laboratory.common.description.dao.CommonInfoTypeDAO;
 import models.laboratory.run.description.ReadSetType;
 import models.utils.dao.AbstractDAOCommonInfoType;
 import models.utils.dao.DAOException;
 
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import play.api.modules.spring.Spring;
@@ -32,6 +35,11 @@ public class ReadSetTypeDAO extends AbstractDAOCommonInfoType<ReadSetType>{
 		//	throw new DAOException("ReadSetCategory is not present !!");
 		//}
 		
+		//Check if criteria exist
+		if (null == readSetType.criterias || readSetType.criterias.size()==0) {
+			throw new DAOException("Criteria doesn't exist or criteria.id is null) !! - "+readSetType.code);
+		}
+		
 		//Add commonInfoType
 		CommonInfoTypeDAO commonInfoTypeDAO = Spring.getBeanOfType(CommonInfoTypeDAO.class);
 		readSetType.id = commonInfoTypeDAO.save(readSetType);
@@ -41,19 +49,61 @@ public class ReadSetTypeDAO extends AbstractDAOCommonInfoType<ReadSetType>{
 		parameters.put("id", readSetType.id);
 		parameters.put("fk_common_info_type", readSetType.id);
 		//parameters.put("fk_readset_category", readSetType.category.id);
+		
 		jdbcInsert.execute(parameters);
+		
+		//Add criterias
+		insertValidationCriterias(readSetType.criterias, readSetType.id, false);
+		
 		return readSetType.id;
+	}
+	
+	private void insertValidationCriterias(List<ValidationCriteria> criterias, Long id, boolean deleteBefore) throws DAOException {
+		if (deleteBefore) {
+			removeValidationCriterias(id);
+		}
+		//Add resolutions list		
+		if (criterias!=null && criterias.size()>0) {
+
+			Map<String, Object> parameters = null;
+			
+			for (ValidationCriteria criteria : criterias) {
+				if (criteria == null) {
+					throw new DAOException("criteria is mandatory");
+				} else {
+					parameters = new HashMap<String, Object>();
+					parameters.put("fk_readset_type", id);
+					parameters.put("fk_validation_criteria", criteria.id);
+					// set the table name to the name of the link table
+					 SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(dataSource)
+			         .withTableName("validation_criteria_readset");
+					jdbcInsert.execute(parameters);
+				}				
+			}
+		} else {
+			throw new DAOException("criterias null or empty");
+		}
+	}
+	
+	private void removeValidationCriterias(Long id)  throws DAOException {
+		String sql = "DELETE FROM validation_criteria_readset WHERE fk_readset_type=?";
+		jdbcTemplate.update(sql, id);
 	}
 
 	@Override
-	public void update(ReadSetType readSetType) throws DAOException
-	{
+	public void update(ReadSetType readSetType) throws DAOException {
+		//Update criterias
+		insertValidationCriterias(readSetType.criterias, readSetType.id, true);
+		
 		CommonInfoTypeDAO commonInfoTypeDAO = Spring.getBeanOfType(CommonInfoTypeDAO.class);
 		commonInfoTypeDAO.update(readSetType);
 	}
 
 	@Override
 	public void remove(ReadSetType readSetType) throws DAOException {
+		//Remove criterias for this readSetType
+		removeValidationCriterias(readSetType.id);
+		//Remove readSetType
 		super.remove(readSetType);
 		//Remove commonInfoType
 		CommonInfoTypeDAO commonInfoTypeDAO = Spring.getBeanOfType(CommonInfoTypeDAO.class);
