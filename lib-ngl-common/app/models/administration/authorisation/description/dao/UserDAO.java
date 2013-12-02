8d9ecf56814a3f58cb7f97b302f4b940e37fe9f7
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2013-12-02 10:41:21 +0000

    Authorization: create user if not exist, set application
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2099 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/administration/authorisation/description/dao/UserDAO.java
+++ lib-ngl-common/app/models/administration/authorisation/description/dao/UserDAO.java
@@ -2,12 +2,16 @@ package models.administration.authorisation.description.dao;
 
 import java.util.List;
 
+import models.administration.authorisation.Application;
 import models.administration.authorisation.Role;
 import models.administration.authorisation.User;
 import models.utils.dao.AbstractDAODefault;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
+
+import play.Logger;
+import play.api.Play;
 
 import fr.cea.ig.authentication.html.IAuthenticate;
 
@@ -29,10 +33,18 @@ public class UserDAO extends AbstractDAODefault<User> implements IAuthenticate{
 	}
 
 	public boolean isUserAccessApplication(String login, String application){
-		String sql = "SELECT a.label "+
-				"FROM user u, application a, user_application ua WHERE u.login='"+login+"' AND a.code='"+application+"' AND ua.user_id=u.id and ua.application_id=a.id";
-		BeanPropertyRowMapper<User> mapper = new BeanPropertyRowMapper<User>(User.class);
-		return this.jdbcTemplate.query(sql, mapper).size()>0;
+		if(getUserId(login) != 0){
+			applicationAccess(login, application);
+			
+			String sql = "SELECT a.label "+
+					"FROM user u, application a, user_application ua WHERE u.login='"+login+"' AND a.code='"+application+"' AND ua.user_id=u.id and ua.application_id=a.id";
+			BeanPropertyRowMapper<User> mapper = new BeanPropertyRowMapper<User>(User.class);
+			return this.jdbcTemplate.query(sql, mapper).size()>0;
+		}else{
+			createUser(login);
+			applicationAccess(login, application);
+			return true;
+		}
 	}
 	
 	public void setDefaultRole(String login, String role){
@@ -51,12 +63,36 @@ public class UserDAO extends AbstractDAODefault<User> implements IAuthenticate{
 		return this.jdbcTemplate.query(sql, mapper,label).get(0).id;
 	}
 	
+	private int getApplicationId(String code){
+		String sql = "SELECT a.id FROM application a WHERE a.code=?";
+		BeanPropertyRowMapper<Role> mapper = new BeanPropertyRowMapper<Role>(Role.class);
+		return this.jdbcTemplate.query(sql, mapper,code).get(0).id;
+	}
+	
 	private int getUserId(String login){
 		String sql = "SELECT u.id FROM user u WHERE u.login=?";
 		BeanPropertyRowMapper<User> mapper = new BeanPropertyRowMapper<User>(User.class);
-		return this.jdbcTemplate.query(sql, mapper,login).get(0).id;
+		List<User> users = this.jdbcTemplate.query(sql, mapper,login);
+		if(users != null && users.size() > 0){
+			return this.jdbcTemplate.query(sql, mapper,login).get(0).id;
+		}else{
+			return 0;
+		}
 	}
 	
-
+	private void applicationAccess(String login,String application){
+		String sql = "SELECT u.id FROM user u, user_application ua, application a WHERE u.login=? and u.id=ua.user_id and a.code = ? and a.id=ua.application_id";
+		
+		BeanPropertyRowMapper<User> mapper = new BeanPropertyRowMapper<User>(User.class);
+		if(this.jdbcTemplate.query(sql, mapper,login, application).size() == 0){
+			sql = "INSERT INTO user_application (user_id,application_id) VALUE (?,?)";
+			this.jdbcTemplate.update(sql, getUserId(login), getApplicationId(application));
+		}
+	}
+	
+	private void createUser(String login){
+		String sql = "INSERT INTO user (login) VALUE (?)";
+		this.jdbcTemplate.update(sql, login);
+	}
 	
 }
