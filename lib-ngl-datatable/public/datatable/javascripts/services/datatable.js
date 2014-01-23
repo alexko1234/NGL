d269190a78260739aabe95f1d58f0bd0cb373239
Guillaume Albini <galbini@genoscope.cns.fr>
2014-01-23 10:10:23 +0000

    replace bs-select par bt-select
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2337 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-datatable/public/datatable/javascripts/services/datatable.js
+++ lib-ngl-datatable/public/datatable/javascripts/services/datatable.js
@@ -1365,6 +1365,7 @@ angular.module('datatableServices', []).
         			    		return name+".config.columns."+attrs.index+".property(value)";
         			    	}else{
         			    		return "value."+col.property;
+        			    		
         			    	}
     			    	};
     			    	var columnFormatter = function(col){
@@ -1448,15 +1449,15 @@ angular.module('datatableServices', []).
 	        		  	    			}
 	        		  	    		}else{
 	        		  	    			if(col.listStyle == "radio"){
-	        		  	    				editElement = '<label ng-repeat="opt in col.possibleValues"  for="radio{{col.id}}"><input id="radio{{col.id}}" html-filter="{{col.type}}" type="radio"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'" value="{{opt.name}}">{{opt.name}}<br></label>';
+	        		  	    				editElement = '<label ng-repeat="opt in col.possibleValues"  for="radio{{col.id}}"><input id="radio{{col.id}}" html-filter="{{col.type}}" type="radio" ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'" value="{{opt.name}}">{{opt.name}}<br></label>';
 	            		  	    		}else if(col.listStyle == "select"){
-	            		  	    			editElement = '<select html-filter="{{col.type}}" ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></select>';
+	            		  	    			editElement = '<select html-filter="{{col.type}}" ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'" ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></select>';
 	            		  	    		}else if(col.listStyle == "multiselect"){
-	            		  	    			editElement = '<select multiple html-filter="{{col.type}}"  ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></select>';
-	            		  	    		}else if(col.listStyle == "bs-select"){
-	            		  	    			editElement = '<select html-filter="{{col.type}}" data-width="auto" title="" ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'" bs-select></select>';
-	            		  	    		}else if(col.listStyle == "bs-select-multiple"){
-	            		  	    			editElement = '<select html-filter="{{col.type}}" data-width="auto" title="" multiple ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'" bs-select></select>';
+	            		  	    			editElement = '<select multiple html-filter="{{col.type}}"  ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'" ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></select>';
+	            		  	    		}else if(col.listStyle == "bt-select"){
+	            		  	    			editElement = '<bt-select html-filter="{{col.type}}" placeholder="" bt-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'" ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></bt-select>';	
+	            		  	    		}else if(col.listStyle == "bt-select-multiple"){
+	            		  	    			editElement = '<bt-select html-filter="{{col.type}}" placeholder="" multiple="true" bt-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'" ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></bt-select>';
 	            		  	    		}else{
 	            		  	    			editElement = '<select html-filter="{{col.type}}" ng-options="opt.code as opt.name for opt in '+getOptions(col)+' '+columnFormatter(col)+'"  ng-model="'+getNgModel(col)+'" ng-change="'+ngChange+'"></select>';
 	            		  	    		}
