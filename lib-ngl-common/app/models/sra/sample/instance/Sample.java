5f5226255ae7f4920d81dcc7ec5fa8374bac21cc
Shahinaz Gas <sgas@genoscope.cns.fr>
2014-01-24 13:27:56 +0000

    initialisation du model metier
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2358 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ lib-ngl-common/app/models/sra/sample/instance/Sample.java
@@ -0,0 +1,29 @@
+package models.sra.sample.instance;
+
+import java.util.Date;
+
+import validation.ContextValidation;
+import validation.IValidation;
+import fr.cea.ig.DBObject;
+
+public class Sample extends DBObject implements IValidation {
+
+	// SampleType
+	//public String alias;         // required mais remplacé par code herité de DBObject, et valeur = projectCode_num
+	public String projectCode;     // required pour nos stats   
+	public int taxonId;            // required 
+	public String classification;     
+	public String commonName;     
+	public String scientificName;  // required next soon 
+	public String title;           // required next soon 
+	public String description;      
+	public String clone;           
+	public String accession;       // numeros d'accession attribué par ebi 
+	public Date releaseDate;       // required, date de mise à disposition en public par l'EBI
+	
+	@Override
+	public void validate(ContextValidation contextValidation) {
+		// TODO Auto-generated method stub	
+	}
+
+}
---
+++ lib-ngl-common/app/models/sra/submission/instance/Submission.java
@@ -0,0 +1,15 @@
+package models.sra.submission.instance;
+
+import validation.ContextValidation;
+import validation.IValidation;
+import fr.cea.ig.DBObject;
+
+public class Submission extends DBObject implements IValidation {
+
+	@Override
+	public void validate(ContextValidation contextValidation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
