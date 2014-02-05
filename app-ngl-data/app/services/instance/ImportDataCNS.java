81454a55c626a01224f734acd3db912b6ec33475
Maud Haquelle <mhaquell@genoscope.cns.fr>
2014-02-05 15:54:02 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2417 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/ImportDataCNS.java
+++ app-ngl-data/app/services/instance/ImportDataCNS.java
@@ -12,12 +12,15 @@ public class ImportDataCNS{
 
 	public ImportDataCNS(){
 
-		// Import Projects tous les jours à 5h45
-		new ProjectImportCNS(Duration.create(ImportDataUtil.nextExecutionInSeconds(5,45),TimeUnit.SECONDS)
+		// Import Projects tous les jours à 16h00
+		new ProjectImportCNS(Duration.create(ImportDataUtil.nextExecutionInSeconds(16,00),TimeUnit.SECONDS)
 				,Duration.create(1,TimeUnit.DAYS));
 		new TubeImportCNS(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
+		// Voir avec julie la frequence pour la mise en prod
 		new PrepaflowcellImportCNS(Duration.create(10,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
 	    new RunImportCNS(Duration.create(15,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
+	   // update Tara
+	   // update SSID archive
 	    
 	}
 
