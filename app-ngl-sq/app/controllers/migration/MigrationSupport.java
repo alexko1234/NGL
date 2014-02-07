dc970ce66ff4eb7875c130cf7b2daa917527a066
Maud Haquelle <mhaquell@genoscope.cns.fr>
2014-02-07 08:28:44 +0000

    Update Methode creation support
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2436 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/app/controllers/migration/MigrationSupport.java
+++ app-ngl-sq/app/controllers/migration/MigrationSupport.java
@@ -17,82 +17,83 @@ import models.utils.InstanceHelpers;
 import play.Logger;
 import play.mvc.Result;
 import validation.ContextValidation;
+import validation.container.instance.ContainerValidationHelper;
 import controllers.CommonController;
 import controllers.migration.models.ContainerOld;
 import fr.cea.ig.MongoDBDAO;
 
 public class MigrationSupport extends CommonController{
-	
+
 	private static final String CONTAINER_COLL_NAME_BCK = InstanceConstants.CONTAINER_COLL_NAME+"_BCK";
-	
-	
+
+
 	public static Result migrationSupport(){
-		
+
 		Logger.info("Start point of Migration Support");
-		
+
 		JacksonDBCollection<ContainerOld, String> containersCollBck = MongoDBDAO.getCollection(CONTAINER_COLL_NAME_BCK, ContainerOld.class);
 		if(containersCollBck.count() == 0){
-			
+
 			Logger.info("Migration Support start");
-			
+
 			backupContainerCollection();
-			
+
 			List<ContainerOld> oldContainers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, ContainerOld.class).toList();
 			Logger.debug("migre "+oldContainers.size()+" containers");
 			for (ContainerOld container : oldContainers) {
 				migreBarCode(container);
 			}
 			Logger.info("Migration barCode end");
-			
+
 			List<Container> newContainers=MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class).toList();
 			Map<String,Support> mapSupport = new HashMap<String, Support>();
 			for (Container container:newContainers) {
 				createSupportCollection(container, mapSupport);
 			}
 			Logger.info("Creation Supports OK");
-			
+
 			updateBD(mapSupport);
-		
+
 		}else{
 			Logger.info("Migration Support already execute !");
 		}
-		
+
 		Logger.info("Migration Support finish");
 		return ok("Migration Support Finish");
 	}
-	
-	
 
-	
+
+
+
 	public static void migreBarCode(ContainerOld container) {
-			if (container.support != null) {
-				Container c = new Container();
-				c.support = new ContainerSupport();
-								
-				if (container.support.barCode != null) {
-					c.support.supportCode = container.support.barCode;
-					
-					MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
-							DBQuery.is("code", container.code), 
-							DBUpdate.unset("support.barCode") 
-							.set("support.supportCode", c.support.supportCode) );
+		if (container.support != null) {
+			Container c = new Container();
+			c.support = new ContainerSupport();
 
-				}
-				else {
+			if (container.support.barCode != null) {
+				c.support.supportCode = container.support.barCode;
 
-					MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
-							DBQuery.is("code", container.code), 
-							DBUpdate.unset("support.barCode")  );
+				MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
+						DBQuery.is("code", container.code), 
+						DBUpdate.unset("support.barCode") 
+						.set("support.supportCode", c.support.supportCode) );
 
-				}
-				
 			}
+			else {
+
+				MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
+						DBQuery.is("code", container.code), 
+						DBUpdate.unset("support.barCode")  );
+
+			}
+
+		}
 	}
-	
-	
-	
-	public static void createSupportCollection(Container container, Map<String,Support> mapSupport) {
-		
+
+
+
+	/*public static void createSupportCollection(Container container, Map<String,Support> mapSupport) {
+
 			if(container.support !=null){
 				Support support=new Support();
 				if(container.support.categoryCode!=null){
@@ -103,21 +104,36 @@ public class MigrationSupport extends CommonController{
 				support.state.code="A";
 				support.projectCodes=container.projectCodes;
 				support.sampleCodes=container.sampleCodes;
-				
+
 				TraceInformation traceInformation =new TraceInformation();
 				traceInformation.setTraceInformation("ngl"); 
 				support.traceInformation = traceInformation;
-				
+
 				if(!mapSupport.containsKey(support.code)){
 					mapSupport.put(support.code,support);
 				}
-				
+
 			}
+	}*/
+
+	public static void createSupportCollection(Container container, Map<String,Support> mapSupports) {
+
+		if (container.support != null) {
+			Support newSupport = ContainerValidationHelper.createSupport(container.support, container.projectCodes, container.sampleCodes);
+			if (!mapSupports.containsKey(newSupport.code)) {
+				mapSupports.put(newSupport.code, newSupport);
+			}
+			else {
+				Support oldSupport = (Support) mapSupports.get(newSupport.code);
+				InstanceHelpers.addCodesList(newSupport.projectCodes, oldSupport.projectCodes); 
+				InstanceHelpers.addCodesList(newSupport.sampleCodes, oldSupport.sampleCodes);
+			}
+
+		}
 	}
-	
-	
+
 	public static Result updateBD(Map<String,Support>  mapSupport) {
-	
+
 		ContextValidation contextValidation=new ContextValidation();
 		contextValidation.setCreationMode();
 		InstanceHelpers.save(InstanceConstants.SUPPORT_COLL_NAME, new ArrayList<Support>(mapSupport.values()),contextValidation);
@@ -131,8 +147,8 @@ public class MigrationSupport extends CommonController{
 		}
 
 	}
-	
-	
+
+
 	private static void backupContainerCollection() {
 		Logger.info("\tCopie "+InstanceConstants.CONTAINER_COLL_NAME+" start");
 		MongoDBDAO.save(CONTAINER_COLL_NAME_BCK, MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, ContainerOld.class).toList());
