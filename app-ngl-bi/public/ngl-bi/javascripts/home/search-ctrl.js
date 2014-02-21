e74b7c85486359ddece948f44801811178269fa8
Guillaume Albini <galbini@genoscope.cns.fr>
2014-02-21 14:08:35 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2537 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-bi/public/ngl-bi/javascripts/home/search-ctrl.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function SearchCtrl($scope, datatable) {
+
+	var datatableConfig = {
+			order :{by:'sequencingStartDate', mode:'local'},
+			search:{
+				url:jsRoutes.controllers.runs.api.Runs.list()
+			},
+			pagination:{
+				mode:'local',
+				numberRecordsPerPage:5,
+				numberRecordsPerPageList: [{number:5, clazz:''},{number:10, clazz:''},{number:25, clazz:''},{number:50, clazz:''},{number:100, clazz:''}]
+			},
+			columns : [
+			           {  	property:"code",
+					    	header: Messages("runs.code"),
+					    	type :"text",
+					    	order:true
+						},
+						{	property:"typeCode",
+							header: Messages("runs.typeCode"),
+							type :"text",
+					    	order:true
+						},
+						{	property:"sequencingStartDate",
+							header: Messages("runs.sequencingStartDate"),
+							type :"date",
+					    	order:true
+						},
+						{	property:"state.code",
+							render:function(value){
+								return Codes("state."+value.state.code);
+							},
+							header: Messages("runs.stateCode"),
+							type :"text",
+							order:true								
+						},
+						{	property:"valuation.valid",
+							render:function(value){
+								return Codes("valuation."+value.valuation.valid);
+							},
+							header: Messages("runs.valuation.valid"),
+							type :"text",
+					    	order:true
+						} 
+						]
+	};
+	
+	
+	
+	$scope.init = function(){
+		$scope.runsIPS = datatable($scope, datatableConfig);			
+		$scope.runsIPS.search({stateCodes:["IP-S"], excludes:["lanes","treatments"]});
+		$scope.runsIPRG = datatable($scope, datatableConfig);			
+		$scope.runsIPRG.search({stateCodes:["IP-RG"], excludes:["lanes","treatments"]});
+		$scope.runsIWV_IPV = datatable($scope, datatableConfig);			
+		$scope.runsIWV_IPV.search({stateCodes:["IW-V","IP-V"], excludes:["lanes","treatments"]});			
+	}	
+};
+
+SearchCtrl.$inject = ['$scope', 'datatable'];
+
