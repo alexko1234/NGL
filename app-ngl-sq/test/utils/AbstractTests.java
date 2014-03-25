50fb082eb35c2aacf290e6d7d26c57dabb65b31c
Maud Haquelle <mhaquell@genoscope.cns.fr>
2014-03-25 14:53:06 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2821 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/test/utils/AbstractTests.java
+++ app-ngl-sq/test/utils/AbstractTests.java
@@ -16,7 +16,7 @@ import play.test.Helpers;
 
 public abstract class AbstractTests {
 	protected static FakeApplication app;
-	
+
 	public static FakeApplication getFakeApplication(){
 		return fakeApplication(fakeConfiguration());
 	}
@@ -33,11 +33,12 @@ public abstract class AbstractTests {
 		Helpers.stop(app);
 	}
 
-	
+
 	public static Map<String,String> fakeConfiguration(){
 		Map<String,String> config = new HashMap<String,String>();
 		config.put("application.secret","0Qj/s6}212-e$B|s6GFQ-`yo[(; 66c2v7z3}U|y[&sn!41eN0{f<49n8Qh.J5OYo.f9-Xj1S1U5YFla");
 		config.put("evolutionplugin", "disabled");
+
 		config.put("db.default.driver", "com.mysql.jdbc.Driver");
 		config.put("db.default.url", "jdbc:mysql://mysqldev.genoscope.cns.fr:3306/NGL_TEST");
 		config.put("db.default.user", "NGL_user");
@@ -46,8 +47,8 @@ public abstract class AbstractTests {
 		config.put("db.default.maxConnectionsPerPartition", "10");
 		config.put("db.default.minConnectionsPerPartition", "1");
 		config.put("db.default.logStatements", "true");
-		config.put("db.default.jndiName", "model");
-		
+		config.put("db.default.jndiName", "ngl");
+
 		config.put("db.lims.driver", "net.sourceforge.jtds.jdbc.Driver");
 		config.put("db.lims.url", "jdbc:jtds:sybase://sybasedev.genoscope.cns.fr:3015/dblims");
 		config.put("db.lims.user", "mhaquell");
@@ -56,24 +57,30 @@ public abstract class AbstractTests {
 		config.put("db.lims.maxConnectionsPerPartition", "6");
 		config.put("db.lims.minConnectionsPerPartition", "1");
 		config.put("db.lims.logStatements", "true");
-		config.put("db.lims.jndiName", "lims");			
-		
-		config.put("casUrlValidator","https://cas.genoscope.cns.fr:8443/cas/serviceValidate");
+		config.put("db.lims.jndiName", "lims");
+
+		config.put("db.tara.driver","com.mysql.jdbc.Driver");
+		config.put("db.tara.url","jdbc:mysql://mysqlcns.genoscope.cns.fr:3307/Tara");
+		config.put("db.tara.user","lims_user");
+		config.put("db.tara.password","Lims_PassWrd");
+		config.put("db.tara.jndiName","tara");
+
+				config.put("casUrlValidator","https://cas.genoscope.cns.fr:8443/cas/serviceValidate");
 		config.put("casUrlLogin","https://cas.genoscope.cns.fr:8443/cas/login");
 		config.put("casRenew","false");
 		config.put("casMode","debug");
-		
+
 		config.put("mongodb.database","NGL-TESTU");
 		config.put("mongodb.credentials","testu:testu");
 		config.put("mongodb.servers","mongodev.genoscope.cns.fr:27017");
 		config.put("ehcacheplugin", "disabled");
 		config.put("mongodbJacksonMapperCloseOnStop", "disabled");
-		
+
 		config.put("asset.url","http://192.168.243.231:9001");
-		
+		config.put("ebean.default", "models.*");
 		return config;
-		
+
 	}
-	
-	
+
+
 }
