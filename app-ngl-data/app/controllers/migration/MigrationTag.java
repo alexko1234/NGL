70a0aaffc0bcd1b153dd6d22512df3e53c3ceb47
Didier Noisette <dnoisett@genoscope.cns.fr>
2014-04-04 09:22:36 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@2913 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/controllers/migration/MigrationTag.java
+++ app-ngl-data/app/controllers/migration/MigrationTag.java
@@ -76,7 +76,7 @@ public class MigrationTag extends CommonController {
 	public static List<Container> findContainerToCreate(final ContextValidation contextError) throws DAOException {
 
 
-		List<Container> results = jdbcTemplate.query("select * from v_flowcell_tongl where isavailable = true order by code, project, code_sample, tag",new Object[]{} 
+		List<Container> results = jdbcTemplate.query("select * from v_flowcell_tongl_reprise where isavailable = true order by code, project, code_sample, tag",new Object[]{} 
 		,new RowMapper<Container>() {
 
 			@SuppressWarnings("rawtypes")
