a4b44ec3f14413e376036eae9fff5480514bd467
Didier Noisette <dnoisett@genoscope.cns.fr>
2014-05-06 07:44:39 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3111 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-projects/app/controllers/main/tpl/Main.java
+++ app-ngl-projects/app/controllers/main/tpl/Main.java
@@ -37,7 +37,6 @@ public class Main extends CommonController {
 			sb.append("\"").append(cl.tableName).append(".").append(cl.code)
 			.append("\":\"").append(cl.label).append("\",");
 		}
-		sb.append("\"category.default\":\"defaut\"");
 		sb.append("};return function(k){if(typeof k == 'object'){for(var i=0;i<k.length&&!ms[k[i]];i++);var m=ms[k[i]]||k[0]}else{m=ms[k]||k}for(i=1;i<arguments.length;i++){m=m.replace('{'+(i-1)+'}',arguments[i])}return m}})();");
 		return sb.toString();
 	}
