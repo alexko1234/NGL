07287accfb2ff1fef0c096109b0f5c993a584e03
Guillaume Albini <galbini@genoscope.cns.fr>
2014-05-23 14:31:33 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3224 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-frameworkweb/app/controllers/ListForm.java
+++ lib-ngl-frameworkweb/app/controllers/ListForm.java
@@ -9,6 +9,8 @@ import views.components.datatable.DatatableForm;
 
 public class ListForm extends DatatableForm {
 	public Boolean list = Boolean.FALSE;
+	public Boolean count = Boolean.FALSE;
+	
 	public Integer limit = 5000; //limit the number or element in the result
 	
 }
