a111d9add72e0705ca1bf50fdf7af3b847e14955
Maud Haquelle <mhaquell@genoscope.cns.fr>
2014-05-28 12:37:06 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3228 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-data/app/services/instance/sample/UpdateSampleCNS.java
@@ -0,0 +1,79 @@
+package services.instance.sample;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.laboratory.common.description.Level;
+import models.laboratory.common.instance.PropertyValue;
+import models.laboratory.sample.description.ImportType;
+import models.laboratory.sample.description.SampleType;
+import models.laboratory.sample.instance.Sample;
+import models.utils.InstanceConstants;
+import models.utils.InstanceHelpers;
+import models.utils.dao.DAOException;
+import models.utils.instance.SampleHelper;
+import rules.services.RulesException;
+import scala.concurrent.duration.FiniteDuration;
+import services.instance.AbstractImportDataCNS;
+import validation.ContextValidation;
+import validation.utils.BusinessValidationHelper;
+
+import com.mongodb.MongoException;
+
+import fr.cea.ig.MongoDBDAO;
+
+public class UpdateSampleCNS extends AbstractImportDataCNS{
+
+	public UpdateSampleCNS(FiniteDuration durationFromStart,
+			FiniteDuration durationFromNextIteration) {
+		super("UpdateSample", durationFromStart, durationFromNextIteration);
+
+	}
+
+	@Override
+	public void runImport() throws SQLException, DAOException, MongoException,
+	RulesException {
+		updateSampleFromTara(contextError, null);
+
+	}
+
+	public static void updateSampleFromTara(ContextValidation contextError,
+			List<String> sampleCodes) throws SQLException, DAOException {
+
+		List<String> results=limsServices.findSampleUpdated(sampleCodes);
+
+		for(String sampleCode:results){
+			Sample sample=limsServices.findSampleToCreate(contextError, sampleCode);
+
+			MongoDBDAO.deleteByCode(InstanceConstants.SAMPLE_COLL_NAME, Sample.class, sample.code);
+			Sample newSample =(Sample) InstanceHelpers.save(InstanceConstants.SAMPLE_COLL_NAME,sample,contextError,true);
+			if(!contextError.hasErrors()){
+				limsServices.updateMaterielLims(newSample, contextError);
+
+				SampleType sampleType =BusinessValidationHelper.validateExistDescriptionCode(null, sample.typeCode, "typeCode", SampleType.find,true);
+				ImportType importType =BusinessValidationHelper.validateExistDescriptionCode(null, sample.importTypeCode, "importTypeCode", ImportType.find,true);
+
+				Map<String, PropertyValue> properties=new HashMap<String, PropertyValue>();
+
+				if(importType !=null){
+					InstanceHelpers.copyPropertyValueFromPropertiesDefinition(importType.getPropertyDefinitionByLevel(Level.CODE.Content), newSample.properties,properties);
+				}
+				if(sampleType !=null){
+					InstanceHelpers.copyPropertyValueFromPropertiesDefinition(sampleType.getPropertyDefinitionByLevel(Level.CODE.Content), newSample.properties,properties);
+				}
+
+				SampleHelper.updateSampleProperties(sampleCode, properties);
+
+			}
+		}
+
+
+
+	}
+
+
+
+
+}
