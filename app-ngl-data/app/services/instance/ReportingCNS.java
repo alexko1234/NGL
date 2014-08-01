9a0f610ddb16c5618476405c9111077e927b9ceb
Didier Noisette <dnoisett@genoscope.cns.fr>
2014-08-01 13:36:08 +0000

    NGL-Data : suppress ReportingCNSDAO
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3482 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/ReportingCNS.java
+++ app-ngl-data/app/services/instance/ReportingCNS.java
@@ -1,17 +1,25 @@
 package services.instance;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import javax.mail.MessagingException;
+
+import org.mongojack.DBQuery;
+
+import com.mongodb.MongoException;
 import com.typesafe.config.ConfigFactory;
+
+import fr.cea.ig.MongoDBDAO;
 
 import mail.MailServiceException;
 import mail.MailServices;
-import models.ReportingCNSDAO;
+import models.laboratory.run.instance.ReadSet;
+import models.utils.InstanceConstants;
 import scala.concurrent.duration.FiniteDuration;
 
 
@@ -35,7 +43,7 @@ public class ReportingCNS extends AbstractReporting {
 		    
 		    MailServices mailService = new MailServices();
 
-		    //getContentType : expected text/html found text/plain. Why ? :
+		    //dnoisett, rem : getContentType : expected text/html found text/plain. Why ? :
 		    //How set contentType ?
 		    /*
 			Properties properties = System.getProperties();
@@ -56,10 +64,10 @@ public class ReportingCNS extends AbstractReporting {
 		    					"4) Readsets à évaluer : SortingRibo manquant",
 		    					"5) Readsets à évaluer : Taxonomy manquant"};
 		    String[] comments = {"Liste des readsets à l\'état IP-QC, pour lesquels le traitement readQualityRaw n\'existe pas.",
-		    		"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement readQualityRaw n\'existe pas.",
-		    		"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement readQualityClean n\'existe pas.",
-		    		"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement sortingRibo n\'existe pas et dont le sampleType est dans la liste : depletedRNA ;  mRNA ; total-RNA ; sRNA ; cDNA",
-		    		"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement taxonomy n\'existe pas."};
+		    	"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement readQualityRaw n\'existe pas.",
+		    	"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement readQualityClean n\'existe pas.",
+		    	"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement sortingRibo n\'existe pas et dont le sampleType est dans la liste : depletedRNA ;  mRNA ; total-RNA ; sRNA ; cDNA",
+		    	"Liste des readsets à l\'état IW-VQC, pour lesquels le traitement taxonomy n\'existe pas."};
 		    String subHeader1 = "Nombre de résultats : ";
 		    String subHeader2 = "Détails : ";
 		    String lineReturn = "<br>";
@@ -67,7 +75,7 @@ public class ReportingCNS extends AbstractReporting {
 		    
 		    int[] nb = new int[nbQueryTypes];
 		    for (int i=0; i<nbQueryTypes; i++) {
-		    	nb[i] = ReportingCNSDAO.getCountOfQuery(i+1);
+		    	nb[i] = getQuery(i+1).size();
 		    }
 		    
 
@@ -79,7 +87,7 @@ public class ReportingCNS extends AbstractReporting {
 		    	buffer.append(subHeader1).append(nb[i]).append(lineReturn).append(lineReturn);
 		    	
 		    	List<String> results = null;
-		    	results = ReportingCNSDAO.getResultsOfQuery(i+1);
+		    	results = getQuery(i+1);
 		    	
 		    	if ((nb[i] > 0) && (results != null)) {
 		    		buffer.append(subHeader2).append(lineReturn);
@@ -99,6 +107,46 @@ public class ReportingCNS extends AbstractReporting {
 		} catch (MailServiceException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public static List<String> getQuery(int queryId) throws MongoException {
+		List<ReadSet> readSets = null;
+		switch(queryId) {
+			case 1:
+				readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.and(DBQuery.is("state.code", "IP-QC"), 
+						DBQuery.notExists("treatments.readQualityRaw"))).toList();
+				break;
+			case 2:
+				readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.and(DBQuery.is("state.code", "IW-VQC"), 
+						DBQuery.notExists("treatments.readQualityRaw"))).toList();
+				break;
+			case 3:
+				readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.and(DBQuery.is("state.code", "IW-VQC"), 
+						DBQuery.notExists("treatments.readQualityClean"))).toList();
+				break;
+			case 4:
+				readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.and( DBQuery.is("state.code", "IW-VQC"), 
+						DBQuery.notExists("treatments.sortingRibo"), DBQuery.in("sampleOnContainer.sampleTypeCode", Arrays.asList("depletedRNA","mRNA","total-RNA","sRNA","cDNA")) )).toList();
+				break;
+			case 5:
+				readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.and(DBQuery.is("state.code", "IW-VQC"), 
+						DBQuery.notExists("treatments.taxonomy"))).toList();
+				break;
+		}
+		ArrayList<String> lines = new ArrayList<String>(); 
+		StringBuffer buffer;
+		for (ReadSet readSet : readSets) {
+			buffer = new StringBuffer();
+			buffer.append("code : ").append(readSet.code);
+			buffer.append(", runCode : ").append(readSet.runCode);
+			buffer.append(", stateCode : ").append(readSet.state.code);
+			if (queryId==4) {
+				buffer.append(", sampleTypeCode : ").append(readSet.sampleOnContainer.sampleTypeCode);
+			}
+			lines.add(buffer.toString());
+		}
+		return lines;
 	}
 	
 }
