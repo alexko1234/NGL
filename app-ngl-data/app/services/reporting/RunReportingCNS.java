f36e664c4f0a671f67a6e6f0860ea31888ac2d12
Didier Noisette <dnoisett@genoscope.cns.fr>
2014-08-13 13:20:00 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3561 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/reporting/RunReportingCNS.java
+++ app-ngl-data/app/services/reporting/RunReportingCNS.java
@@ -7,7 +7,7 @@ import services.instance.ImportDataUtil;
 public class RunReportingCNS {
 	
 	public RunReportingCNS() {
-		new ReportingCNS(Duration.create(ImportDataUtil.nextExecutionInSeconds(8, 0),TimeUnit.SECONDS)
+		new ReportingCNS(Duration.create(ImportDataUtil.nextExecutionInSeconds(15, 17),TimeUnit.SECONDS)
 				,Duration.create(1,TimeUnit.DAYS));
 	}
 
