4e63d82a546d6db494c36fa5343bb6018c887b19
Guillaume Albini <galbini@genoscope.cns.fr>
2014-09-03 10:08:19 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3690 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/app/models/laboratory/project/instance/BioinformaticParameters.java
+++ lib-ngl-common/app/models/laboratory/project/instance/BioinformaticParameters.java
@@ -11,19 +11,6 @@ public class BioinformaticParameters {
 	public String regexBA;
 	public String mappingReference;
 	
-	public BioinformaticParameters() {
-		this.bioinformaticAnalysis = Boolean.FALSE;
-		this.regexBA = null;
-		this.mappingReference = null;
-	}
-	
-	public BioinformaticParameters(Boolean bioinformaticAnalysis) {
-		this.bioinformaticAnalysis = bioinformaticAnalysis;
-		this.regexBA = null;
-		this.mappingReference = null;
-	}
-	
-	
 	
 	@Override
 	public String toString() {
