ad97fe7351de87273ab1a9b3aa3015aa0a611012
Guillaume Albini <galbini@genoscope.cns.fr>
2014-10-15 13:05:41 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3806 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-bi/app/controllers/migration/UpdateBABle.java
@@ -0,0 +1,65 @@
+package controllers.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.laboratory.common.instance.property.PropertySingleValue;
+import models.laboratory.run.instance.Analysis;
+import models.laboratory.run.instance.ReadSet;
+import models.laboratory.run.instance.SampleOnContainer;
+import models.utils.InstanceConstants;
+import models.utils.InstanceHelpers;
+
+import org.mongojack.DBQuery;
+import org.mongojack.DBUpdate;
+import org.mongojack.JacksonDBCollection;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+import com.mongodb.BasicDBObject;
+
+import play.Logger;
+import play.Play;
+import play.libs.Akka;
+import play.mvc.Result;
+import rules.services.RulesActor;
+import rules.services.RulesMessage;
+import controllers.CommonController;
+import fr.cea.ig.MongoDBDAO;
+
+public class UpdateBABle extends CommonController {
+	private static ActorRef rulesActor = Akka.system().actorOf(Props.create(RulesActor.class));
+	
+	public static Result migration(){
+		Logger.info("Migration Analysis start");
+		List<Analysis> analyzes = MongoDBDAO.find(InstanceConstants.ANALYSIS_COLL_NAME, Analysis.class, DBQuery.notExists("treatments.contigFilterBA.pairs.lostBasesPercent")).sort("code").toList();
+		Logger.debug("migre "+analyzes.size()+" Analysis for LostBasePercent");
+		for(Analysis analysis : analyzes){
+			migre(analysis,"BPA_ContigFilter_1");				
+		}
+		
+		
+		analyzes = MongoDBDAO.find(InstanceConstants.ANALYSIS_COLL_NAME, Analysis.class, DBQuery.notExists("masterReadSetProperties")).sort("code").toList();
+		Logger.debug("migre "+analyzes.size()+" Analysis for ReadSetProperties");
+		for(Analysis analysis : analyzes){
+			migre(analysis,"BPA_SetReadSetProperties_1");				
+		}
+		
+		Logger.info("Migration Analysis end");
+		
+		Logger.info("Migration finish");
+		return ok("Migration Finish");
+
+	}
+
+	private static void migre(Analysis analysis, String ruleCode) {
+		ArrayList<Object> facts = new ArrayList<Object>();
+		facts.add(analysis);
+		// Outside of an actor and if no reply is needed the second argument can be null
+		rulesActor.tell(new RulesMessage(facts,Play.application().configuration().getString("rules.key"),ruleCode),null);
+	}
+	
+	
+}
+
