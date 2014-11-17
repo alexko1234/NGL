4817823053ccaa232492ed05c26ef9ebcfa9fcb5
Yann Deshayes <ydeshayes@genoscope.cns.fr>
2014-11-17 08:44:54 +0000

    default-value directive OK, tested with input and bt-select
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@3936 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/public/ngl-common/javascripts/services/commons.js
+++ lib-ngl-common/public/ngl-common/javascripts/services/commons.js
@@ -285,8 +285,9 @@ angular.module('commonsServices', []).
 					}
     			}
     		};
+    	//Write in an input or select in a list element the value passed to the directive when the list or the input ngModel is undefined or empty
+    	//EXAMPLE: <input type="text" default-value="test" ng-model="x">
     	}]).directive('defaultValue',['$parse', function($parse) {
-    		var OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w\d]*)|(?:\(\s*([\$\w][\$\w\d]*)\s*,\s*([\$\w][\$\w\d]*)\s*\)))\s+in\s+(.*)$/;
     		return {
     			require: 'ngModel',
     			link: function(scope, element, attrs, ngModel) {
