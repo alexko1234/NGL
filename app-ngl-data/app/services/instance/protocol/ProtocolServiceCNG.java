b8d903b9dbf597c2a0adcac8efcda34eb341a42a
Maud Haquelle <mhaquell@genoscope.cns.fr>
2015-01-27 13:57:27 +0000

    Remove protocols
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4304 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/protocol/ProtocolServiceCNG.java
+++ app-ngl-data/app/services/instance/protocol/ProtocolServiceCNG.java
@@ -5,6 +5,9 @@ import static services.instance.InstanceFactory.newProtocol;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mongojack.DBQuery;
+
+import fr.cea.ig.MongoDBDAO;
 import models.laboratory.protocol.instance.Protocol;
 import models.utils.InstanceConstants;
 import models.utils.InstanceHelpers;
@@ -18,6 +21,8 @@ public class ProtocolServiceCNG {
 	public static void main(ContextValidation ctx) {	
 
 		Logger.info("Start to create protocols collection for "+institute+"...");
+		Logger.info("Remove protocol");
+		removeProtocols(ctx);
 		Logger.info("Save protocols ...");
 		saveProtocols(ctx);
 		Logger.info(institute+" Protocols collection creation is done!");
@@ -49,6 +54,10 @@ public class ProtocolServiceCNG {
 
 	} */
 	
+	private static void removeProtocols(ContextValidation ctx) {
+		MongoDBDAO.delete(InstanceConstants.PROTOCOL_COLL_NAME, Protocol.class, DBQuery.empty());
+	}
+
 	
 	public static void saveProtocols(ContextValidation ctx){		
 		List<Protocol> lp = new ArrayList<Protocol>();
--- app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
+++ app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
@@ -5,10 +5,14 @@ import static services.instance.InstanceFactory.newProtocol;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mongojack.DBQuery;
+
+import fr.cea.ig.MongoDBDAO;
 import models.laboratory.protocol.instance.Protocol;
 import models.utils.InstanceConstants;
 import models.utils.InstanceHelpers;
 import play.Logger;
+import scala.collection.script.Remove;
 import services.instance.InstanceFactory;
 import validation.ContextValidation;
 
@@ -19,12 +23,19 @@ public class ProtocolServiceCNS {
 	public static void main(ContextValidation ctx) {	
 
 		Logger.info("Start to create protocols collection for "+institute+"...");
+		Logger.info("Remove protocol");
+		removeProtocols(ctx);
 		Logger.info("Save protocols ...");
 		saveProtocols(ctx);
 		Logger.info(institute+" Protocols collection creation is done!");
 	}
 	
 	
+	private static void removeProtocols(ContextValidation ctx) {
+		MongoDBDAO.delete(InstanceConstants.PROTOCOL_COLL_NAME, Protocol.class, DBQuery.empty());
+	}
+
+
 	public static void saveProtocols(ContextValidation ctx){		
 		List<Protocol> lp = new ArrayList<Protocol>();
 		
