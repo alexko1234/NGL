2dc7d5c7233d3fbee934eefc6d33bbd895a1eadc
Maud Haquelle <mhaquell@genoscope.cns.fr>
2015-01-29 10:01:04 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4333 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
+++ app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.mongojack.DBQuery;
 
+import com.typesafe.config.ConfigFactory;
+
 import fr.cea.ig.MongoDBDAO;
 import models.laboratory.protocol.instance.Protocol;
 import models.utils.InstanceConstants;
@@ -39,16 +41,19 @@ public class ProtocolServiceCNS {
 	public static void saveProtocols(ContextValidation ctx){		
 		List<Protocol> lp = new ArrayList<Protocol>();
 		
-		lp.add(newProtocol("fragmentation_ptr_sox140_1","Fragmentation_ptr_sox140_1","path1","1","production", InstanceFactory.setExperimentTypeCodes("fragmentation")));
-		lp.add(newProtocol("bqspri_ptr_sox142_1","BqSPRI_ptr_sox142_1","path2","1","production", InstanceFactory.setExperimentTypeCodes("librairie-indexing", "librairie-dualindexing")));
-		lp.add(newProtocol("amplif_ptr_sox144_1","Amplif_ptr_sox144_1","path3","1","production", InstanceFactory.setExperimentTypeCodes("amplification", "solution-stock")));
-		lp.add(newProtocol("depot_illumina_ptr_1","Depot_Illumina_prt_1","path4","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
-		lp.add(newProtocol("depot_illumina_ptr_2","Depot_Illumina_prt_2","path5","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
-		lp.add(newProtocol("depot_illumina_ptr_3","Depot_Illumina_prt_3","path6","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
 		lp.add(newProtocol("depot_opgen_ptr_1","Depot_Opgen_prt_1","path7","1","production", InstanceFactory.setExperimentTypeCodes("opgen-depot")));
-		lp.add(newProtocol("prepfc_cbot_ptr_sox139_1","PrepFC_CBot_ptr_sox139_1","path7","1","production", InstanceFactory.setExperimentTypeCodes("prepa-flowcell")));
-		lp.add(newProtocol("proto_qc_v1","Proto_QC_v1","path7","1","production", InstanceFactory.setExperimentTypeCodes("chip-migration-post-pcr", "chip-migration-pre-pcr", "fluo-quantification", "qpcr-quantification")));
-	
+		
+		if(	!ConfigFactory.load().getString("ngl.env").equals("PROD") ){
+		
+			lp.add(newProtocol("fragmentation_ptr_sox140_1","Fragmentation_ptr_sox140_1","path1","1","production", InstanceFactory.setExperimentTypeCodes("fragmentation")));
+			lp.add(newProtocol("bqspri_ptr_sox142_1","BqSPRI_ptr_sox142_1","path2","1","production", InstanceFactory.setExperimentTypeCodes("librairie-indexing", "librairie-dualindexing")));
+			lp.add(newProtocol("amplif_ptr_sox144_1","Amplif_ptr_sox144_1","path3","1","production", InstanceFactory.setExperimentTypeCodes("amplification", "solution-stock")));
+			lp.add(newProtocol("depot_illumina_ptr_1","Depot_Illumina_prt_1","path4","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("depot_illumina_ptr_2","Depot_Illumina_prt_2","path5","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("depot_illumina_ptr_3","Depot_Illumina_prt_3","path6","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("prepfc_cbot_ptr_sox139_1","PrepFC_CBot_ptr_sox139_1","path7","1","production", InstanceFactory.setExperimentTypeCodes("prepa-flowcell")));
+			lp.add(newProtocol("proto_qc_v1","Proto_QC_v1","path7","1","production", InstanceFactory.setExperimentTypeCodes("chip-migration-post-pcr", "chip-migration-pre-pcr", "fluo-quantification", "qpcr-quantification")));
+		}
 		
 		for(Protocol protocole:lp){
 			InstanceHelpers.save(InstanceConstants.PROTOCOL_COLL_NAME, protocole,ctx);
