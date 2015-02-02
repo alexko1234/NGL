70491c31a096b64c54753dbfa43beb2dc9add13e
Xavier Nicolza <xnicolza@genoscope.cns.fr>
2015-02-02 15:44:56 +0000

    NGL-479: NGL-SQ-DEV : mettre à jour les protocoles dans les expériences prep FC et dépôt (CNS) https://gsphere.genoscope.cns.fr/jira/browse/NGL-479
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4347 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
+++ app-ngl-data/app/services/instance/protocol/ProtocolServiceCNS.java
@@ -48,10 +48,13 @@ public class ProtocolServiceCNS {
 			lp.add(newProtocol("fragmentation_ptr_sox140_1","Fragmentation_ptr_sox140_1","path1","1","production", InstanceFactory.setExperimentTypeCodes("fragmentation")));
 			lp.add(newProtocol("bqspri_ptr_sox142_1","BqSPRI_ptr_sox142_1","path2","1","production", InstanceFactory.setExperimentTypeCodes("librairie-indexing", "librairie-dualindexing")));
 			lp.add(newProtocol("amplif_ptr_sox144_1","Amplif_ptr_sox144_1","path3","1","production", InstanceFactory.setExperimentTypeCodes("amplification", "solution-stock")));
-			lp.add(newProtocol("depot_illumina_ptr_1","Depot_Illumina_prt_1","path4","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
-			lp.add(newProtocol("depot_illumina_ptr_2","Depot_Illumina_prt_2","path5","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
-			lp.add(newProtocol("depot_illumina_ptr_3","Depot_Illumina_prt_3","path6","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("hiseq2000_Illumina","HiSeq2000_Illumina","path4","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("hiseq2500_fast_Illumina","HiSeq2500_fast_Illumina","path5","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("hiseq2500_Illumina","HiSeq2500_Illumina","path6","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
+			lp.add(newProtocol("ptr_sox147_v1_depot","PTR_SOX147_v1","path6","1","production", InstanceFactory.setExperimentTypeCodes("illumina-depot")));
 			lp.add(newProtocol("prepfc_cbot_ptr_sox139_1","PrepFC_CBot_ptr_sox139_1","path7","1","production", InstanceFactory.setExperimentTypeCodes("prepa-flowcell")));
+			lp.add(newProtocol("cbot_rapid_run_2500_Illumina","CBot_rapid_run_2500_Illumina","path7","1","production", InstanceFactory.setExperimentTypeCodes("prepa-flowcell")));
+			lp.add(newProtocol("ptr_sox147_v1","PTR_SOX147_v1","path7","1","production", InstanceFactory.setExperimentTypeCodes("prepa-flowcell")));
 			lp.add(newProtocol("proto_qc_v1","Proto_QC_v1","path7","1","production", InstanceFactory.setExperimentTypeCodes("chip-migration-post-pcr", "chip-migration-pre-pcr", "fluo-quantification", "qpcr-quantification")));
 		}
 		
