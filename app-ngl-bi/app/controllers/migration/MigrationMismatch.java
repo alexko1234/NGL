acd0ed4700f921c55c3e7e4292ff9b6d273ec170
Guillaume Albini <galbini@genoscope.cns.fr>
2015-02-05 09:57:17 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4357 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-bi/app/controllers/migration/MigrationMismatch.java
@@ -0,0 +1,45 @@
+package controllers.migration;
+
+import java.util.List;
+
+import org.mongojack.DBQuery;
+import org.mongojack.DBUpdate;
+
+import lims.cns.dao.LimsAbandonDAO;
+import lims.models.experiment.illumina.RunSolexa;
+import models.laboratory.run.instance.Run;
+import models.utils.InstanceConstants;
+import play.Logger;
+import play.api.modules.spring.Spring;
+import play.mvc.Result;
+import controllers.CommonController;
+import fr.cea.ig.MongoDBDAO;
+
+public class MigrationMismatch extends CommonController{
+	
+	public static Result migration(){
+		List<RunSolexa> runs = Spring.getBeanOfType(LimsAbandonDAO.class).findRunMismatch();
+		int t = 0, f = 0;
+		for(RunSolexa rs : runs){
+			
+			if(MongoDBDAO.checkObjectExist(InstanceConstants.RUN_ILLUMINA_COLL_NAME, Run.class, DBQuery.is("code", rs.runslnom).exists("treatments.ngsrg.default.mismatch.value"))){
+				//Logger.info("update run : "+rs.runslnom+" = "+((rs.mismatch == 1)?true:false));
+				
+				MongoDBDAO.update(InstanceConstants.RUN_ILLUMINA_COLL_NAME, Run.class, 
+						DBQuery.is("code", rs.runslnom).exists("treatments.ngsrg.default.mismatch.value"), 
+						DBUpdate.set("treatments.ngsrg.default.mismatch.value", ((rs.mismatch == 1)?true:false)));
+				
+				
+				if(((rs.mismatch == 1)?true:false))t++;
+				else f++;
+			}else{
+				Logger.debug("Not found ="+rs.runslnom);
+			}
+			
+		}
+		Logger.debug("true = "+t+" / false = "+f+" total = "+runs.size());
+		
+		return ok();
+	}
+
+}
