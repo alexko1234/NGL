95770fe37fce7cd97f43fdf7e7985c02cc98a5f5
Guillaume Albini <galbini@genoscope.cns.fr>
2015-02-09 14:01:26 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4382 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ app-ngl-bi/app/controllers/migration/MigrationTaxonomyFungi.java
@@ -0,0 +1,48 @@
+package controllers.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mongojack.DBQuery;
+
+import com.mongodb.BasicDBObject;
+
+import models.laboratory.run.instance.ReadSet;
+import models.utils.InstanceConstants;
+import play.Logger;
+import play.Play;
+import play.mvc.Result;
+import rules.services.RulesException;
+import rules.services.RulesServices;
+import controllers.CommonController;
+import fr.cea.ig.MongoDBDAO;
+
+
+public class MigrationTaxonomyFungi extends CommonController{
+	
+	public static Result migration(){
+		BasicDBObject keys = new BasicDBObject();
+		keys.put("code", 1);
+		keys.put("treatments.taxonomy", 1);
+		
+		List<ReadSet> rsl = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.exists("treatments.taxonomy"), keys).toList();
+		
+		RulesServices rulesServices = new RulesServices();
+		Logger.info("Treat "+rsl.size()+" readset");
+		for(ReadSet rs : rsl){
+			
+			try {
+				List<Object> facts = new ArrayList<Object>();
+				facts.add(rs);
+				
+				rulesServices.callRules(Play.application().configuration().getString("rules.key"), "QC_1", facts);
+			} catch (RulesException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return ok();
+	}
+	
+
+}
