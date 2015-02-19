9ed0162754e26333982956fedfe9eb89c8df8d98
Guillaume Albini <galbini@genoscope.cns.fr>
2015-02-19 13:29:54 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4468 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
    val appVersion = "1.0-SNAPSHOT"
      
    val sqVersion = "1.4-SNAPSHOT"  
-   val biVersion = "1.9.1-SNAPSHOT"
+   val biVersion = "1.10-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.0-SNAPSHOT" 
    val subVersion = "1.0-SNAPSHOT"  
