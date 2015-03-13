ceb5461b9099ea21804b7a904c46890d04495fb8
Guillaume Albini <galbini@genoscope.cns.fr>
2015-03-13 08:56:33 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4651 d0e871a8-0b98-4e59-9d77-5ea3c331444f

---
+++ lib-ngl-common/app/controllers/readsets/api/ReadSetsController.java
@@ -0,0 +1,24 @@
+package controllers.readsets.api;
+
+import models.laboratory.common.instance.TraceInformation;
+import models.laboratory.run.instance.ReadSet;
+import models.utils.InstanceConstants;
+import controllers.CommonController;
+import controllers.NGLControllerHelper;
+import fr.cea.ig.MongoDBDAO;
+
+public class ReadSetsController extends CommonController {
+
+    protected static ReadSet getReadSet(String code) {
+    	ReadSet readSet = MongoDBDAO.findByCode(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, code);
+    	return readSet;
+    }
+   
+    
+    protected static TraceInformation getUpdateTraceInformation(ReadSet readSet) {
+		TraceInformation ti = readSet.traceInformation;
+		ti.setTraceInformation(getCurrentUser());
+		return ti;
+	}
+	
+}
