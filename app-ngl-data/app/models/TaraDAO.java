e5272450b4be500bebff7dba20f2e0d4a9ed17c0
Maud Haquelle <mhaquell@genoscope.cns.fr>
2015-03-23 14:32:00 +0000

    Pbl de jointure avec la table cross_ref Gaelle a créé une vue appelée V_SAMPLE_NGL
    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4732 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/models/TaraDAO.java
+++ app-ngl-data/app/models/TaraDAO.java
@@ -25,7 +25,9 @@ public class TaraDAO {
 
 	private JdbcTemplate jdbcTemplate;
 
-	protected final String  SELECT_MATERIEL_TARA= " SELECT"+
+	protected final String  SELECT_MATERIEL_TARA= "select * from V_SAMPLE_NGL" ;
+			/*
+			" SELECT"+
 			"  cast(AREA.AREA_CODE as SIGNED INT) as station ,"+
 			"  AREA.AREA_NAME,"+
 			"  FRACTION.FRACTION_CODE as filtreCode,"+
@@ -40,13 +42,13 @@ public class TaraDAO {
 			"  COLLAB_INFOS.INFO as codebarre"+
 			" FROM "+
 			" AREA INNER JOIN SAMPLE ON (AREA.AREA_ID=SAMPLE.AREA_ID)"+
-			"  INNER JOIN CROSS_REF ON (SAMPLE.SAMPLE_ID=CROSS_REF.SAMPLE_ID)"+
+			"  INNER JOIN CROSS_REF ON (SAMPLE.SAMPLE_ID=CROSS_REF.SAMPLE_ID and CROSS_REF.REF_DB='lims')"+
 			"  INNER JOIN FRACTION ON (SAMPLE.FRACTION_ID=FRACTION.FRACTION_ID)"+
 			"  INNER JOIN ITERATION ON (ITERATION.ITERATION_ID=SAMPLE.ITERATION_ID)"+
 			"  INNER JOIN LOCUS ON (SAMPLE.LOCUS_ID=LOCUS.LOCUS_ID)"+
 			"  LEFT JOIN COLLAB_INFOS ON (COLLAB_INFOS.SAMPLE_ID=SAMPLE.SAMPLE_ID AND COLLAB_INFOS.INFOS_TYPE_CODE=1)"+
 			"  INNER JOIN MATERIAL ON (MATERIAL.MATERIAL_ID=SAMPLE.MATERIAL_ID)";
-
+*/
 	@Autowired
 	@Qualifier("tara")
 	public void setDataSourceTara(DataSource dataSource) {
@@ -57,7 +59,7 @@ public class TaraDAO {
 	public Map<String,PropertyValue> findTaraSampleFromLimsCode(Integer limsCode,ContextValidation contextValidation){
 
 		List<Map<String,PropertyValue>> results =  this.jdbcTemplate.query(SELECT_MATERIEL_TARA +
-				"  WHERE CROSS_REF.REF_ID=? ", 
+				"  WHERE REF_ID=? ", 
 				new Object[]{limsCode},new RowMapper<Map<String,PropertyValue>>() {
 
 			public Map<String,PropertyValue> mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -82,10 +84,10 @@ public class TaraDAO {
 
 		String sql=null;
 		if(limsCodes==null){
-			sql=SELECT_MATERIEL_TARA+" WHERE TO_DAYS(NOW()) - TO_DAYS(SAMPLE.LAST_UPD_TARA_DB) <= 20";
+			sql=SELECT_MATERIEL_TARA+" WHERE TO_DAYS(NOW()) - TO_DAYS(LAST_UPD_TARA_DB) <= 10";
 		}else {
 			//Pour les tests unitaires
-			sql=SELECT_MATERIEL_TARA+" WHERE CROSS_REF.REF_ID in (";
+			sql=SELECT_MATERIEL_TARA+" WHERE REF_ID in (";
 			for(String code:limsCodes){
 				sql=sql+"'"+code+"',";
 			}
