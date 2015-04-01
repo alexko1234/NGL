cb505e50d9adf7d0e6f5c0a43b7d6ba0363587a0
Maud Haquelle <mhaquell@genoscope.cns.fr>
2015-04-01 14:10:51 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4827 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-sq/test/processes/ProcessesTest.java
+++ app-ngl-sq/test/processes/ProcessesTest.java
@@ -68,41 +68,6 @@ public class ProcessesTest extends AbstractTests{
 	
 	protected static ALogger logger=Logger.of("ProcessesTest");
 	
-	//TODO: container avec plusieurs contents (impoter jeu de données)
-	@Test
-	public void validateSaveFromSupportIlluminaRunProcess() throws JsonParseException, JsonMappingException, IOException{
-		ContainerSupport cs =  MongoDBDAO.find(InstanceConstants.CONTAINER_SUPPORT_COLL_NAME, ContainerSupport.class, DBQuery.and(DBQuery.regex("code", Pattern.compile("^ASR")),DBQuery.is("state.code", "IW-P"))).toList().get(0);		
-		Process process = ProcessTestHelper.getFakeProcess("sequencing", "illumina-run");
-		process.projectCode = cs.projectCodes.get(0);
-		process.properties = new HashMap<String, PropertyValue>();
-		process.properties.put("estimatedPercentPerLane", new PropertySingleValue("50"));
-		process.properties.put("readLength", new PropertySingleValue("100"));
-		process.properties.put("readType", new PropertySingleValue("PE"));
-		process.properties.put("sequencingType", new PropertySingleValue("Hiseq 2500 Rapide"));		
-	
-		Result result = callAction(controllers.processes.api.routes.ref.Processes.save(),fakeRequest("POST","?fromSupportContainerCode="+cs.code).withJsonBody(Json.toJson(process)));			
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-		
-		MapperHelper mh = new MapperHelper();		
-		List<Process> processesResult = mh.convertValue(mh.resultToJsNode(result), new TypeReference<List<Process>>(){});		
-		
-		assertThat(processesResult).isNotNull();
-		
-		for(int i=0; i<processesResult.size();i++){
-			Process newProcess=processesResult.get(i);
-			Logger.info("code Process: "+ newProcess.code);
-			result = callAction(controllers.processes.api.routes.ref.Processes.head(newProcess.code),fakeRequest());
-			assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-			
-			result = callAction(controllers.processes.api.routes.ref.Processes.get(newProcess.code),fakeRequest());
-			assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-			
-			result = callAction(controllers.processes.api.routes.ref.Processes.delete(newProcess.code),fakeRequest());
-			assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);			
-		}		
-	}
-	
-	
 	
 /*	@Test
 	public void validatesaveFromSupportOpgenRun() throws JsonParseException, JsonMappingException, IOException{
@@ -226,65 +191,16 @@ public class ProcessesTest extends AbstractTests{
 		}
 	}
 	
-	@Test
-	public void validateUpdateCode(){
-		//TODO
-	}
 	
 	
-	@Test
-	public void updateNotFound(){
-		Result result = callAction(controllers.processes.api.routes.ref.Processes.update("not_found"),fakeRequest());
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.NOT_FOUND);
-	}
 	
-	@Test
-	public void validateUpdateStateCode(){
-		//TODO
-	}
 	
-	@Test
-	public void delete(){
-		String processCode = "ILLUMINA-RUN-BFB-NULL-2014103112111983";
-		Process process = MongoDBDAO.findByCode(InstanceConstants.PROCESS_COLL_NAME, Process.class, processCode);
-		Result result = callAction(controllers.processes.api.routes.ref.Processes.delete(process.code),fakeRequest());
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, process.containerInputCode);
-		assertThat(container.state.code).isEqualTo("IS");
-		assertThat(container.fromExperimentTypeCodes.contains(processCode)).isEqualTo(false);
-	}
 	
-	@Test
-	public void validateDeleteNotFound(){
-		Result result = callAction(controllers.processes.api.routes.ref.Processes.delete("not_found"),fakeRequest());
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.NOT_FOUND);
-	}
 	
-	@Test
-	public void validateQuery() throws JsonParseException, JsonMappingException, IOException{
-		Result result = null;
+	
+	
+	
 		
-		result = callAction(controllers.processes.api.routes.ref.Processes.list(),fakeRequest());
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-		
-		result = callAction(controllers.processes.api.routes.ref.Processes.list(),fakeRequest("GET","?projectCode=ADI"));
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-		
-		ObjectMapper mapper=new ObjectMapper();
-		List<Process> processes=mapper.readValue(play.test.Helpers.contentAsString(result),new TypeReference<List<Process>>(){});
-		
-		assertThat(processes.size()).isEqualTo(1);
-		assertThat(processes.get(0).projectCode).isEqualTo("ADI");
-		
-		result = callAction(controllers.processes.api.routes.ref.Processes.list(),fakeRequest("GET","?datatable=true&projectCode=XXX"));
-		assertThat(status(result)).isEqualTo(play.mvc.Http.Status.OK);
-		mapper=new ObjectMapper();
-		JsonNode jsonNode=Json.parse(play.test.Helpers.contentAsString(result));
-		DatatableResponseForTest<Process> datatableResponse=mapper.convertValue(jsonNode,new TypeReference<DatatableResponseForTest<Process>>(){});
-		assertThat(datatableResponse.data.size()).isEqualTo(0);
-		
-	}
-	
 	private Double roundValue(Double value)
 	{
 		BigDecimal bg = new BigDecimal(value.toString()).setScale(2, RoundingMode.HALF_UP); 
