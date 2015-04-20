01df00c7d60d4a0ae9395808a9ee88eb23eb30bc
Guillaume Albini <galbini@genoscope.cns.fr>
2015-04-20 14:36:36 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@4925 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/ImportDataCNS.java
+++ app-ngl-data/app/services/instance/ImportDataCNS.java
@@ -30,7 +30,7 @@ public class ImportDataCNS{
 		new TubeImportCNS(Duration.create(1,TimeUnit.MINUTES),Duration.create(60,TimeUnit.MINUTES));
 		new PrepaflowcellImportCNS(Duration.create(2,TimeUnit.MINUTES),Duration.create(15,TimeUnit.MINUTES));
 	    //new RunImportCNS(Duration.create(5,TimeUnit.MINUTES),Duration.create(60,TimeUnit.MINUTES));
-	    new UpdateReadSetCNS(Duration.create(10,TimeUnit.MINUTES),Duration.create(60,TimeUnit.MINUTES));
+	    //new UpdateReadSetCNS(Duration.create(10,TimeUnit.MINUTES),Duration.create(60,TimeUnit.MINUTES));
 	    //Update State and Tara Properties
 	    new UpdateTaraPropertiesCNS(Duration.create(ImportDataUtil.nextExecutionInSeconds(2,00),TimeUnit.SECONDS)
 				,Duration.create(1,TimeUnit.DAYS));
