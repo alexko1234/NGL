28ce9978013c49ea94009ae42fd69a48423ea480
Guillaume Albini <galbini@genoscope.cns.fr>
2015-05-20 14:58:18 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@5069 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- app-ngl-data/app/services/instance/ImportDataCNG.java
+++ app-ngl-data/app/services/instance/ImportDataCNG.java
@@ -12,9 +12,10 @@ import services.instance.project.ProjectImportCNG;
 public class ImportDataCNG {
 	
 	public ImportDataCNG(){
-		new ProjectImportCNG(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
-		new ContainerImportCNG(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
-		new IndexImportCNG(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));		
+		new ProjectImportCNG(Duration.create(1,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
+		new ContainerImportCNG(Duration.create(5,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
+		new IndexImportCNG(Duration.create(10,TimeUnit.SECONDS),Duration.create(1,TimeUnit.DAYS));
+		
 		//new ExperimentImportCNG(Duration.create(4,TimeUnit.SECONDS),Duration.create(60,TimeUnit.MINUTES));
 	}
 
