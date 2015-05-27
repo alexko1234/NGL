42227db7581666072554e2c71522ead5973850aa
Guillaume Albini <galbini@genoscope.cns.fr>
2015-05-27 14:31:35 +0000

    git-svn-id: https://gsphere.genoscope.cns.fr/svn/NGL/trunk/ngl@5109 d0e871a8-0b98-4e59-9d77-5ea3c331444f

--- lib-ngl-common/public/ngl-common/javascripts/services/commons.js
+++ lib-ngl-common/public/ngl-common/javascripts/services/commons.js
@@ -972,7 +972,6 @@ angular.module('commonsServices', []).
 		* @param key
 		* @returns [Array]
 		*/
-    	
     	filter('getArray', ['$parse',function($parse) {
     	    return function(objects, key) {
     	    	if(key && !angular.isString(key))throw "key is not valid, only string is authorized"; 	
@@ -1045,5 +1044,29 @@ angular.module('commonsServices', []).
     	    		
     	    	});
     	    	return possibleValues.length;    	    	
+    	    };
+    	}]).filter('collect', ['$parse',function($parse) {
+    	    return function(array, key) {
+    	    	if (!array || array.length === 0)return undefined;
+    	    	if (!angular.isArray(array) && (angular.isObject(array) || angular.isNumber(array) || angular.isString(array) || angular.isDate(array))) array = [array];
+    	    	else if(!angular.isArray(array)) throw "input is not an array, object, number or string !";
+    	    	
+    	    	if(key && !angular.isString(key))throw "key is not valid, only string is authorized";
+    	    	
+    	    	var possibleValues = [];
+    	    	angular.forEach(array, function(element){
+    	    		if (angular.isObject(element)) {
+    	    			var currentValue = $parse(key)(element);
+    	    			if(undefined !== currentValue && null !== currentValue){
+       	    				Array.prototype.push.apply(possibleValues, currentValue);
+    	    			}
+    	    			
+    	    			
+    	    		}else if (!params.key && angular.isObject(value)){
+    	    			throw "missing key !";
+    	    		}
+    	    		
+    	    	});
+    	    	return possibleValues;    	    	
     	    };
     	}]);
