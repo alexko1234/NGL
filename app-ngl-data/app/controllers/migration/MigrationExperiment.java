027c30e766c4299881e58e2f4a6453778986ee32
Deshayes Yann <ydeshayes@genoscope.cns.fr>
2015-06-10 14:57:00 +0200

    de-revert Migration experiment line and column

--- app-ngl-data/app/controllers/migration/MigrationExperiment.java
+++ app-ngl-data/app/controllers/migration/MigrationExperiment.java
@@ -70,6 +70,9 @@ public class MigrationExperiment extends CommonController{
 				atomicTransfertMethod=new OneToVoidContainer();
 			}
 
+			atomicTransfertMethod.line=String.valueOf(atomics.getValue().position);
+			atomicTransfertMethod.column="1";
+			
 			atomicTransfertMethod.inputContainerUseds=atomics.getValue().getInputContainers();
 			atomicTransfertMethod.outputContainerUseds=atomics.getValue().getOutputContainers();
 			exp.atomicTransfertMethods.add(atomicTransfertMethod);
