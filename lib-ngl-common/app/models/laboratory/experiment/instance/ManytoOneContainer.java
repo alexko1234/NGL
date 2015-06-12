f8afbcc4f524b80ca4da99ceaaa83bc0ef56dd14
mhaquell <mhaquell@genoscope.cns.fr>
2015-06-12 14:39:08 +0200

    volume, concentration in outputContainerUsed

--- lib-ngl-common/app/models/laboratory/experiment/instance/ManytoOneContainer.java
+++ lib-ngl-common/app/models/laboratory/experiment/instance/ManytoOneContainer.java
@@ -85,7 +85,6 @@ public class ManytoOneContainer extends AtomicTransfertMethod{
 				}
 			}
 
-						
 			PropertyValue volume = new PropertySingleValue();
 			PropertyValue concentration = new PropertySingleValue();
 			
