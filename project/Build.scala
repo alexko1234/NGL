13f89f8a484343a3e10b27e0b2c5500c68ad0923
Maud HAQUELLE <mhaquell@genoscope.cns.fr>
2015-07-10 14:58:20 +0200

    New version ngl-sq.1.8.1

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.8-SNAPSHOT"  
+   val sqVersion = "1.8.1-SNAPSHOT"  
    val biVersion = "1.18.3-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.0-SNAPSHOT" 
