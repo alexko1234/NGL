6dc3ed65161104e15fb13a916aaa588daaa65e0b
Maud HAQUELLE <mhaquell@genoscope.cns.fr>
2015-07-10 10:00:44 +0200

    New version ngl-bi.1.18.3

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
    val appVersion = "1.0-SNAPSHOT"
      
    val sqVersion = "1.8-SNAPSHOT"  
-   val biVersion = "1.18.2-SNAPSHOT"
+   val biVersion = "1.18.3-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.0-SNAPSHOT" 
    val subVersion = "1.0-SNAPSHOT"  
