56dea050f1ca5362819da51431714a2d7acbc0a3
mhaquell <mhaquell@genoscope.cns.fr>
2015-09-16 10:32:56 +0200

    Ngl-sq 1.11

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.10-SNAPSHOT"  
+   val sqVersion = "1.11-SNAPSHOT"  
    val biVersion = "1.21-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.0-SNAPSHOT" 
