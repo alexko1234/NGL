76a1ef3ef4bde416b43e0bc0c64305d0d69570ed
mhaquell <mhaquell@genoscope.cns.fr>
2015-09-23 09:31:12 +0200

    View prepa-fc-ordered

---
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/manyToOne/cns/prepa-fc-ordered-ctrl.js
@@ -0,0 +1,134 @@
+angular.module('home').controller('CNSPrepaFlowcellOrderedCtrl',['$scope', '$parse', 'atmToDragNDrop',
+                                                               function($scope, $parse, atmToDragNDrop) {
+	
+	
+
+	var atmToSingleDatatable = $scope.atmService.$atmToSingleDatatable;
+	
+	var columns = [  
+	             {
+		        	 "header":Messages("containers.table.support.number"),
+		        	 "property":"atomicTransfertMethod.line",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"text",
+		        	 "position":0,
+		        	 "extraHeaders":{0:"solution stock"}
+		         },	
+		         {
+		        	 "header":Messages("containers.table.supportCode"),
+		        	 "property":"inputContainer.support.code",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"text",
+		        	 "position":1,
+		        	 "extraHeaders":{0:"solution stock"}
+		         },	
+		         {
+		        	"header":Messages("containers.table.tags"),
+		 			"property": "inputContainer.contents",
+		 			"filter": "getArray:'properties.tag.value'",
+		 			"order":false,
+		 			"hide":true,
+		 			"type":"text",
+		 			"position":2,
+		 			"render":"<div list-resize='cellValue | unique' list-resize-min-size='3'>",
+		        	 "extraHeaders":{0:"solution stock"}
+		         },				         
+				 {
+		        	 "header":Messages("containers.table.concentration") + " (nM)",
+		        	 "property":"inputContainer.mesuredConcentration.value",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"number",
+		        	 "position":5,
+		        	 "extraHeaders":{0:"solution stock"}
+		         },
+		        
+		         {
+		        	 "header":Messages("containers.table.volume") + " (µL)",
+		        	 "property":"inputContainer.mesuredVolume.value",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"number",
+		        	 "position":6,
+		        	 "extraHeaders":{0:"solution stock"}
+		         },
+		         {
+		        	 "header":Messages("containers.table.state.code"),
+		        	 "property":"inputContainer.state.code",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"text",
+					 "filter":"codes:'state'",
+		        	 "position":7,
+		        	 "extraHeaders":{0:"solution stock"}
+		         },
+		         {
+		        	 "header":Messages("containers.table.percentage"),
+		        	 "property":"inputContainerUsed.percentage",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+		        	 "type":"number",
+		        	 "position":50,
+		        	 "extraHeaders":{0:"prep FC"}
+		         },		         
+		         {
+		        	 "header":Messages("containers.table.code"),
+		        	 "property":"outputContainerUsed.code",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+					 "type":"text",
+		        	 "position":400,
+		        	 "extraHeaders":{0:"prep FC"}
+		         },
+		         {
+		        	 "header":Messages("containers.table.stateCode"),
+		        	 "property":"outputContainer.state.code | codes:'state'",
+		        	 "order":true,
+					 "edit":false,
+					 "hide":true,
+					 "type":"text",
+		        	 "position":500,
+		        	 "extraHeaders":{0:"prep FC"}
+		         }
+		         ];
+	
+	//overide defaut method
+	atmToSingleDatatable.convertOutputPropertiesToDatatableColumn = function(property){
+		return   this.$commonATM.convertSinglePropertyToDatatableColumn(property,"outputContainerUsed.experimentProperties.",{"0":"prep FC"});
+		
+	};
+	atmToSingleDatatable.convertInputPropertiesToDatatableColumn = function(property){
+		if(property.displayOrder < 20){		
+			return   this.$commonATM.convertSinglePropertyToDatatableColumn(property,"inputContainerUsed.experimentProperties.",{"0":"denaturation"});
+		}else if(property.displayOrder < 30){
+			return   this.$commonATM.convertSinglePropertyToDatatableColumn(property,"inputContainerUsed.experimentProperties.",{"0":"dilution"});
+		}else if(property.displayOrder < 50){
+			return   this.$commonATM.convertSinglePropertyToDatatableColumn(property,"inputContainerUsed.experimentProperties.",{"0":"prep FC"});
+		}
+	};
+	
+		
+	atmToSingleDatatable.data.setColumnsConfig(columns);
+	atmToSingleDatatable.addExperimentPropertiesToDatatable($scope.experiment.experimentProperties.inputs);
+	
+	$scope.$parent.changeValueOnFlowcellDesign = function(){
+		$scope.atmService.data.updateDatatable();
+		
+		if($scope.experiment.editMode){
+			$scope.message.clazz = "alert alert-warning";
+			$scope.message.text = "Vous venez de modifier une valeur";
+			$scope.message.text += ", vous devez impérativement cliquer sur sauvegarder pour que les calculs de la FDR se remettent à jour.";
+			$scope.message.isDetails = false;
+		}
+	};
+	
+}]);
