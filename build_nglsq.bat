61135471fd57ed62fc580da51b052f4f8ff34b9b
galbini <galbini@genoscope.cns.fr>
2015-10-07 09:12:53 +0200

    change sq version to 1.12

--- build_nglsq.bat
+++
@@ -1,3 +0,0 @@
-play clean-all
-play clean "project ngl-sq" compile run 
-pause
--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.11-SNAPSHOT"  
+   val sqVersion = "1.12-SNAPSHOT"  
    val biVersion = "1.22-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.0-SNAPSHOT" 
