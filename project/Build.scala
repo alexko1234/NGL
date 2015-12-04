08519d4f07c94c4302024b469bb98fee0250c621
Ekrame Jacoby <ejacoby@vm-dev.pc.local>
2015-12-04 17:40:01 +0100

    On branch master Increment version NGL-BI

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
    val appVersion = "1.0-SNAPSHOT"
      
    val sqVersion = "1.15.4-SNAPSHOT"  
-   val biVersion = "1.24-SNAPSHOT"
+   val biVersion = "1.25-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.1-SNAPSHOT" 
    val subVersion = "1.0-SNAPSHOT"  
