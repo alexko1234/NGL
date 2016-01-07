d435e807080f0b4ff984ac5c6474dcedd6fbe2b5
mhaquell <mhaquell@genoscope.cns.fr>
2016-01-07 16:19:21 +0100

    Update value treatment

--- app-ngl-data/app/services/description/run/TreatmentServiceCNS.java
+++ app-ngl-data/app/services/description/run/TreatmentServiceCNS.java
@@ -751,7 +751,7 @@ public class TreatmentServiceCNS extends AbstractTreatmentService {
 		
 		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version MinKNOW","minKnowVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","0.50.2.15"));
-        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version Metrichor","metrichorVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","2.34.3_V1.48"));
+        propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version Metrichor","metrichorVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","2.36.2_V1.62"));
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Metrichor run ID","metrichorRunID", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), Long.class, false, "single"));
         
         propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Channels with Reads","minknowChannelsWithReads", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), Long.class, false, "single"));
