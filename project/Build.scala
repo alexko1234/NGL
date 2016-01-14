8c3daab57af73ce232b68748dd6df0bdee3d68f7
mhaquell <mhaquell@genoscope.cns.fr>
2016-01-14 16:56:30 +0100

    version NGL-SQ-1.15.7

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.15.6-SNAPSHOT"  
+   val sqVersion = "1.15.7-SNAPSHOT"  
    val biVersion = "1.26-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.1-SNAPSHOT" 
