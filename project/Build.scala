914c009b2c89030b00792a1c316e563392f41aa0
galbini <galbini@genoscope.cns.fr>
2016-02-10 15:33:15 +0100

    change version ngl-sq to 1.17.0

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.17.X-SNAPSHOT"  
+   val sqVersion = "1.17.0-SNAPSHOT"  
    val biVersion = "1.27-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.1-SNAPSHOT" 
