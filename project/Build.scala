ad0628caaa0446e5d72aedb0aa8c038effcee861
galbini <galbini@genoscope.cns.fr>
2016-02-22 16:52:47 +0100

    change bi version to 1.28.1

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
      
 
    val sqVersion = "1.18.1-SNAPSHOT"  
-   val biVersion = "1.28-SNAPSHOT"
+   val biVersion = "1.28.1-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.1-SNAPSHOT" 
    val subVersion = "1.0-SNAPSHOT"  
