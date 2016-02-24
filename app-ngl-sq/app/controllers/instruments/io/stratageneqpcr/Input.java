50ba5697282c90d88acaa0e449c5a21541773363
galbini <galbini@genoscope.cns.fr>
2016-02-24 10:37:48 +0100

    push excel methof on abstractinput

--- app-ngl-sq/app/controllers/instruments/io/stratageneqpcr/Input.java
+++ app-ngl-sq/app/controllers/instruments/io/stratageneqpcr/Input.java
@@ -69,24 +69,6 @@ public class Input extends AbstractInput {
 		return experiment;
 	}
 	
-	private static String getStringValue(Cell cell){
-		if(Cell.CELL_TYPE_STRING == cell.getCellType()){
-			return cell.getStringCellValue();
-		}else{
-			return null;
-		}
-	}
 	
-	private static Double getNumericValue(Cell cell) {
-		
-		if(Cell.CELL_TYPE_NUMERIC == cell.getCellType() || Cell.CELL_TYPE_FORMULA == cell.getCellType()){
-			return cell.getNumericCellValue();
-		}else if(Cell.CELL_TYPE_STRING == cell.getCellType()){
-			return Double.valueOf(cell.getStringCellValue());
-		}else{
-			return null;
-		}
-		
-	}
 
 }
--- app-ngl-sq/app/controllers/instruments/io/utils/AbstractInput.java
+++ app-ngl-sq/app/controllers/instruments/io/utils/AbstractInput.java
@@ -1,6 +1,8 @@
 package controllers.instruments.io.utils;
 
 
+import org.apache.poi.ss.usermodel.Cell;
+
 import models.laboratory.common.instance.property.PropertyFileValue;
 import models.laboratory.experiment.instance.Experiment;
 import validation.ContextValidation;
@@ -8,4 +10,24 @@ import validation.ContextValidation;
 public abstract class AbstractInput {
 	
 	public abstract Experiment importFile(Experiment experiment, PropertyFileValue pfv, ContextValidation contextValidation) throws Exception;
+	
+	protected static String getStringValue(Cell cell){
+		if(Cell.CELL_TYPE_STRING == cell.getCellType()){
+			return cell.getStringCellValue();
+		}else{
+			return null;
+		}
+	}
+	
+	protected static Double getNumericValue(Cell cell) {
+		
+		if(Cell.CELL_TYPE_NUMERIC == cell.getCellType() || Cell.CELL_TYPE_FORMULA == cell.getCellType()){
+			return cell.getNumericCellValue();
+		}else if(Cell.CELL_TYPE_STRING == cell.getCellType()){
+			return Double.valueOf(cell.getStringCellValue());
+		}else{
+			return null;
+		}
+		
+	}
 }
