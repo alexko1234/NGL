ef5789cae354a5964b7a0fe561a519de5ed7f395
Marc Michieli <michieli@genoscope.cns.fr>
2016-04-04 15:57:45 +0200

    vie-container-20160404

--- app-ngl-sq/public/ngl-sq/javascripts/containers/details-ctrl.js
+++ app-ngl-sq/public/ngl-sq/javascripts/containers/details-ctrl.js
@@ -1,8 +1,8 @@
 "use strict";
 
-angular.module('home').controller('DetailsCtrl', ['$scope', '$http', '$routeParams', 'mainService', 'tabService', 'messages', function($scope,$http,$routeParams,mainService,tabService,messages){
+angular.module('home').controller('DetailsCtrl', ['$scope', '$http', '$routeParams', 'mainService', 'tabService', 'lists', 'messages', function($scope,$http,$routeParams,mainService,tabService,lists,messages){
 	
-	$scope.getActiveTabClass = function(value){
+	$scope.getTabClass = function(value){
 		if(value === mainService.get('containerActiveTab')){
 			return 'active';
 		}
@@ -11,52 +11,26 @@ angular.module('home').controller('DetailsCtrl', ['$scope', '$http', '$routePara
 		mainService.put('containerActiveTab', value)
 	};
 	
-	$scope.save = function(){
-		console.log("Sauvegarde !");
-	};
-	$scope.cancel = function(){
-		$scope.messages.clear();
-		mainService.stopEditMode();
-		updateData();
-		console.log("Cancel !");
-	};
-	$scope.activeEditMode = function(){
-		$scope.messages.clear();
-		mainService.startEditMode();
-	};
-	
-	var updateData = function(){
-		$http.get(jsRoutes.controllers.containers.api.Containers.get($routeParams.code).url).success(function(response) {
-			$scope.container = response;
-		});
-	}
-	
-	var isStateMode = function() {
-		return (mainService.isHomePage('state') || ($routeParams.page && $routeParams.page.indexof('state') == 0));
-	}
-	
 	var init = function(){
 		$scope.messages = messages();
+		$scope.lists = lists;
 		mainService.stopEditMode();
-		/*
-			if(isStateMode()){
-				mainService.startEditMode();
-			}
-		*/
+
 		$http.get(jsRoutes.controllers.containers.api.Containers.get($routeParams.code).url).success(function(response) {
-			$scope.container = response;	
-		
-			if(tabService.getTabs().length ==0){
-				/*
-				if(isStateMode()){ //state mode
-					tabService.addTabs({label:Messages('containers.page.tab.state'),href:jsRoutes.controllers.containers.tpl.Containers.home("state").url,remove:true});
-					tabService.addTabs({label:$scope.container.code,href:jsRoutes.controllers.containers.tpl.Containers.state($scope.container.code).url,remove:true});
-				} else { //detail mode
-				*/
-				tabService.addTabs({label:Messages('containers.tab.search'),href:jsRoutes.controllers.containers.tpl.Containers.home("search").url,remove:true});
+			
+			$scope.container = response;
+			console.log($scope.container);
+			
+			if(tabService.getTabs().length == 0){
+				
+				tabService.addTabs({label:Messages('containers.tabs.search'),href:jsRoutes.controllers.containers.tpl.Containers.home("search").url,remove:true});
 				tabService.addTabs({label:$scope.container.code,href:jsRoutes.controllers.containers.tpl.Containers.home($scope.container.code).url,remove:true});
 				tabService.activeTab($scope.getTabs(1));
 			}
+			
+			$scope.lists.refresh.containerSupportCategories();
+			$scope.lists.refresh.experimentTypes({categoryCodes:["transformation"], withoutOneToVoid:false});
+			
 			
 			if(undefined == mainService.get('containerActiveTab')){
 				mainService.put('containerActiveTab', 'general');
