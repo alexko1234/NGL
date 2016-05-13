4b692cb61d3d6b3e0ef19dd1a791fffb4d039bfa
galbini <galbini@genoscope.cns.fr>
2016-05-13 10:42:37 +0200

    merge x management on sample sheet

--- app-ngl-sq/app/controllers/instruments/io/hiseqx/Output.java
+++ app-ngl-sq/app/controllers/instruments/io/hiseqx/Output.java
@@ -1,28 +1,7 @@
 package controllers.instruments.io.hiseqx;
 
-import java.util.List;
-
-import models.laboratory.container.instance.Container;
-import models.laboratory.experiment.instance.Experiment;
-import validation.ContextValidation;
 import controllers.instruments.io.hiseq.HiSeqOutput;
-import controllers.instruments.io.hiseqx.tpl.txt.sampleSheet_1;
-import controllers.instruments.io.utils.AbstractOutput;
-import controllers.instruments.io.utils.File;
-import controllers.instruments.io.utils.OutputHelper;
-import controllers.instruments.io.utils.TagModel;
 
-public class Output extends AbstractOutput {
-	
-	@Override
-	public File generateFile(Experiment experiment, ContextValidation contextValidation) {
-		List<Container> containers = OutputHelper.getInputContainersFromExperiment(experiment);
-		TagModel tagModel = OutputHelper.getTagModel(containers);
-		String content = OutputHelper.format(sampleSheet_1.render(experiment,containers,tagModel).body());
-		String filename = OutputHelper.getInstrumentPath(experiment.instrument.code)+containers.get(0).support.code+".csv";
-		File file = new File(filename, content);
-		OutputHelper.writeFile(file);
-		return file;
-	}
+public class Output extends HiSeqOutput {
 
 }
