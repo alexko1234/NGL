4fdccadfce85be68b04cca5d5be83733efb489df
Marc Michieli <michieli@genoscope.cns.fr>
2016-05-24 18:05:12 +0200

    dev-douchette-20160524

--- app-ngl-sq/public/ngl-sq/javascripts/containerSupports/scanner/services.js
+++ app-ngl-sq/public/ngl-sq/javascripts/containerSupports/scanner/services.js
@@ -1,31 +1,33 @@
 "use strict";
 
 angular.module('ngl-sq.barCodeSearchServices', []).
-factory('barCodeSearchService', ['$http', function($http){
+factory('barCodeSearchService', ['$http','$q', function($http,$q){
 
 	var searchService = {
 			response: undefined,
-			form : undefined,
-			
-			resetForm : function(){
-				this.form = undefined;
-				this.response = undefined;
-			},
+			form: undefined,
 			search : function() { 
-				this.response = undefined;
+				this.response = {};
             	var that = this;
-            	$http({
-            		method: 'GET',
-            		url: jsRoutes.controllers.containers.api.ContainerSupports.get(this.form.code).url
-            	}).then(function success(response) {
-            		that.response = response;
-            		that.form = undefined;
+            	var promise = [];
+            	if(!angular.isUndefined(this.form)){
+            		
+                	promise.push($http.get(jsRoutes.controllers.containers.api.ContainerSupports.get(this.form.code).url));
+                	promise.push($http.get(jsRoutes.controllers.containers.api.Containers.list().url, {params:{supportCodeRegex:this.form.code}}));
+	            	
+                	$q.all(promise).then(function(results) {
+	            		that.response.support = results[0].data;
+	            		if(that.response.support.categoryCode.indexOf('tube')>=0 || that.response.support.categoryCode.indexOf('mapcard')>=0){
+	            			that.response.containers = results[1].data;
+	            		}
+	            		//=> TODO
+	            		that.form = undefined;
+	            		angular.element("#scan").focus();
+	            	});
+            	}else{
+            		this.form = undefined;
             		angular.element("#scan").focus();
-            	}, function error(response){
-            		that.response = response;
-            		that.form = undefined;
-            		angular.element("#scan").focus();
-            	});
+            	}
             }
 	}
 	return searchService;
