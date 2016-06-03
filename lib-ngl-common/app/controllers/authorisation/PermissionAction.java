b4671537727fd865bd8f6083019880c224f49020
galbini <galbini@genoscope.cns.fr>
2016-06-03 16:43:42 +0200

    remove logger

--- lib-ngl-common/app/controllers/authorisation/PermissionAction.java
+++ lib-ngl-common/app/controllers/authorisation/PermissionAction.java
@@ -31,8 +31,6 @@ public class PermissionAction extends Action<Permission> {
 			username = "ngsrg";
 		}else if(userAgent.contains("Honeywell")){
 			username = "scanner";
-		}else{
-			Logger.warn("User agent other "+userAgent);
 		}
 		
 		for(String checkPermission:configuration.value()){
