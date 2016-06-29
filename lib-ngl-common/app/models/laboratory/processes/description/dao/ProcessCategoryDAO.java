def0a787a1e06ed71afe6591870fea2d1c97fe51
mhaquell <mhaquell@genoscope.cns.fr>
2016-06-29 15:31:58 +0200

    Process category remove order by

--- lib-ngl-common/app/models/laboratory/processes/description/dao/ProcessCategoryDAO.java
+++ lib-ngl-common/app/models/laboratory/processes/description/dao/ProcessCategoryDAO.java
@@ -1,9 +1,18 @@
 package models.laboratory.processes.description.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import models.laboratory.processes.description.ProcessCategory;
 import models.utils.dao.AbstractDAODefault;
+import models.utils.dao.DAOException;
 
+import org.apache.poi.ss.formula.functions.T;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
+
+import play.Logger;
 
 @Repository
 public class ProcessCategoryDAO extends AbstractDAODefault<ProcessCategory>{
@@ -12,4 +21,16 @@ public class ProcessCategoryDAO extends AbstractDAODefault<ProcessCategory>{
 		super("process_category",ProcessCategory.class,true);
 	}
 
+	//overload to delete "order by" in sql query
+	public List<ProcessCategory> findAll() throws DAOException
+	{
+		try {
+			String sql = getSqlCommon();
+			Logger.debug(sql);
+			BeanPropertyRowMapper<ProcessCategory> mapper = new BeanPropertyRowMapper<ProcessCategory>(entityClass);
+			return this.jdbcTemplate.query(sql, mapper);
+		} catch (DataAccessException e) {
+			return new ArrayList<ProcessCategory>();
+		}
+	}
 }
