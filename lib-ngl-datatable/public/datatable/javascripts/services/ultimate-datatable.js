5272f60b18e1dcee8adf827ff8faca2b6bcc6477
galbini <galbini@genoscope.cns.fr>
2016-06-29 12:12:48 +0200

    better i18 management in udtHtmlFilter

--- lib-ngl-datatable/public/datatable/javascripts/services/ultimate-datatable.js
+++ lib-ngl-datatable/public/datatable/javascripts/services/ultimate-datatable.js
@@ -3182,21 +3182,23 @@ directive('udtForm', function(){
 		}
 	};
 });;angular.module('ultimateDataTableServices').
-directive("udtHtmlFilter", function($filter) {
+directive("udtHtmlFilter", function($filter, udtI18n) {
 				return {
 					  require: 'ngModel',
 					  link: function(scope, element, attrs, ngModelController) {
-					    ngModelController.$formatters.push(function(data) {
-					    	var convertedData = data;
-					    	  if(attrs.udtHtmlFilter === "datetime"){
-					    			convertedData = $filter('date')(convertedData, scope.udtTableFunctions.messages.Messages("datetime.format"));
-					    	   }else if(attrs.udtHtmlFilter === "date"){
-					    		   	convertedData = $filter('date')(convertedData, scope.udtTableFunctions.messages.Messages("date.format"));
-					    	   }else if(attrs.udtHtmlFilter === "number"){
-					    		   	convertedData = $filter('number')(convertedData);
-					    	   }					    	
-					    	  return convertedData;
-					    }); 
+						  	var messagesService = udtI18n(navigator.languages || navigator.language || navigator.userLanguage);
+						  
+							ngModelController.$formatters.push(function(data) {
+								var convertedData = data;
+								  if(attrs.udtHtmlFilter === "datetime"){
+									  convertedData = $filter('date')(convertedData, messagesService.Messages("datetime.format"));
+							   }else if(attrs.udtHtmlFilter === "date"){
+								   convertedData = $filter('date')(convertedData, messagesService.Messages("date.format"));
+							   }else if(attrs.udtHtmlFilter === "number"){
+								   convertedData = $filter('number')(convertedData);
+								   }					    	
+								  return convertedData;
+							}); 
 					    
 					    ngModelController.$parsers.push(function(data) {
 					    	var convertedData = data;
@@ -3207,6 +3209,13 @@ directive("udtHtmlFilter", function($filter) {
 					    			   convertedData = convertedData*1;
 					    		   }else if( isNaN(convertedData) || convertedData === ""){
 					    			   convertedData = undefined;
+					    		   }
+					    	   }else if(attrs.udtHtmlFilter === "date" && null != convertedData && undefined != convertedData 
+					    			   && angular.isString(convertedData)){
+					    		   if(moment){
+					    			   convertedData = moment(data, messagesService.Messages("date.format").toUpperCase()).valueOf();
+					    		   }else{
+					    			   console.log("mission moment library to convert string to date");
 					    		   }
 					    	   }
 					    	   //TODO GA date and datetime quiz about timestamps
