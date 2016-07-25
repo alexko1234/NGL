768d9acdaf707d61ccf06cbef279d0f2302361ac
galbini <galbini@genoscope.cns.fr>
2016-07-25 10:00:42 +0200

    plate-to-tubes

--- app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToOne/cns/plate-to-tubes-ctrl.js
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToOne/cns/plate-to-tubes-ctrl.js
@@ -3,7 +3,42 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 	var datatableConfig = {
 			name:$scope.experiment.typeCode.toUpperCase(),
 			columns:[			  
-					
+			 		{
+						"header" : Messages("containers.table.supportCode"),
+						"property" : "inputContainer.support.code",
+						"order" : true,
+						"edit" : false,
+						"hide" : true,
+						"type" : "text",
+						"position" : 1,
+						"extraHeaders" : {
+							0 : Messages("experiments.inputs")
+						}
+					},
+					{
+						"header" : Messages("containers.table.support.line"),
+						"property" : "inputContainer.support.line",
+						"order" : true,
+						"edit" : false,
+						"hide" : true,
+						"type" : "text",
+						"position" : 1.1,
+						"extraHeaders" : {
+							0 : Messages("experiments.inputs")
+						}
+					},
+					{
+						"header" : Messages("containers.table.support.column"),
+						"property" : "inputContainer.support.column*1",
+						"order" : true,
+						"edit" : false,
+						"hide" : true,
+						"type" : "text",
+						"position" : 1.2,
+						"extraHeaders" : {
+							0 : Messages("experiments.inputs")
+						}
+					},
 				     {
 			        	"header":Messages("containers.table.sampleCodes"),
 			 			"property": "inputContainer.sampleCodes",
@@ -26,62 +61,7 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 			        	 "position":4,
 			        	 "extraHeaders":{0:Messages("experiments.inputs")}
 			         },
-			         /*
-			         {
-			        	"header":Messages("containers.table.tags"),
-			 			"property": "inputContainer.contents",
-			 			"filter": "getArray:'properties.tag.value'",
-			 			"order":true,
-			 			"hide":true,
-			 			"type":"text",
-			 			"position":4,
-			 			"render":"<div list-resize='cellValue | unique' ' list-resize-min-size='3'>",
-			        	 "extraHeaders":{0:Messages("experiments.inputs")}
-			         },
-					*/			 
-					 {
-			        	 "header" : Messages("containers.table.concentration"),
-			 			 "property": "(inputContainer.concentration.value|number).concat(' '+inputContainer.concentration.unit)",
-			 			 "order":true,
-						 "edit":false,
-						 "hide":true,
-			        	 "type":"text",
-			        	 "position":5,
-			        	 "extraHeaders":{0:Messages("experiments.inputs")}
-			         },
-			         /*
-					 {
-			        	 "header":Messages("containers.table.concentration.unit"),
-			        	 "property":"inputContainer.concentration.unit",
-			        	 "order":true,
-						 "edit":false,
-						 "hide":true,
-			        	 "type":"text",
-			        	 "position":5.1,
-			        	 "extraHeaders":{0:Messages("experiments.inputs")}
-			         },
-			         */
-			         {
-			        	 "header":function(){return Messages("containers.table.volume") + " (µL)"},
-			        	 "property":"inputContainer.volume.value",
-			        	 "order":true,
-						 "edit":false,
-						 "hide":true,
-			        	 "type":"number",
-			        	 "position":6,
-			        	 "extraHeaders":{0:Messages("experiments.inputs")}
-			         },
-			        /*
-			         {
-				 			"header":Messages("containers.table.size"),
-				 			"property": "inputContainer.size.value",
-				 			"order":false,
-				 			"hide":true,
-				 			"type":"text",
-				 			"position":6.5,
-				 			"extraHeaders":{0:Messages("experiments.inputs")}			 						 			
-				 	 },
-				 	 */
+			        
 			         {
 			        	 "header":Messages("containers.table.state.code"),
 			        	 "property":"inputContainer.state.code",
@@ -94,43 +74,17 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 			        	 "extraHeaders":{0:Messages("experiments.inputs")}
 			         },		
 			         {
-			        	 "header":Messages("containers.table.concentration"),
-			        	 "property":"outputContainerUsed.concentration.value",
-			        	 "editDirectives":" udt-change='updatePropertyFromUDT(value,col)' ",
-			        	 "tdClass":"valuationService.valuationCriteriaClass(value.data, experiment.status.criteriaCode, col.property)",
-			        	 "order":true,
-						 "edit":true,
-						 "hide":true,
-			        	 "type":"number",
-			        	 "position":50,
-			        	 "extraHeaders":{0:Messages("experiments.outputs")}
-			         },
-			         {
-			        	 "header":Messages("containers.table.concentration.unit") ,
-			        	 "property":"outputContainerUsed.concentration.unit",
-			        	 "order":true,
-						 "edit":false,
-						 "hide":true,
-			        	 "type":"text",
-			        	 "defaultValues":"nM",
-			        	 "position":51,
-			        	 "extraHeaders":{0:Messages("experiments.outputs")}
-			         },
-			         {
-			        	 "header":Messages("containers.table.volume")+ " (µL)",
-			        	 "property":"outputContainerUsed.volume.value",
-			        	 //utilisation de la directive utd-change car elle capture les modifications du header puis déclenche la function calculVolume 
-			        	 // Si ng-change seul l'evenement utilisateur est capturé, la valeur de la cellule est modifiée mais le calcul non executé
-			        	 "editDirectives":" udt-change='updatePropertyFromUDT(value,col)' ",
-			        	 "tdClass":"valuationService.valuationCriteriaClass(value.data, experiment.status.criteriaCode, col.property)",
-			        	 "order":true,
-						 "edit":true,
-						 "hide":true,
-			        	 "type":"number",
-			        	 "position":52,
-			        	 "extraHeaders":{0:Messages("experiments.outputs")}
-			         },
-			         
+			 			"header" : Messages("containers.table.code"),
+			 			"property" : "outputContainerUsed.code",
+			 			"order" : true,
+			 			"edit" : false,
+			 			"hide" : true,
+			 			"type" : "text",
+			 			"position" : 400,
+			 			"extraHeaders" : {
+			 				0 : Messages("experiments.outputs")
+			 			}
+			 		},			         
 			         {
 			        	 "header":Messages("containers.table.stateCode"),
 			        	 "property":"outputContainer.state.code | codes:'state'",
@@ -198,30 +152,16 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 			extraHeaders:{
 				number:2,
 				dynamic:true,
-			},
-			otherButtons: {
-                active: ($scope.isEditModeAvailable() && $scope.isWorkflowModeAvailable('IP')),
-                template: '<button class="btn btn-default" ng-click="computeLineAndColumn()" data-toggle="tooltip" ng-disabled="!isEditMode()" ng-if="experiment.instrument.outContainerSupportCategoryCode!==\'tube\'"><i class="fa fa-magic"></i> </button>'
-    			
-            }
+			}
 			
 	};
 
-	var updateATM = function(experiment){
-		if(experiment.instrument.outContainerSupportCategoryCode!=="tube"){
-			experiment.atomicTransfertMethods.forEach(function(atm){
-				atm.line = atm.outputContainerUseds[0].locationOnContainerSupport.line;
-				atm.column = atm.outputContainerUseds[0].locationOnContainerSupport.column;
-			});
-		}		
-	}
 	
 	$scope.$on('save', function(e, callbackFunction) {	
 		console.log("call event save");
 		$scope.atmService.data.save();
 		$scope.atmService.viewToExperimentOneToOne($scope.experiment);
 		$scope.updatePropertyUnit($scope.experiment);
-		updateATM($scope.experiment);
 		$scope.$emit('childSaved', callbackFunction);
 	});
 	
@@ -254,161 +194,18 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 		$scope.atmService.data.setEdit();
 	});
 	
-	//Init	
-	if($scope.experiment.instrument.inContainerSupportCategoryCode!=="tube"){
-		datatableConfig.columns.push({
-			"header" : Messages("containers.table.supportCode"),
-			"property" : "inputContainer.support.code",
-			"order" : true,
-			"edit" : false,
-			"hide" : true,
-			"type" : "text",
-			"position" : 1,
-			"extraHeaders" : {
-				0 : Messages("experiments.inputs")
-			}
-		});
-		datatableConfig.columns.push({
-			"header" : Messages("containers.table.support.line"),
-			"property" : "inputContainer.support.line",
-			"order" : true,
-			"edit" : false,
-			"hide" : true,
-			"type" : "text",
-			"position" : 1.1,
-			"extraHeaders" : {
-				0 : Messages("experiments.inputs")
-			}
-		});
-		datatableConfig.columns.push({
-			"header" : Messages("containers.table.support.column"),
-			"property" : "inputContainer.support.column*1",
-			"order" : true,
-			"edit" : false,
-			"hide" : true,
-			"type" : "text",
-			"position" : 1.2,
-			"extraHeaders" : {
-				0 : Messages("experiments.inputs")
-			}
-		});
-
-	} else {
-		datatableConfig.columns.push({
-			"header" : Messages("containers.table.code"),
-			"property" : "inputContainer.support.code",
-			"order" : true,
-			"edit" : false,
-			"hide" : true,
-			"type" : "text",
-			"position" : 1,
-			"extraHeaders" : {
-				0 : Messages("experiments.inputs")
-			}
-		});
-	}
-	
-
-
-	if($scope.experiment.instrument.outContainerSupportCategoryCode !== "tube") {
-		datatableConfig.columns.push({
-			// barcode plaque sortie == support Container used code... faut Used
-			"header" : Messages("containers.table.support.name"),
-			"property" : "outputContainerUsed.locationOnContainerSupport.code",
-			"hide" : true,
-			"type" : "text",
-			"position" : 400,
-			"extraHeaders" : {
-				0 : Messages("experiments.outputs")
-			}
-		});
-		datatableConfig.columns.push({
-			// Ligne
-			"header" : Messages("containers.table.support.line"),
-			"property" : "outputContainerUsed.locationOnContainerSupport.line",
-			"edit" : true,
-			"choiceInList":true,
-			"possibleValues":[{"name":'A',"code":"A"},{"name":'B',"code":"B"},{"name":'C',"code":"C"},{"name":'D',"code":"D"},
-			                  {"name":'E',"code":"E"},{"name":'F',"code":"F"},{"name":'G',"code":"G"},{"name":'H',"code":"H"}],
-			"order" : true,
-			"hide" : true,
-			"type" : "text",
-			"position" : 401,
-			"extraHeaders" : {
-				0 : Messages("experiments.outputs")
-			}
-		});
-		datatableConfig.columns.push({// colonne
-			"header" : Messages("containers.table.support.column"),
-			// astuce GA: pour pouvoir trier les colonnes dans l'ordre naturel
-			// forcer a numerique.=> type:number, property: *1
-			"property" : "outputContainerUsed.locationOnContainerSupport.column",
-			"edit" : true,
-			"choiceInList":true,
-			"possibleValues":[{"name":'1',"code":"1"},{"name":'2',"code":"2"},{"name":'3',"code":"3"},{"name":'4',"code":"4"},
-			                  {"name":'5',"code":"5"},{"name":'6',"code":"6"},{"name":'7',"code":"7"},{"name":'8',"code":"8"},
-			                  {"name":'9',"code":"9"},{"name":'10',"code":"10"},{"name":'11',"code":"11"},{"name":'12',"code":"13"}], 
-			"order" : true,
-			"hide" : true,
-			"type" : "number",
-			"position" : 402,
-			"extraHeaders" : {
-				0 : Messages("experiments.outputs")
-			}
-		});
-
-	} else {
-		datatableConfig.columns.push({
-			"header" : Messages("containers.table.code"),
-			"property" : "outputContainerUsed.code",
-			"order" : true,
-			"edit" : false,
-			"hide" : true,
-			"type" : "text",
-			"position" : 400,
-			"extraHeaders" : {
-				0 : Messages("experiments.outputs")
-			}
-		});
-	}
-	
-	
-	$scope.computeLineAndColumn = function(){
-		var wells = $scope.atmService.data.displayResult;
-		var nbCol = 12;
-		var nbLine = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-		var x = 0;
-		for(var i = 0; i < nbCol ; i++){
-			for(var j = 0; j < nbLine.length; j++){
-				if(x < wells.length){
-					wells[x].data.outputContainerUsed.locationOnContainerSupport.line = nbLine[j]+'';
-					wells[x].data.outputContainerUsed.locationOnContainerSupport.column = i+1;					
-				}
-				x++;
-			}
-		}		
-	};
-	
+	$scope.$watch("experiment.instrument.outContainerSupportCategoryCode", function(){
+		$scope.experiment.instrument.outContainerSupportCategoryCode = "tube";
+	});
 	
 	var atmService = atmToSingleDatatable($scope, datatableConfig);
 	// defined new atomictransfertMethod
 	atmService.newAtomicTransfertMethod =  function(line, column){
-		var getLine = function(line){
-			if($scope.experiment.instrument.outContainerSupportCategoryCode 
-					=== $scope.experiment.instrument.inContainerSupportCategoryCode){
-				return line;
-			}else if($scope.experiment.instrument.outContainerSupportCategoryCode !== "tube" 
-				&& $scope.experiment.instrument.inContainerSupportCategoryCode === "tube") {
-				return undefined;
-			}
-			
-		}
-		var getColumn=getLine;
 		
 		return {
 			class:"OneToOne",
-			line:getLine(line), 
-			column:getColumn(column), 				
+			line:"1", 
+			column:"1", 				
 			inputContainerUseds:new Array(0), 
 			outputContainerUseds:new Array(0)
 		};
@@ -420,144 +217,11 @@ angular.module('home').controller('CNSPlateToTubesCtrl',['$scope' ,'$http','$par
 	}
 	
 	atmService.experimentToView($scope.experiment, $scope.experimentType);
-	$scope.atmService = atmService;
-	
-	$scope.updatePropertyFromUDT = function(value, col){
-		console.log("update from property : "+col.property);
-		
-		if(col.property === 'outputContainerUsed.concentration.value'){
-			computeInputVolume(value.data);
-			computeFinalVolume(value.data);
-			computeBufferVolume(value.data);
-		}else if(col.property === 'outputContainerUsed.volume.value'){
-			computeInputVolume(value.data);
-			computeBufferVolume(value.data);
-		}else if(col.property === 'inputContainerUsed.experimentProperties.inputVolume.value'){
-			computeFinalVolume(value.data);
-			computeBufferVolume(value.data);
-		}
-		
+	if($scope.experiment.instrument.inContainerSupportCategoryCode === "96-well-plate"){
+		$scope.messages.clear();
+		$scope.atmService = atmService;
+	}else{
+		$scope.messages.setError(Messages('experiment.input.error.only-plates'));					
 	}
-	//cOut * vOut / cIn : 
-	//outputContainerUsed.concentration.value * outputContainerUsed.volume.value / inputContainerUsed.concentration.value
-	var computeInputVolume = function(udtData){
-		var getter = $parse("inputContainerUsed.experimentProperties.inputVolume.value");
-		var inputVolume = getter(udtData);
-		
-		var compute = {
-				outputConc : $parse("outputContainerUsed.concentration.value")(udtData),
-				inputConc : $parse("inputContainerUsed.concentration.value")(udtData),
-				outputVol : $parse("outputContainerUsed.volume.value")(udtData),			
-				isReady:function(){
-					return (this.outputConc && this.inputConc && this.outputVol);
-				}
-			};
-		
-		if(compute.isReady()){
-			var result = $parse("(outputConc * outputVol) / inputConc")(compute);
-			console.log("result = "+result);
-			if(angular.isNumber(result) && !isNaN(result)){
-				inputVolume = Math.round(result*10)/10;				
-			}else{
-				inputVolume = undefined;
-			}	
-			getter.assign(udtData, inputVolume);
-		}else{
-			console.log("not ready to computeInputVolume");
-		}
-		
-	}
-	//cIn * inputVolume / cOut : 
-	//inputContainerUsed.concentration.value * inputContainerUsed.experimentProperties.inputVolume.value / outputContainerUsed.concentration.value
-	var computeFinalVolume = function(udtData){
-		var getter = $parse("outputContainerUsed.volume.value");
-		var outputVolume = getter(udtData);
-		
-		var compute = {
-				outputConc : $parse("outputContainerUsed.concentration.value")(udtData),
-				inputConc : $parse("inputContainerUsed.concentration.value")(udtData),
-				inputVol : $parse("inputContainerUsed.experimentProperties.inputVolume.value")(udtData),			
-				isReady:function(){
-					return (this.outputConc && this.inputConc && this.inputVol);
-				}
-			};
-		
-		if(compute.isReady()){
-			var result = $parse("(inputConc * inputVol) / outputConc")(compute);
-			console.log("result = "+result);
-			if(angular.isNumber(result) && !isNaN(result)){
-				outputVolume = Math.round(result*10)/10;				
-			}else{
-				outputVolume = undefined;
-			}	
-			getter.assign(udtData, outputVolume);
-		}else{
-			console.log("not ready to computeFinalVolume");
-		}
-		
-	}
-	//vOut - inputVolume
-	//outputContainerUsed.volume.value - inputContainerUsed.experimentProperties.inputVolume.value
-	var computeBufferVolume = function(udtData){
-		var getter = $parse("inputContainerUsed.experimentProperties.bufferVolume.value");
-		var bufferVolume = getter(udtData);
-		
-		var compute = {
-				inputVol : $parse("inputContainerUsed.experimentProperties.inputVolume.value")(udtData),			
-				outputVol : $parse("outputContainerUsed.volume.value")(udtData),			
-				isReady:function(){
-					return (this.outputVol && this.inputVol);
-				}
-			};
-		
-		if(compute.isReady()){
-			var result = $parse("(outputVol - inputVol)")(compute);
-			console.log("result = "+result);
-			if(angular.isNumber(result) && !isNaN(result)){
-				bufferVolume = Math.round(result*10)/10;				
-			}else{
-				bufferVolume = undefined;
-			}	
-			getter.assign(udtData, bufferVolume);
-		}else{
-			console.log("not ready to computeBufferVolume");
-		}
-	}
-	
-	
-	var generateSampleSheet = function(){
-		$http.post(jsRoutes.controllers.instruments.io.IO.generateFile($scope.experiment.code).url,{})
-		.success(function(data, status, headers, config) {
-			var header = headers("Content-disposition");
-			var filepath = header.split("filename=")[1];
-			
-			var filename = filepath.split(/\/|\\/);
-			filename = filename[filename.length-1];
-			if(data!=null){
-				$scope.messages.clazz="alert alert-success";
-				$scope.messages.text=Messages('experiments.msg.generateSampleSheet.success')+" : "+filepath;
-				$scope.messages.showDetails = false;
-				$scope.messages.open();	
-				
-				var blob = new Blob([data], {type: "text/plain;charset=utf-8"});    					
-				saveAs(blob, filename);
-			}
-		})
-		.error(function(data, status, headers, config) {
-			$scope.messages.clazz = "alert alert-danger";
-			$scope.messages.text = Messages('experiments.msg.generateSampleSheet.error');
-			$scope.messages.setDetails(data);
-			$scope.messages.showDetails = true;
-			$scope.messages.open();				
-		});
-	};
-
-	$scope.setAdditionnalButtons([{
-		isDisabled : function(){return $scope.isNewState();} ,
-		isShow:function(){return !$scope.isNewState();},
-		click:generateSampleSheet,
-		label:Messages("experiments.sampleSheet")
-	}]);
-	
 	
 }]);
