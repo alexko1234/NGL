e951c77c514fdd4a1e97f3e57120d86137fd30ab
galbini <galbini@genoscope.cns.fr>
2016-07-26 16:36:05 +0200

    add value and resolve bun on set projectCode

--- app-ngl-data/app/services/description/instrument/InstrumentServiceCNS.java
+++ app-ngl-data/app/services/description/instrument/InstrumentServiceCNS.java
@@ -545,7 +545,7 @@ public class InstrumentServiceCNS extends AbstractInstrumentService{
 	
 	private static List<PropertyDefinition> getQuBitFluoroskanProperties() throws DAOException {
 		List<PropertyDefinition> l = new ArrayList<PropertyDefinition>();
-		l.add(newPropertiesDefinition("Gamme", "gamme", LevelService.getLevels(Level.CODE.Instrument), String.class, false, null,  newValues("BR","HS","BR et HS"), 
+		l.add(newPropertiesDefinition("Gamme", "gamme", LevelService.getLevels(Level.CODE.Instrument), String.class, false, null,  newValues("BR","HS","BR et HS","RNA HS"), 
 				"single", 10, true, null,null));
 		return l;
 	}
