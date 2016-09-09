54fe78ae3d1bd7aa5fc4238aa81a23341d9f6337
Ekrame Jacoby <ejacoby@vm-dev.pc.local>
2016-09-09 15:18:22 +0200

    On branch NGL-1066_PropagationPropProcessus Code migration propriete inputQuantitu NGL-1068

--- app-ngl-data/app/controllers/migration/cns/MigrationExperimentProperties.java
+++ app-ngl-data/app/controllers/migration/cns/MigrationExperimentProperties.java
@@ -1,10 +1,8 @@
 package controllers.migration.cns;
 
 import java.text.SimpleDateFormat;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.mongojack.DBQuery;
 
@@ -14,8 +12,9 @@ import controllers.CommonController;
 import fr.cea.ig.MongoDBDAO;
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.container.instance.Container;
-import models.laboratory.container.instance.Content;
-import models.laboratory.processes.instance.Process;
+import models.laboratory.container.instance.ContainerSupport;
+import models.laboratory.experiment.instance.Experiment;
+import models.laboratory.experiment.instance.OneToOneContainer;
 import models.laboratory.run.instance.ReadSet;
 import models.utils.InstanceConstants;
 import play.Logger;
@@ -24,100 +23,141 @@ import play.mvc.Result;
 public class MigrationExperimentProperties extends CommonController{
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmm");
-	
+
 	/**
 	 * Propagation d'une propriété d'experiment au niveau content (containers et readSets)
 	 * @param experimentTypeCode
 	 * @param keyProperty
 	 * @return
 	 */
-	public static Result migration(String experimentTypeCode, String keyProperty){
+	public static Result migration(String experimentTypeCode, String keyProperty, String newKeyProperty){
 
 		//backupContainerCollection();
 		//backupReadSetCollection();
-		
+
 		//Get list experiment
-		
-		//Get all inputQuantity to change to libraryInputQuantity
-		
-		//add libraryInputQuantity
-		
-		
-		/*List<Process> processes = MongoDBDAO.find(InstanceConstants.PROCESS_COLL_NAME, Process.class, DBQuery.is("typeCode", processTypeCode).exists("outputContainerSupportCodes")).toList();
+		List<Experiment> experiments = MongoDBDAO.find(InstanceConstants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.is("typeCode", experimentTypeCode)).limit(1).toList();
 
-		
-		//Check same sample Code in list content container child
-		Logger.debug("Check Child Container unique sampleCode");
-		List<String> errorMessagesSample = processes.stream().map(process->process.outputContainerSupportCodes).flatMap(container->container.stream()).filter(containerCode->{
-			//Get container 
-			Set<String> containersError = new HashSet<String>();
-			Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerCode);
-			Set<String> counts = container.contents.stream().collect(Collectors.collectingAndThen(Collectors.groupingBy(c->c.sampleCode, Collectors.counting()), map->{map.values().removeIf(l -> l==1); return map.keySet();}));
-			if(counts.size()>0)
-				containersError.add(container.code);
-			return containersError.size()>0;
-		}).collect(Collectors.toList());
-
-		String error=null;
-		
-		if(errorMessagesSample.size()>0){
-			error=" Child Container with multiple same sample ";
-			for(String errorMessage : errorMessagesSample){
-				error+=errorMessage+" ";
-			}
+		//Get list experiment with no experiment properties
+		for(Experiment exp : experiments){
+			//Logger.debug("Code experiment "+exp.code);
+			//Logger.debug("Classe "+OneToOneContainer.class.getName());
+			exp.atomicTransfertMethods.stream().filter(atm->!atm.getClass().getName().equals(OneToOneContainer.class.getName())).forEach(atm->{
+				Logger.debug("Experiment "+exp.code+" ATM not one to one ");
+			});
+			exp.atomicTransfertMethods.stream().filter(atm->atm.getClass().getName().equals(OneToOneContainer.class.getName())).forEach(atm->{
+				atm.inputContainerUseds.stream().filter(input->input.experimentProperties==null).forEach(input->{
+					Logger.debug("Experiment "+exp.code+" inputContainer "+input.code+" no experiment property");
+				});
+			});
+			exp.atomicTransfertMethods.stream().filter(atm->atm.getClass().getName().equals(OneToOneContainer.class.getName())).forEach(atm->{
+				atm.inputContainerUseds.stream().filter(input->input.experimentProperties!=null && !input.experimentProperties.containsKey(keyProperty)).forEach(input->{
+					Logger.debug("Experiment "+exp.code+" inputContainer "+input.code+" no property "+keyProperty);
+				});
+			});
 		}
 
-		if(error!=null)
-			return badRequest(error);
-		//Get all process to migrate
+		//Get all inputQuantity to change to libraryInputQuantity
+		for(Experiment exp : experiments){
+			Logger.debug("Code experiment "+exp.code);
+			Logger.debug("Classe "+OneToOneContainer.class.getName());
+			exp.atomicTransfertMethods.stream().filter(atm->atm.getClass().getName().equals(OneToOneContainer.class.getName())).forEach(atm->{
+				Logger.debug("ATM "+atm.getClass());
+				atm.inputContainerUseds.stream().filter(input->input.experimentProperties!=null && input.experimentProperties.containsKey(keyProperty)).forEach(input->{
+					Logger.debug("Update property");
+					input.experimentProperties.put(newKeyProperty, input.experimentProperties.get(keyProperty));
+					input.experimentProperties.remove(keyProperty);
+					PropertyValue propValue = input.experimentProperties.get(newKeyProperty);
+					//add property to contents properties to inputContainerUsed
+					if(input.contents.size()>1)
+						Logger.error("Multiple contents ");
+					if(input.contents.size()>0){
+						input.contents.stream().filter(content->!content.properties.containsKey(newKeyProperty)).forEach(content->{
+							Logger.debug("add property to content "+content.sampleCode);
+							content.properties.put(newKeyProperty, propValue);
+						});
+					}
+					//Get container from input
+					updateContainer(input.code, newKeyProperty, propValue);
 
-		Logger.debug("Update "+processes.size()+" processes");
-		for(Process p : processes){
-			//Get property value to add
-			PropertyValue property = p.properties.get(keyProperty);
-			Logger.debug("Update container for process "+p.code+" with property "+property);
-			
-			for(String containerCode : p.outputContainerSupportCodes){
-				//Get container
-				Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, containerCode);
-				container.contents.stream().filter(c->c.sampleCode.equals(p.sampleCode) && c.projectCode.equals(p.projectCode)).forEach(c->{
-					c.properties.put(keyProperty, property);
-				});
-				MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, container);
+					//Get outputContainer
+					atm.outputContainerUseds.stream().forEach(output->{
+						Logger.debug("Get outputContainerCode "+output.code);
+						updateContainer(output.code, newKeyProperty, propValue);
+						//Get list of all Container in process
+						List<String> containerCodes = new ArrayList<String>();
+						getListContainerCode(output.locationOnContainerSupport.code, containerCodes);
+						for(String codeContainer : containerCodes){
+							Logger.debug("Update container code "+codeContainer);
+							updateContainer(codeContainer, newKeyProperty, propValue);
+						}
+					});
 
-				//get readSet and update
-				List<ReadSet> readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.is("sampleOnContainer.containerCode", containerCode)).toList();
-				int sizeAllReadSets = readSets.size();
-				readSets.stream().filter(readset->readset.sampleCode.equals(p.sampleCode) && readset.projectCode.equals(p.projectCode)).forEach(r->{
-					r.sampleOnContainer.properties.put(keyProperty, property);
 				});
-				
-				int sizeUpdateReadSets = readSets.size();
-				
-				if(sizeAllReadSets!=sizeUpdateReadSets){
-					Logger.warn("Check ReadSet for container "+containerCode);
-				}
-				for(ReadSet readSet : readSets){
-					MongoDBDAO.update(InstanceConstants.READSET_ILLUMINA_COLL_NAME, readSet);
-				}
-			}
-		}*/
+			});
+
+			//Update experiment in database
+			//MongoDBDAO.update(InstanceConstants.EXPERIMENT_COLL_NAME, exp);
+		}
+
+
+
 		return ok();
 	}
-	
+
+	private static void getListContainerCode(String codeContainer, List<String> listContainerCode)
+	{
+		List<Experiment> experiments = MongoDBDAO.find(InstanceConstants.EXPERIMENT_COLL_NAME, Experiment.class, DBQuery.in("inputContainerSupportCodes", codeContainer)).toList();
+		for(Experiment exp : experiments){
+			if(exp.outputContainerSupportCodes!=null && exp.outputContainerSupportCodes.size()>0){
+				//Get outputContainerCode from outputContainerSupportCode
+				for(String outputContainerSupportCode : exp.outputContainerSupportCodes){
+					List<Container> containers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.is("support.code", outputContainerSupportCode)).toList();
+					for(Container container : containers){
+						listContainerCode.add(container.code);
+						getListContainerCode(container.code, listContainerCode);
+					}
+				}
+			}
+		}
+	}
+
+	private static void updateContainer(String codeContainer, String newKeyProperty, PropertyValue propValue)
+	{
+		//Get container
+		Container container = MongoDBDAO.findByCode(InstanceConstants.CONTAINER_COLL_NAME, Container.class, codeContainer);
+		//add property to container
+		container.contents.stream().forEach(c->{
+			Logger.debug("Update container "+codeContainer+" for content "+c.sampleCode);
+			c.properties.put(newKeyProperty, propValue);
+		});
+		//MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, container);
+
+		//Get ReadSet to update
+		List<ReadSet> readSets = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.is("sampleOnContainer.containerCode", container.code)).toList();
+		readSets.stream().forEach(r->{
+			Logger.debug("Update ReadSet"+r.code);
+			r.sampleOnContainer.properties.put(newKeyProperty, propValue);
+		});
+
+		for(ReadSet readSet : readSets){
+			//MongoDBDAO.update(InstanceConstants.READSET_ILLUMINA_COLL_NAME, readSet);
+		}
+	}
+
 	private static void backupContainerCollection() {
 		String backupName = InstanceConstants.CONTAINER_COLL_NAME+"_BCK_"+sdf.format(new java.util.Date());
-		
+
 		Logger.info("\tCopie "+InstanceConstants.CONTAINER_COLL_NAME+" start");
 		MongoDBDAO.save(backupName, MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class).toList());
 		Logger.info("\tCopie "+InstanceConstants.CONTAINER_COLL_NAME+" end");
 	}
-	
+
 	private static void backupReadSetCollection() {
 		String backupName = InstanceConstants.READSET_ILLUMINA_COLL_NAME+"_BCK_"+sdf.format(new java.util.Date());
 		BasicDBObject keys = new BasicDBObject();
 		keys.put("treatments", 0);
-		
+
 		Logger.info("\tCopie "+InstanceConstants.READSET_ILLUMINA_COLL_NAME+" start");
 		MongoDBDAO.save(backupName, MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.exists("sampleOnContainer"), keys).toList());
 		Logger.info("\tCopie "+InstanceConstants.CONTAINER_COLL_NAME+" end");
