d4fda0cdb2b3c866b72c15988c8370d3af785341
galbini <galbini@genoscope.cns.fr>
2016-09-09 17:06:25 +0200

    change sq and bi version

--- project/Build.scala
+++ project/Build.scala
@@ -13,8 +13,8 @@ object ApplicationBuild extends Build {
    val appName = "ngl"
    val appVersion = "1.0-SNAPSHOT"
      
-   val sqVersion = "1.27.2-SNAPSHOT"  
-   val biVersion = "1.33.2-SNAPSHOT"
+   val sqVersion = "1.27.3-SNAPSHOT"  
+   val biVersion = "1.33.3-SNAPSHOT"
    val projectsVersion = "1.0-SNAPSHOT"  
    val reagentsVersion = "1.2-SNAPSHOT" 
    val subVersion = "1.1-SNAPSHOT"  
