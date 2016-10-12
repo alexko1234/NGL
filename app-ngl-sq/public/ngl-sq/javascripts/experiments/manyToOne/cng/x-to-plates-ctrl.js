b28fb7bef8702b61b8561cd1fd1e38fb019ed2f6
Fernando Dos Santos <fdsantos@cng.fr>
2016-10-12 18:42:38 +0200

    algo pooling stabilisé

--- app-ngl-sq/public/ngl-sq/javascripts/experiments/manyToOne/cng/x-to-plates-ctrl.js
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/manyToOne/cng/x-to-plates-ctrl.js
@@ -284,15 +284,14 @@ angular.module('home').controller('XToPlatesCtrl',['$scope', '$http','$parse',
     $scope.lastSupportCode=null;
     $scope.lastStorageCode=null;
     
-    
-    // TEST FDS 03/10/2016 selection des parametres de pool
+    // 01/10/2016  gestion du mode pooling "automatique"
     $scope.poolModes=[ {code: 'Lmode',  name: 'ligne'}, 
                        {code: 'Cmode',  name: 'colonne'},
                        {code: 'IDmode', name: 'Illumina Dual Index'},
                        {code: 'ISmode', name: 'Illumina Single Index'}
                        ];
-    $scope.poolMode=null;
     
+    $scope.poolMode=null;
     $scope.poolPlex=null;
     $scope.startColumn=null; 
     $scope.startLine=null;
@@ -304,34 +303,33 @@ angular.module('home').controller('XToPlatesCtrl',['$scope', '$http','$parse',
     	
     	console.log("mode="+ poolMode.name + "("+ poolMode.code+")");
     	console.log("plex="+ poolPlex);
-    	
     	//console.log("selected startColumn="+startColumn);
     	//console.log("selected startLine="+startLine);
-       	var startPos=$scope.setPos96FromLineAndCol_C(startLine, startColumn );
-    	console.log ("start position ="+ startPos);
-
     	//console.log("selected endColumn="+endColumn);
     	//console.log("selected endLine="+endLine);
-    	var endPos=$scope.setPos96FromLineAndCol_C(endLine, endColumn);
-    	console.log ("end position ="+ endPos);
-    	
-    	// startPos ne peux plus etre null ( voir setPos96FromLineAndCol_C )
-    	if ( endPos && startPos && poolPlex ){
+    		
+        // 12/10/2016 si le mode choisi est le mode Ligne il faut ultiliser les fonctions XX_L
+    	if ( poolMode.code === "Lmode") {
+    		var startPos=$scope.setPos96FromLineAndCol_L(startLine, startColumn );
+    		var endPos=$scope.setPos96FromLineAndCol_L(endLine, endColumn);		
+    	} else {
+    		var startPos=$scope.setPos96FromLineAndCol_C(startLine, startColumn );
+    		var endPos=$scope.setPos96FromLineAndCol_C(endLine, endColumn);
+    	}	
+		console.log ("start position ="+ startPos);
+		console.log ("end position ="+ endPos);
+		
+	    if ( endPos && startPos && poolPlex ){	
     		if ( endPos > startPos ){
     			$scope.messages.clear();
-    			
-    		//var nbWells=	endPos - startPos;
-            //	if ( Math.floor(nbWells/poolPlex) === nbWells/poolPlex ){
-            		if      ( poolMode.code === "IDmode"){ $scope.poolIlluminaDual( poolPlex, startPos, endPos ); }
-            		else if ( poolMode.code === "ISmode"){ $scope.poolIlluminaSingle( poolPlex, startPos, endPos ); }
-            		else if ( poolMode.code === "Cmode") { $scope.poolColumn( poolPlex, startPos, endPos ); }
-            		else if ( poolMode.code === "Lmode") { $scope.poolLine( poolPlex, startPos, endPos ); }
-            	//} else {
-            	//	$scope.messages.clazz = "alert alert-danger";
-            	//	$scope.messages.text = Messages("Le nombre de puits choisis n'est pas multiple de "+poolPlex );
-            	//	$scope.messages.showDetails = false;
-            	//	$scope.messages.open();				
-            	//}
+            	if      ( poolMode.code === "IDmode"){ $scope.poolIlluminaDual( poolPlex, startPos, endPos ); }
+            	else if ( poolMode.code === "ISmode"){ $scope.poolIlluminaSingle( poolPlex, startPos, endPos ); }
+            	// 12/10/2016 en fait mode colonn ou ligne sont identiques si le robot est capable de switcher sa numerotation 
+            	// en fonction de l'orientation de la plaque " horizontalement ou verticalement !! Voir D. Derbala
+            	// la methode de pooling etant alors "poolContigu" !!!
+            	//else if ( poolMode.code === "Cmode") { $scope.poolColumn( poolPlex, startPos, endPos ); }
+            	//else if ( poolMode.code === "Lmode") { $scope.poolLine( poolPlex, startPos, endPos ); }
+            	else if ( (poolMode.code === "Lmode") || ( poolMode.code === "Cmode")) { $scope.poolContigu( poolPlex, startPos, endPos ); }
     		}else{
         		$scope.messages.clazz = "alert alert-danger";
         		$scope.messages.text = Messages("Position debut doit etre apres la position fin sur la plaque");
@@ -341,87 +339,146 @@ angular.module('home').controller('XToPlatesCtrl',['$scope', '$http','$parse',
     	}
     }
     
-	// chaque fonction doit verifier que les positions choisies correspondent a un nombre de bloc complet !!!
-	// TODO !!! 
+
     $scope.poolIlluminaDual=function( poolPlex,  startPos, endPos ){
-    	// le pooling illumina est un pooling par blocs contigus...
-    	//    3-plex =bloc 2x2 avec 1 trou; 4-plex= bloc 2X2;
+    	// le pooling illumina Dual index est un pooling par blocs contigus...( voir doc Illumina: TruSeq Library prepPooling)
+    	//    3-plex =bloc 2x2 avec 1 trou; 4-plex=bloc 2X2;
     	//    5-plex= bloc 2x3 avec 1 trou; 6-plex=bloc 2x3; 
     	//    7-plex= bloc 2x4 avec 1 trou; 8-plex-option1 =bloc 2x4; 8-plex-option2=bloc 4x2
     	//    12-plex= bloc 4x3; 16-plex= bloc xx4
-    	//console.log ("POOLING MODE ILLUMINA DUAL INDEX");
+    	console.log ("POOLING MODE ILLUMINA DUAL INDEX");
     	
-    	var bloc=null;
-    	
-    	if      (poolPlex === 3) { bloc={ line: 2, col:2, neighbors:[1, 8]}; }
-    	else if (poolPlex === 4) { bloc={ line: 2, col:2, neighbors:[1, 8,9]}; }
-    	else if (poolPlex === 5) { bloc={ line: 2, col:3, neighbors:[1, 8,9, 16]}; }
-    	else if (poolPlex === 6) { bloc={ line: 2, col:3, neighbors:[1, 8,9, 16,17]}; }
-    	else if (poolPlex === 7) { bloc={ line: 2, col:4, neighbors:[1, 8,9, 16,17, 24]}; }
-    	else if (poolPlex === 8) { bloc={ line: 2, col:4, neighbors:[1, 8,9, 16,17, 24,25]}; } // mode 1
-    	else if (poolPlex === 12){ bloc={ line: 4, col:3, neighbors:[1,2,3, 8,9,10,11, 16,17,18,19,]}; } // mode 1
-    	else if (poolPlex === 16){ bloc={ line: 4, col:4, neighbors:[1,2,3, 8,9,10,11, 16,17,18,19, 24,25,26,27]}; }
+    	var block=null;
+    	// members contient la position relative des autres puits du bloc par rapport au puit "pere" du bloc
+    	// ATTENTION numerotation en mode colonne des puits!!!!=> plaque placee horizontalement sur robot 8 aiguilles
+    	if      (poolPlex === 3) { block={ line: 2, col:2, members:[1,8]}; }  // comme poolPlex 4 mais sans le puit +9
+    	else if (poolPlex === 4) { block={ line: 2, col:2, members:[1,8,9]}; }
+    	else if (poolPlex === 5) { block={ line: 2, col:3, members:[1,8,9,16]}; } // comme poolPlex 6 mais sans le puit +17
+    	else if (poolPlex === 6) { block={ line: 2, col:3, members:[1,8,9,16,17]}; }
+    	else if (poolPlex === 7) { block={ line: 2, col:4, members:[1,8,9,16,17,24]}; } // comme poolPlex 8 mais sans le puit +25
+    	else if (poolPlex === 8) { block={ line: 2, col:4, members:[1,8,9,16,17,24,25]}; } // le mode 2 (=4x2) pas pris encharge..]
+    	else if (poolPlex === 12){ block={ line: 4, col:3, members:[1,2,3,8,9,10,11,16,17,18,19,]}; } // le mode 3x4 n'est pas pertinent pour une plaque 8x12
+    	else if (poolPlex === 16){ block={ line: 4, col:4, members:[1,2,3,8,9,10,11,16,17,18,19,24,25,26,27]}; }
     	else{
     		$scope.messages.clazz = "alert alert-danger";
-    		$scope.messages.text = Messages('plex '+ poolPlex +' not managed in Illumina Dual Index mode');
+    		$scope.messages.text = Messages("plex "+ poolPlex +" not managed in Illumina Dual Index mode");
     		$scope.messages.showDetails = false;
     		$scope.messages.open();				
     	}
 
-    	if ( bloc ){
+    	if ( block ){
     		$scope.messages.clear();
-    		//console.log ("POOLING MODE ILLUMINA DUAL INDEX plex="+ poolPlex + "("+ bloc.line+"/"+bloc.col+")" );
+    		//console.log ("POOLING MODE ILLUMINA DUAL INDEX plex="+ poolPlex + "("+ block.line+"/"+block.col+")" );
     		
-    	    var blocSize=bloc.line*bloc.col;
+    	    var blockSize=block.line*block.col;
+    	    var lastBlockMember=block.members.slice(-1)[0];
+    	    
+    	    // verifier que les positions choisies correspondent a un nombre de blocs complet
+    	    //  PB PAS SI SIMPLE !!!!    nbWellsSelected=endPos-StartPos +1;
+    	    //  par exemple si debut =0 et fin =12 en mode block de 4 on a 8 puits et pas 12 (11-0 +1)!!! 
+    	    
+    	    // attention !!! en fonction de la posion de depart  le block  peut se retrouver a cheval le bord de plaque
+    	    // ==> comment controler ca ?????????
 
-    	    //!! cet algo marche pour numerotation a partir de 0 donc:  -1 a startPos, +1 en sortie pour (i) Pool et (n) puit
-    	    // TEST remplacer for par while
-    	    //for ( var i=(startPos-1) ; i <  96 ; i++){
-    	    // NON c'est toujours pas le bon critere de sortie
-    	    var i=(startPos-1);
-    	    var n=0;
-    	    while ( n < (endPos -1) ) {
-    	    	 //[encore !!  thx NW ]
-    	    	var blocparcol = 8/bloc.line; // line toujours diviseur de 8 ( 2 ou 4!!)
-    	        var j= blocparcol *Math.floor(i /blocparcol) * blocSize;
-    	        var k= (i % blocparcol ) * bloc.line;
-    	        var n= j+k ;
-
-    	    	console.log ("pool "+ (i+1) +" commence a puit="+(n+1));
-    	        $scope.makePool ( i+1, n+1, bloc.neighbors );
-    	    	i++;
+    	    var pi=0 //pool index
+    	    var loop=true;
+    	    while ( loop  ) {
+    	    	//[thx NW   encore !!]
+    	    	//!! cet algo marche pour numerotation a partir de 0 donc:  -1 a startPos, +1 en sortie pour (pi) Pool et pour (wi) puit
+    	    	var blockparcol = 8/block.line; // line toujours diviseur de 8 pour l'instant ( 2 ou 4!! voir liste des poolPlex autorisés )
+    	        var j= blockparcol * Math.floor(pi / blockparcol) * blockSize;
+    	        var k= (pi % blockparcol ) * block.line;
+    	        var wi= j + k +(startPos -1);// prendre en compte la position de debut !!
+    	        
+    	        var lastWell=lastBlockMember + (wi+1);
+    	        console.log ("pool "+ (pi+1) +" commence au puit "+(wi+1)+" et fini au puit "+lastWell);
+    	        $scope.createPoolBlock( pi+1, wi+1, block.members );
+    	        
+    	        // il faut sortir quand le plus grand indice de puit du block courant est plus grand que la position de fin demandee
+    	        // probleme pour les blocs a trou...compter les trous
+    	        var blockHoles=blockSize - block.members.length -1;
+    	        
+        	    if (lastWell  >=  ( endPos - blockHoles )) { 
+    	        	console.log ("exit loop!!!"); 
+    	        	loop=false
+    	        } 
+    	    	pi++;
    			}
     	}
     }// end poolIlluminaDual
     
     $scope.poolIlluminaSingle=function( poolPlex,  startPos, endPos ){
     	console.log ("POOLINGMODE ILLUMINA SINGLE INDEX");
+    	console.log ("....TODO....");
     } 
     
-    $scope.poolColumn=function( poolPlex,  startPos, endPos ){
-    	console.log ("POOLING COLUMN MODE");
+    $scope.poolContigu=function( poolPlex,  startPos, endPos ){
+    	console.log ("POOLING MODE 'CONTIGU'");
     	
- 
-		//var nbWells=endPos - startPos +1;
-		//var nbPools= Math.floor(nbWells / nbPlex);
-		//console.log ("nbPools="+ nbPools);
-		//var rest= nbWells - (nbPools * nbPlex);// il doit y avoir une method Math pour ca !!
-		//console.log ("reste="+ rest);
-
-    }
+		var nbWells=endPos - startPos +1;
+		var nbPools= Math.floor(nbWells / poolPlex);
+		console.log ("nbPools="+ nbPools);
+		
+		var remain= nbWells % poolPlex;
+		//le dernier pool est donc un pool reduit
+		console.log ("reste="+ remain);
+		
+		// ici les indices pi et wi commencent a 1
+		var pi=1;
+		var pool=[];
+		for ( var wi= startPos; wi < (endPos+1); wi++){
+			 //console.log (">>ajouter puit "+ wi +" dans pool "+ pi);
+			 pool.push(wi);
+			 // si wi multiple de poolPlex alors on change de pool 
+			 // ATTENTION prise en compte de la position de debut 	 
+			 if  ((wi - startPos +1 )%poolPlex == 0 ) {
+				 $scope.createPoolContigu( pi, pool);
+				 var pool=[];
+				 pi++;
+			 }
+			 // et le pool reduitfinal eventuel
+			 if (( wi == endPos )&& ( remain > 0)) {
+				 console.log ("pool final avec le reste");
+				 $scope.createPoolContigu( pi, pool); 
+			}
+		}
+    } 
     
-    $scope.poolLine=function( poolPlex,  startPos, endPos ){
-    	console.log ("POOLING LINE MODE");	
-    }
-    
-    $scope.makePool=function(p, firstWell, otherWells ){
-    	console.log ("making pool "+ p);
-    	console.log ("firstWell="+ firstWell)
-    	// utiliser plutot forEach ???
-    	for (var j=0; j < otherWells.length; j++){
-    		console.log ("nextWell="+  (firstWell+ otherWells[j]));
+    $scope.createPoolBlock=function(p, firstWell, otherWellsArray ){
+    	console.log ("creating pool "+ p);
+    	
+    	if ( $scope.isValidForPool(firstWell) ) {
+    	   console.log ("add well="+ firstWell+" in pool");
+    	  //add2Pool.....
+        }
+    	
+    	// utiliser plutot forEach ???NON plus lent et pas standard...
+    	for (var j=0; j < otherWellsArray.length; j++){
+    		if ( $scope.isValidForPool(firstWell + otherWellsArray[j]) ) {
+    		   console.log ("add well="+  (firstWell+ otherWellsArray[j]) +" in pool" );
+    		   //add2Pool...
+    		}
     	}
     }
     
+    $scope.createPoolContigu=function(p, wellsArray ){
+    	// pour les mode ligne ou colonne
+    	console.log ("creating pool "+ p);
+    	
+    	// utiliser plutot forEach ???NON plus lent et pas standard...
+    	for (var j=0; j < wellsArray.length; j++){
+    		if ( $scope.isValidForPool( wellsArray[j]) ) {
+    		   console.log ("add well="+  wellsArray[j] +" in pool" );
+    		   //add2Pool...
+    		}
+    	}
+    }
+    
+    $scope.isValidForPool=function(well){
+    	//TODO
+       	//  -1- il doit y avoir qq chose dans le puits
+    	//  -2- concentration minimale ???
+    	console.log ("Check  valid criteria for well "+well);
+    }
     
 }]);
