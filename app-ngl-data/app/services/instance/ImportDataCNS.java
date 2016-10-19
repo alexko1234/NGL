25ac52de0b219449417847a854f34a9cb6ee5f4a
mhaquell <mhaquell@genoscope.cns.fr>
2016-10-19 14:29:02 +0200

    NGL-DATA import bq ampli et sizing in NGL

--- app-ngl-data/app/services/instance/ImportDataCNS.java
+++ app-ngl-data/app/services/instance/ImportDataCNS.java
@@ -40,17 +40,17 @@ public class ImportDataCNS{
 	    
 		new RunExtImportCNS(Duration.create(10,TimeUnit.MINUTES),Duration.create(12,TimeUnit.HOURS));
 
-		new SolutionStockImportCNS(Duration.create(5,TimeUnit.SECONDS),Duration.create(5,TimeUnit.MINUTES));
+		new SolutionStockImportCNS(Duration.create(30,TimeUnit.SECONDS),Duration.create(10,TimeUnit.MINUTES));
 		new UpdateSolutionStockCNS(Duration.create(20,TimeUnit.SECONDS),Duration.create(5,TimeUnit.MINUTES));
 		
 		//Update NCBI scientificName and lineage for Sample
 		new UpdateSampleNCBITaxonCNS(Duration.create(5,TimeUnit.MINUTES),Duration.create(6,TimeUnit.HOURS));
 	
-		/*	
-		new BanqueAmpliImportCNS(Duration.create(5,TimeUnit.SECONDS),Duration.create(10,TimeUnit.MINUTES));
-		new SizingImportCNS(Duration.create(10,TimeUnit.SECONDS),Duration.create(10,TimeUnit.MINUTES));
+			
+		new BanqueAmpliImportCNS(Duration.create(5,TimeUnit.SECONDS),Duration.create(5,TimeUnit.MINUTES));
+		new SizingImportCNS(Duration.create(10,TimeUnit.SECONDS),Duration.create(5,TimeUnit.MINUTES));
 		
-		
+		/*
 		new UpdateSizingCNS(Duration.create(1,TimeUnit.MINUTES),Duration.create(10,TimeUnit.MINUTES));
 		new UpdateAmpliCNS(Duration.create(1,TimeUnit.MINUTES),Duration.create(10,TimeUnit.MINUTES));
 		*/
