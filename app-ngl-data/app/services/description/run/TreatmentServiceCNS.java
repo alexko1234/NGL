b404d549f08acd555eea8036ef6457ff6b09f0a8
Ekrame Jacoby <ejacoby@vm-dev.pc.local>
2016-10-28 10:51:24 +0200

    On branch master SUPSQ-2298 : Version metrichor 2.43.1

--- app-ngl-data/app/services/description/run/TreatmentServiceCNS.java
+++ app-ngl-data/app/services/description/run/TreatmentServiceCNS.java
@@ -808,7 +808,7 @@ public class TreatmentServiceCNS extends AbstractTreatmentService {
 
 		List<PropertyDefinition> propertyDefinitions = new ArrayList<PropertyDefinition>();
 		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version MinKNOW","minKnowVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","1.1.15"));
-		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version Metrichor","metrichorVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","2.42.2"));
+		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version Metrichor","metrichorVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","2.43.1"));
 		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Nom workflow","metrichorWorkflowName", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","2D Basecalling RNN for NSK007"));
 		propertyDefinitions.add(DescriptionFactory.newPropertiesDefinition("Version workflow","metrichorWorkflowVersion", LevelService.getLevels(Level.CODE.Run, Level.CODE.Default), String.class, false, "single","1.107"));
 
