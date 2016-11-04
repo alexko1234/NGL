6b03f947df436f945e316ca10cffbd0004e68a32
mhaquell <mhaquell@genoscope.cns.fr>
2016-11-04 16:54:34 +0100

    NGL-636 tube update createDate container tube + container bq ampli ext add properties.receptionDate

--- app-ngl-data/app/controllers/migration/cns/MigrationContainerProperties.java
+++ app-ngl-data/app/controllers/migration/cns/MigrationContainerProperties.java
@@ -1,6 +1,16 @@
 package controllers.migration.cns;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+
+
+
+
+
 
 
 
@@ -8,6 +18,7 @@ import java.util.List;
 
 import models.LimsCNSDAO;
 import models.laboratory.common.instance.PropertyValue;
+import models.laboratory.common.instance.TraceInformation;
 import models.laboratory.common.instance.property.PropertySingleValue;
 import models.laboratory.container.instance.Container;
 import models.laboratory.container.instance.Content;
@@ -18,10 +29,16 @@ import models.utils.InstanceConstants;
 
 
 
+
+
+
+
+
 import org.mongojack.DBQuery;
 
 
 import org.mongojack.DBUpdate;
+import org.springframework.jdbc.core.RowMapper;
 
 import play.Logger;
 import play.Logger.ALogger;
@@ -38,42 +55,104 @@ public class MigrationContainerProperties  extends CommonController{
 	protected static LimsCNSDAO  limsServices = Spring.getBeanOfType(LimsCNSDAO.class);
 
 	public static Result migration() {
-		ContextValidation contextError=new ContextValidation("ngl-sq");
-		List<Container> containers=null;
+				updateDateCreationTube();
+				updateAmplificationExt();
+				return ok("Migration update container Finish");
+	}
 
-		containers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
-				DBQuery.exists("properties.limsCode").notExists("contents.properties.sampleAliquoteCode").notExists("contents.properties.tag").notIn("fromTransformationTypeCodes","prepa-flowcell")).toList();
+	public static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+	
+	private static void updateDateCreationTube() {
+		String sql="select matmanom,  tubdc from Materielmanip m, Tubeident t where m.matmaco=t.matmaco and matmaInNGL!=null";
+		List<Container> results =  limsServices.jdbcTemplate.query(sql 
+				,new RowMapper<Container>() {
 
-		Logger.info("Nb containers to update :"+containers.size());
+			public Container mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Container container=new Container();
+				container.code=rs.getString("matmanom");
+				container.traceInformation=new TraceInformation(); 
+				try {
+					container.traceInformation.creationDate=formatter.parse(rs.getString("tubdc"));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return container;
+			}
 
-		containers.forEach(c->{
-			MigrationContainerProperties.updateProperties(c, "sampleAliquoteCode", new PropertySingleValue(c.code), c.contents.get(0).sampleCode, null);
-		});
-
+		}); 
 		
-		containers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.exists("properties.limsCode").notExists("contents.properties.sampleAliquoteCode").size("fromTransformationTypeCodes", 1)).toList();
+		for(Container c:results){
+			MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.is("code",c.code),DBUpdate.set("traceInformation.creationDate", c.traceInformation.creationDate));
+		}
+		
+	}
 
-		Logger.info("Nb containers to update :"+containers.size());
-		containers.forEach(container -> {
-			Logger.debug("Container "+container.code);
-			try {
 
-				List<Content> contents =limsServices.findContentsFromContainer("pl_ContentFromContainer @matmanom=?", container.code);
-				contents.forEach(c->{
-					Logger.debug("Content container :"+ container.code+", sample "+c.sampleCode+", tag "+c.properties.get("tag").value);
-					MigrationContainerProperties.updateProperties(container, "sampleAliquoteCode", (PropertySingleValue) c.properties.get("sampleAliquoteCode"), c.sampleCode, c.properties.get("tag").value.toString());
-				});
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	private static void updateAmplificationExt() {
+		String sql="select matmanom, matmadc from Materielmanip m where proco in (168,480) and emnco=18 and matmaInNGL!=null";
+		List<Container> results =  limsServices.jdbcTemplate.query(sql 
+				,new RowMapper<Container>() {
+
+			public Container mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Container container=new Container();
+				container.code=rs.getString("matmanom");
+				container.properties=new HashMap<String, PropertyValue>();
+				try {
+					container.properties.put("receptionDate",new PropertySingleValue(formatter.parse(rs.getString("matmadc"))));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return container;
 			}
 
 		});
-		
 
-		return ok("Migration update sampleAliquoteCode Finish");
+		for(Container c:results){
+			Logger.debug("Container "+c.code+" date "+ c.properties.get("receptionDate").value);
+
+			MongoDBDAO.update(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.is("code",c.code),DBUpdate.set("properties.receptionDate", c.properties.get("receptionDate")));
+		}
 	}
 
+
+	public static void updatesampleAliquoteCode(){
+		ContextValidation contextError=new ContextValidation("ngl-sq");
+		List<Container> containers=null;
+
+	containers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, 
+			DBQuery.exists("properties.limsCode").notExists("contents.properties.sampleAliquoteCode").notExists("contents.properties.tag").notIn("fromTransformationTypeCodes","prepa-flowcell")).toList();
+
+	Logger.info("Nb containers to update :"+containers.size());
+
+	containers.forEach(c->{
+		MigrationContainerProperties.updateProperties(c, "sampleAliquoteCode", new PropertySingleValue(c.code), c.contents.get(0).sampleCode, null);
+	});
+
+	
+	containers = MongoDBDAO.find(InstanceConstants.CONTAINER_COLL_NAME, Container.class, DBQuery.exists("properties.limsCode").notExists("contents.properties.sampleAliquoteCode").size("fromTransformationTypeCodes", 1)).toList();
+
+	Logger.info("Nb containers to update :"+containers.size());
+	containers.forEach(container -> {
+		Logger.debug("Container "+container.code);
+		try {
+
+			List<Content> contents =limsServices.findContentsFromContainer("pl_ContentFromContainer @matmanom=?", container.code);
+			contents.forEach(c->{
+				Logger.debug("Content container :"+ container.code+", sample "+c.sampleCode+", tag "+c.properties.get("tag").value);
+				MigrationContainerProperties.updateProperties(container, "sampleAliquoteCode", (PropertySingleValue) c.properties.get("sampleAliquoteCode"), c.sampleCode, c.properties.get("tag").value.toString());
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	});
+	
+
+	}
+	
 	public static void updateProperties(Container container, String propertyName, PropertySingleValue propertyValue,String sampleCode, String tag){
 		if(propertyValue!=null){
 			Logger.debug("Update Container :"+container.code);
