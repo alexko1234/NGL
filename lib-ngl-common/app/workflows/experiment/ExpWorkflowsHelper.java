3cdbb56c6ce73934384232728ba31eb3e37378bf
galbini <galbini@genoscope.cns.fr>
2017-01-03 16:17:48 +0100

    add comment on OutputContainerUsed

--- lib-ngl-common/app/workflows/experiment/ExpWorkflowsHelper.java
+++ lib-ngl-common/app/workflows/experiment/ExpWorkflowsHelper.java
@@ -1,6 +1,7 @@
 package workflows.experiment;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,9 +22,12 @@ import java.util.stream.Collectors;
 
 
 
+
+
 import models.laboratory.common.description.Level;
 import models.laboratory.common.description.Level.CODE;
 import models.laboratory.common.description.PropertyDefinition;
+import models.laboratory.common.instance.Comment;
 import models.laboratory.common.instance.PropertyValue;
 import models.laboratory.common.instance.State;
 import models.laboratory.common.instance.TBoolean;
@@ -63,6 +67,8 @@ import org.mongojack.DBUpdate;
 import org.mongojack.WriteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 
 
 
@@ -699,6 +705,9 @@ public class ExpWorkflowsHelper {
 			c.state = state;
 			c.traceInformation = traceInformation;
 			c.treeOfLife=tree;
+			if(null != ocu.comment){
+				c.comments = Collections.singletonList(updateComment(ocu.comment, validation));
+			}
 			newContainers.add(c);
 		}
 		
@@ -734,12 +743,23 @@ public class ExpWorkflowsHelper {
 				c.state = state;
 				c.traceInformation = traceInformation;
 				c.treeOfLife=tree;
+				if(null != ocu.comment){
+					c.comments = Collections.singletonList(ocu.comment);
+				}
 				newContainers.add(c);
 			});			
 		}
 
 		return newContainers;
 	}
+
+	private Comment updateComment(Comment comment, ContextValidation validation) {
+		comment.createUser = validation.getUser();
+		comment.creationDate = new Date();
+		comment.code = CodeHelper.getInstance().generateExperimentCommentCode(comment);
+		return comment;
+	}
+
 
 	private Set<String> getSamplesFromContents(List<Content> contents) {
 		return contents.stream().map(c-> c.sampleCode).collect(Collectors.toSet());
