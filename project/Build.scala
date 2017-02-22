8304953c1d0553f419be053989db0ced00248c32
galbini <galbini@genoscope.cns.fr>
2017-02-22 17:18:56 +0100

    next NGL-SQ version

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
      
 
 
-   val sqVersion = "1.28.1-SNAPSHOT"  
+   val sqVersion = "1.29.0-SNAPSHOT"  
    val biVersion = "1.33.9-SNAPSHOT"
 
    val projectsVersion = "1.2-SNAPSHOT"  
