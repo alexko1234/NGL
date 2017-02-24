408dfd1b15b0b591fa5f26026922c5243ec1b7b2
Fernando Dos Santos <fdsantos@cng.fr>
2017-02-24 11:27:04 +0100

    Chromium: gem-generation presque OK

---
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToOne/cng/chromium-gem-generation-ctrl.js
@@ -0,0 +1,300 @@
+// FDS 23/02/2017 -- JIRA NGL-1167
+angular.module('homeeee').controller('ChromiumGemCtrl',['$scope', '$parse',  '$filter', 'atmToSingleDatatable',
+                                                     function($scope, $parse, $filter, atmToSingleDatatable ){
+	var inputExtraHeaders=Messages("experiments.inputs");
+}]);
+
+///////////
+angular.module('home').controller('ChromiumGemCtrllllll',['$scope', '$parse',  '$filter', 'atmToSingleDatatable',
+	                                                     function($scope, $parse, $filter, atmToSingleDatatable ){	
+	var inputExtraHeaders=Messages("experiments.inputs");
+	var outputExtraHeaders=Messages("experiments.outputs");	
+	
+	var datatableConfig = {
+			name: $scope.experiment.typeCode.toUpperCase(),
+
+			"columns":[
+			         //--------------------- INPUT containers section -----------------------
+			         // entree tubes
+					{
+			        	 "header":Messages("containers.table.code"),
+			        	 "property":"inputContainer.code",
+			        	 "order":true,
+						 "hide":true,
+			        	 "type":"text",
+			        	 "position":0,
+			        	 "extraHeaders":{0: inputExtraHeaders}
+			         },	
+			         { // Projet(s)
+			        	"header":Messages("containers.table.projectCodes"),
+			 			"property": "inputContainer.projectCodes",
+			 			"order":true,
+			 			"hide":true,
+			 			"type":"text",
+			 			"position":11,
+			 			"render":"<div list-resize='cellValue' list-resize-min-size='3'>",
+			        	 "extraHeaders":{0: inputExtraHeaders}
+				     },
+				     { // Echantillon(s) 
+			        	"header":Messages("containers.table.sampleCodes"),
+			 			"property": "inputContainer.sampleCodes",
+			 			"order":true,
+			 			"hide":true,
+			 			"type":"text",
+			 			"position":12,
+			 			"render":"<div list-resize='cellValue' list-resize-min-size='3'>",
+			        	"extraHeaders":{0: inputExtraHeaders}
+				     },
+				     { // sampleAliquoteCode 
+				        "header":Messages("containers.table.codeAliquot"),
+				 		"property": "inputContainer.contents", 
+				 		"filter": "getArray:'properties.sampleAliquoteCode.value'",
+				 		"order":true,
+				 		"hide":true,
+				 		"type":"text",
+				 		"position":13,
+				 		"render":"<div list-resize='cellValue' list-resize-min-size='3'>",
+				        "extraHeaders":{0: inputExtraHeaders}
+					 },
+					 { // Concentration
+			        	 "header":Messages("containers.table.concentration") + " (ng/µL)",
+			        	 "property":"inputContainer.concentration.value",
+			        	 "order":true,
+						 "hide":true,
+			        	 "type":"number",
+			        	 "position":14,
+			        	 "extraHeaders":{0: inputExtraHeaders}
+			         },  
+			         { // Volume
+			        	 "header":Messages("containers.table.volume") + " (µL)",
+			        	 "property":"inputContainer.volume.value",
+			        	 "order":true,
+						 "hide":true,
+			        	 "type":"number",
+			        	 "position":15,
+			        	 "extraHeaders":{0: inputExtraHeaders}
+			         },
+			         { // Etat input Container 
+			        	 "header":Messages("containers.table.state.code"),
+			        	 "property":"inputContainer.state.code | codes:'state'",
+			        	 "order":true,
+						 "hide":true,
+			        	 "type":"text",
+			        	 "position":16,
+			        	 "extraHeaders":{0: inputExtraHeaders}
+			         },
+			         //--->  colonnes specifiques experience s'inserent ici  (inputUsed ??)     
+			         
+			         //------------------------- OUTPUT containers section --------------------------
+			         
+			         //--->  colonnes specifiques experience s'inserent ici  (outputUsed ??)
+			         
+		             /// ne pas aficher les containercodes  sauf pour DEBUG
+			            {
+			        	 "header":"DEBUG code",
+			        	 "property":"outputContainer.code",
+			        	 "order":true,
+						 "hide":true,
+						 "edit":false,
+			        	 "type":"text",
+			        	 "position":99,
+			        	 "extraHeaders":{0: outputExtraHeaders}
+			            },
+			         { // Volume avec valeur par defaut
+			        	 "header":Messages("containers.table.volume") + " (µL)",
+			        	 "property":"outputContainerUsed.volume.value",
+			        	 "hide":true,
+			        	 "edit":true,
+			        	 "type":"number",
+			        	 "defaultValues":20,
+			        	 "position":34,
+			        	 "extraHeaders":{0: outputExtraHeaders}
+			         },
+			         { //  barcode du containerSupport strip sortie == support Container used code... faut Used 
+			        	 "header":Messages("containers.table.support.name"),
+			        	 "property":"outputContainerUsed.locationOnContainerSupport.code", 
+						 "hide":true,
+			        	 "type":"text",
+			        	 "position":35,
+			        	 "extraHeaders":{0: outputExtraHeaders}
+			         },      
+			         { // colonne==> position dans le strip
+			        	 "header":Messages("containers.table.support.column"),
+			        	 //"header":"STRIP POS",
+			        	 // astuce GA: pour pouvoir trier les colonnes dans l'ordre naturel forcer a numerique.=> type:number,   property:  *1
+			        	 "property":"outputContainerUsed.locationOnContainerSupport.column*1", 
+			        	 "order":true,
+						 "hide":true,
+			        	 "type":"number",
+			        	 "position":37,
+			        	 "extraHeaders":{0: outputExtraHeaders}
+			         },	
+			         { // Etat outpout container      
+			        	 "header":Messages("containers.table.state.code"),
+			        	 "property":"outputContainer.state.code | codes:'state'",
+			        	 "order":true,
+						 "edit":false,
+						 "hide":true,
+			        	 "type":"text",
+			        	 "position":40,
+			        	 "extraHeaders":{0: outputExtraHeaders}
+			         }
+			         ],
+			"compact":true,
+			"pagination":{
+				"active":false
+			},		
+			"search":{
+				"active":false
+			},
+			"order":{
+				"mode":"local",
+				"active":true//,
+				// FDS : ce tri donne 1,10,11,12,2.... comment avoir un tri 1,2....10,11,12,13 ??
+				//"by":"inputContainer.support.column*1"
+			},
+			"remove":{
+				"active": ($scope.isEditModeAvailable() && $scope.isNewState()),
+				"showButton": ($scope.isEditModeAvailable() && $scope.isNewState()),
+				"mode":"local"
+			},
+			"save":{
+				"active":true,
+	        	"withoutEdit": true,
+	        	"changeClass":false,
+	        	"showButton":false,
+	        	"mode":"local",
+	        	"callback":function(datatable){
+	        		copyContainerSupportCodeAndStorageCodeToDT(datatable);
+	        	}
+			},
+			"hide":{
+				"active":true
+			},
+			"edit":{ // editable si mode=Finished ????
+				active: ($scope.isEditModeAvailable() && $scope.isWorkflowModeAvailable('F')),
+				showButton: ($scope.isEditModeAvailable() && $scope.isWorkflowModeAvailable('F')),
+				byDefault:($scope.isCreationMode()),
+				columnMode:true
+			},
+			"messages":{
+				"active":false,
+				"columnMode":true
+			},
+			"exportCSV":{
+				"active":true,
+				"showButton":true,
+				"delimiter":";",
+				"start":false
+			},
+			"extraHeaders":{
+				"number":2,
+				"dynamic":true,
+			}
+	}; // fin struct datatableConfig
+
+/*
+	$scope.$on('save', function(e, callbackFunction) {	
+		console.log("call event save");
+		$scope.atmService.data.save();
+		$scope.atmService.viewToExperimentOneToOne($scope.experiment);
+		$scope.$emit('childSaved', callbackFunction);
+	});
+	
+	var copyContainerSupportCodeAndStorageCodeToDT = function(datatable){
+
+		var dataMain = datatable.getData();
+		
+		var outputContainerSupportCode = $scope.outputContainerSupport.code;
+		var outputContainerSupportStorageCode = $scope.outputContainerSupport.storageCode;
+
+		if ( null != outputContainerSupportCode && undefined != outputContainerSupportCode){
+			for(var i = 0; i < dataMain.length; i++){
+				
+				var atm = dataMain[i].atomicTransfertMethod;
+				
+				if ( null != atm.positionOnChip ) {
+					console.log(i + "position on chip=" + atm.positionOnChip);	
+					
+					//var newContainerCode = outputContainerSupportCode+"_"+atm.positionOnChip;
+					var newContainerCode = outputContainerSupportCode+"_9" ;
+
+					$parse('outputContainerUsed.code').assign(dataMain[i],newContainerCode);
+					$parse('outputContainerUsed.locationOnContainerSupport.code').assign(dataMain[i],outputContainerSupportCode);
+				
+					if( null != outputContainerSupportStorageCode && undefined != outputContainerSupportStorageCode){
+						$parse('outputContainerUsed.locationOnContainerSupport.storageCode').assign(dataMain[i],outputContainerSupportStorageCode);
+					}
+				}
+			}	
+		}
+		
+	    datatable.setData(dataMain);
+	}
+	
+	// ajout showButton + suppression start = false;
+	$scope.$on('refresh', function(e) {
+		console.log("call event refresh");		
+		var dtConfig = $scope.atmService.data.getConfig();
+		dtConfig.edit.active = ($scope.isEditModeAvailable() && $scope.isWorkflowModeAvailable('F'));
+		dtConfig.edit.showButton = ($scope.isEditModeAvailable() && $scope.isWorkflowModeAvailable('F'));
+		dtConfig.edit.byDefault = false;
+		dtConfig.remove.active = ($scope.isEditModeAvailable() && $scope.isNewState());
+		$scope.atmService.data.setConfig(dtConfig);
+		$scope.atmService.refreshViewFromExperiment($scope.experiment);
+		$scope.$emit('viewRefeshed');
+	});
+	
+	$scope.$on('cancel', function(e) {
+		console.log("call event cancel");
+		$scope.atmService.data.cancel();
+		
+		if($scope.isCreationMode()){
+			var dtConfig = $scope.atmService.data.getConfig();
+			dtConfig.edit.byDefault = false;
+			$scope.atmService.data.setConfig(dtConfig);
+		}
+		
+	});
+	
+	$scope.$on('activeEditMode', function(e) {
+		console.log("call event activeEditMode");
+		$scope.atmService.data.selectAll(true);
+		$scope.atmService.data.setEdit();
+	});
+	
+		
+	//Init
+	
+	var atmService = atmToSingleDatatable($scope, datatableConfig);
+	//defined new atomictransfertMethod
+	atmService.newAtomicTransfertMethod = function(l, c){
+		return {
+			class:"OneToOne",
+			line: undefined, 
+			column: undefined, 				
+			inputContainerUseds:new Array(0), 
+			outputContainerUseds:new Array(0)
+		};
+	};
+	
+	//defined default output unit
+	atmService.defaultOutputUnit = {
+			volume : "µL"
+	};
+	atmService.experimentToView($scope.experiment, $scope.experimentType);
+	
+	$scope.atmService = atmService;
+	
+	$scope.outputContainerSupport = { code : null , storageCode : null};	
+		
+	if ( undefined !== $scope.experiment.atomicTransfertMethods[0]) { 
+		 $scope.outputContainerSupport.code=$scope.experiment.atomicTransfertMethods[0].outputContainerUseds[0].locationOnContainerSupport.code;
+		console.log("previous code: "+ $scope.outputContainerSupport.code);
+	}
+	if ( undefined !== $scope.experiment.atomicTransfertMethods[0]) {
+		 $scope.outputContainerSupport.storageCode=$scope.experiment.atomicTransfertMethods[0].outputContainerUseds[0].locationOnContainerSupport.storageCode;
+		 console.log("previous storageCode: "+ $scope.outputContainerSupport.storageCode);
+	}
+	*/
+}]);
