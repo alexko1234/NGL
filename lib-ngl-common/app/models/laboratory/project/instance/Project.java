96db3ac8e55247cb584fea604553e0ef76f3ceac
galbini <galbini@genoscope.cns.fr>
2017-03-17 16:36:08 +0100

    add projet properties

--- lib-ngl-common/app/models/laboratory/project/instance/Project.java
+++ lib-ngl-common/app/models/laboratory/project/instance/Project.java
@@ -43,8 +43,13 @@ public class Project extends DBObject implements IValidation{
 	public String umbrellaProjectCode;
 	public BioinformaticParameters bioinformaticParameters;
 	public String lastSampleCode;
+	public Integer nbCharactersInSampleCode = 4;
+	public List<String> authorizedUsers;
 	
-
+	public Boolean archive;
+	
+	
+	
 	@Override
 	@JsonIgnore
 	public void validate(ContextValidation contextValidation) {				
