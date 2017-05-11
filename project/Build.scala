d368b64066cc1025e54922c6a700ffb395e8dafc
jacoby <ejacoby@genoscope.cns.fr>
2017-05-11 17:17:01 +0200

    On branch master  Merge master-isoprod-bi

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
 
    val sqVersion = "1.31.0-SNAPSHOT"  
 
-   val biVersion = "1.35.1-SNAPSHOT"
+   val biVersion = "1.36.0-SNAPSHOT"
 
    val projectsVersion = "1.3.0-SNAPSHOT"  
    val reagentsVersion = "1.3.0-SNAPSHOT" 
