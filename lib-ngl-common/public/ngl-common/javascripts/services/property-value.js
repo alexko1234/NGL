fa67c7bd157506e138254e3df22045c2facc945b
jacoby <ejacoby@genoscope.cns.fr>
2017-05-18 14:48:00 +0200

    On branch PbLoadingReport  NGL-1400 : affichage propriete object_list

--- lib-ngl-common/public/ngl-common/javascripts/services/property-value.js
+++ lib-ngl-common/public/ngl-common/javascripts/services/property-value.js
@@ -1,9 +1,8 @@
 angular.module('commonsServices').factory('propertyDefinitions', ['$http', function($http){
 	
-	var datas = undefined;
+	var datas = new Map();
 	
 	var promise = $http.get(jsRoutes.controllers.commons.api.PropertyDefinitions.list().url).success(function(data) {
-		datas = new Map();
 		for(var i=0; i<data.length; i++){
 			datas.set(data[i].code,data[i]);
 		}
@@ -12,9 +11,10 @@ angular.module('commonsServices').factory('propertyDefinitions', ['$http', funct
 	return {
 		datas : datas,
 		get : function get(type){
-			if(datas !== undefined)
+			if(datas !== undefined){
+				
 				return datas.get(type);
-			else
+			}else
 				return null;
 		}
 	};
@@ -26,101 +26,74 @@ angular.module('commonsServices').factory('propertyDefinitions', ['$http', funct
 			valueNgModel :'=',
 			keyNgModel : '='
 		},
-		template : '<div ng-if="propValue._type == \'object_list\'">'
-						+'<object-list value-ng-model="propValue" key-ng-model="keyValue"/>'
+		template : '<div ng-if="valueNgModel._type == \'object_list\'">'
+						+'<object-list value-ng-model="valueNgModel" key-ng-model="keyNgModel" format-ng-model="paragraph"/>'
 					+'</div>'
-					+'<div ng-if="propValue._type == \'single\'">'
-						+'<single value-ng-model="propValue" key-ng-model="keyValue"/>'
-					+'</div>',
-			link : function(scope, element, attr){
-				if(attr.valueNgModel){
-					scope.propValue=scope.valueNgModel;
-				}
-				if(attr.keyNgModel){
-					scope.keyValue=scope.keyNgModel;
-				}
-			}
+					+'<div ng-if="valueNgModel._type == \'single\'">'
+						+'<single value-ng-model="valueNgModel" key-ng-model="keyNgModel"/>'
+					+'</div>'
 		};
 }]).directive('objectList',[ function() {
 	return {
 		restrict : 'EA',
 		scope : {
 			valueNgModel :'=',
-			keyNgModel : '='
+			keyNgModel : '=',
+			formatNgModel : '=',
 		},
-			template :'<div ng-if="format == \'line\'" ng-repeat="property in propValue.value">'
-							+'<div class="row">'
-								+'<label class="col-md-6 col-lg-6 control-label">{{keyValue|codes:\'property_definition\'}}</label>'
-								+'<div class="col-md-6 col-lg-6">'
-									+'<span ng-repeat="(keyProp,valueProp) in property" ng-if="propValue.unit !== null">{{keyProp|codes:\'property_definition.\'+keyValue}} ({{propValue.unit}}) : {{valueProp|codes:\'value.\'+keyProp:false}} </span>'
-									+'<span ng-repeat="(keyProp,valueProp) in property" ng-if="propValue.unit === null"> {{keyProp|codes:\'property_definition.\'+keyValue}} : {{valueProp|codes:\'value.\'+keyProp:false}}</span>'
-								+'</div>'
+			template :'<div ng-if="format == \'line\'" ng-repeat="property in valueNgModel.value">'
+						+'<div class="row">'
+							+'<label class="col-md-6 col-lg-6 control-label">{{keyNgModel|codes:\'property_definition\'}}</label>'
+							+'<div class="col-md-6 col-lg-6">'
+							+'<span ng-repeat="(keyProp,valueProp) in property"> <label class="control-label">{{keyProp|codes:\'property_definition.\'+keyNgModel}}</label> : <value value-ng-model=valueProp key-ng-model=keyProp key-prop-def-ng-model=keyNgModel+\'.\'+keyProp/></span>'
 							+'</div>'
-						+'</div>',
+						+'</div>'
+					  +'</div>'
+					  +'<div ng-if="format == \'paragraph\'" ng-repeat="property in valueNgModel.value">'
+						+'<div class="row">'
+							+'<label class="col-md-6 col-lg-6 control-label">{{keyNgModel|codes:\'property_definition\'}}</label>'
+							+'<div class="col-md-6 col-lg-6">'
+							+'<p ng-repeat="(keyProp,valueProp) in property"> <label class="control-label">{{keyProp|codes:\'property_definition.\'+keyNgModel}}</label> : <value value-ng-model=valueProp key-ng-model=keyProp key-prop-def-ng-model=keyNgModel+\'.\'+keyProp/></p>'
+							+'</div>'
+						+'</div>'
+					  +'</div>',
 			link : function(scope, element, attr){
-				if(attr.valueNgModel){
-					scope.propValue=scope.valueNgModel;
-				}
-				if(attr.keyNgModel){
-					scope.keyValue=scope.keyNgModel;
-				}
 				scope.format="line";
-				if(attr.format && attr.format !== "line"){
-					scope.format = attr.format;
+				if(attr.formatNgModel && attr.formatNgModel !== "line"){
+					scope.format = attr.formatNgModel;
 				}
 			}
 		};
-}]).directive('single',['propertyDefinitions', function(propertyDefinitions) {
+}]).directive('single',[function() {
 	return {
 		restrict : 'EA',
 		scope : {
 			valueNgModel :'=',
 			keyNgModel : '='
-		},
-			template : '<value value-ng-model="propValue" key-ng-model="keyValue" type-ng-model="propertyDefinitions.get(keyValue)"/>',
-			link : function(scope, element, attr){
-				console.log("Scope "+scope);
-				if(attr.valueNgModel){
-					scope.propValue=scope.valueNgModel;
-				}
-				if(attr.keyNgModel){
-					scope.keyValue=scope.keyNgModel;
-				}
-				//scope.typeValue=propertyDefinitions.get(scope.keyValue);
-				scope.propertyDefinitions=propertyDefinitions;
-				
-			}
+		},					
+		template : '<label class="col-md-6 col-lg-6 control-label" ng-if="valueNgModel.unit !== null"> {{keyNgModel|codes:\'property_definition\'}} ({{valueNgModel.unit}})</label>'
+					+'<label class="col-md-6 col-lg-6 control-label" ng-if="valueNgModel.unit === null" ng-bind="keyNgModel|codes:\'property_definition\'"></label>' 
+					+'<p class="col-md-6 col-lg-6 form-control-static"><value value-ng-model=valueNgModel.value key-ng-model=keyNgModel key-prop-def-ng-model=keyNgModel/></p>'			
 		};
 }]).directive('value',['propertyDefinitions', function(propertyDefinitions) {
 	return {
 		restrict : 'EA',
 		scope : {
 			valueNgModel :'=',
-			keyNgModel : '=',
-			typeNgModel : '='
-		},
-		template : '<div ng-switch on="typeValue">'
-						+'<div ng-switch-when="java.util.Date" class="col-md-6 col-lg-6">'
-							+'<label class="control-label" ng-if="propValue.unit === null" ng-bind="keyValue|codes:\'property_definition\'"></label>'
-							+'<label class="control-label" ng-if="propValue.unit === null" ng-bind="keyValue|codes:\'property_definition\'"></label>'
-							+'<p class="form-control-static" ng-bind="propValue.value|codes:\'value.\'+keyValue:false | date:\'@Messages("date.format")\'" />'
-						+'</div>'
-						+'<div ng-switch-default class="col-md-6 col-lg-6">'
-							+'<label class="control-label" ng-if="propValue.unit === null" ng-bind="keyValue|codes:\'property_definition\'"></label>'
-							+'<label class="control-label" ng-if="propValue.unit === null" ng-bind="keyValue|codes:\'property_definition\'"></label>'
-							+'<p class="form-control-static" ng-bind="propValue.value|codes:\'value.\'+keyValue:false" />'
-						+'</div>'
-					+'</div>',
-			link : function(scope, element, attr){
-				if(attr.valueNgModel){
-					scope.propValue=scope.valueNgModel;
-				}
-				if(attr.keyNgModel){
-					scope.keyValue=scope.keyNgModel;
-				}
-				if(attr.typeNgModel){
-					scope.typeValue=scope.typeNgModel;
-				}
-			}
+			//For property type object_list who property key in description table is different because of concatenation of parent key and property key
+			keyPropDefNgModel : '=',
+			keyNgModel : '='
+		},					
+		template :'<span ng-switch on="propertyDefinitions.get(keyPropDefNgModel).valueType">'
+						+'<span ng-switch-when="java.util.Date">'
+							+'<span ng-bind="valueNgModel|codes:\'value.\'+keyNgModel:false|date:\'dd/MM/yyyy\'" />'
+						+'</span>'
+						+'<span ng-switch-default>'
+				   			+'<span ng-bind="valueNgModel|codes:\'value.\'+keyNgModel:false" />'
+				   		+'</span>'
+				   +'</span>' ,
+		link : function(scope, element, attr){
+			scope.propertyDefinitions = propertyDefinitions;
+		}
 		};
 }]);
