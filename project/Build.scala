132ebc6ee635cd595fa0f10dcf8484967209f017
Jean Verdier <vrd@genoscope.cns.fr>
2017-10-17 15:38:44 +0200

    Minimal modification to allow full compilation. Boot sequences are incorrect.

--- project/Build.scala
+++ project/Build.scala
@@ -255,8 +255,8 @@ object ApplicationBuild extends Build {
     publishTo := Some(nexusigpublish),
     packagedArtifacts in publishLocal := {
       val artifacts: Map[sbt.Artifact, java.io.File] = (packagedArtifacts in publishLocal).value
-     val assets: java.io.File = (PlayKeys.playPackageAssets in Compile).value
-     artifacts + (Artifact(moduleName.value, "asset", "jar", "assets") -> assets)
+      val assets: java.io.File = (PlayKeys.playPackageAssets in Compile).value
+      artifacts + (Artifact(moduleName.value, "asset", "jar", "assets") -> assets)
     },
     packagedArtifacts in publish := {
       val artifacts: Map[sbt.Artifact, java.io.File] = (packagedArtifacts in publishLocal).value
@@ -435,9 +435,5 @@ object ApplicationBuild extends Build {
 	)
 		   */
 
-   val depcheck = Project("depcheck",file("depcheck")).enablePlugins(play.sbt.PlayJava).settings(
-		     version := "0.1-SNAPHSHOT",
-		     globSettings
-		   )
 
 }
