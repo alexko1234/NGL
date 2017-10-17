7f006da724472c66cb8bd4800e34b669127987cd
unknown <ejacoby@I0015907.pc.local>
2017-10-17 17:10:16 +0200

    On branch master-isoprod-bi  Increment version isoprod-bi 1.37.5

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
      
 
    val sqVersion = "1.34.0-SNAPSHOT"  
-   val biVersion = "1.37.4-SNAPSHOT"
+   val biVersion = "1.37.5-SNAPSHOT"
 
    val projectsVersion = "1.3.0-SNAPSHOT"  
    val reagentsVersion = "1.3.0-SNAPSHOT" 
