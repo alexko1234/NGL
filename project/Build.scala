24cf4fbd1da1d94cf0159443288abcefdcbccaa9
unknown <ejacoby@I0015907.pc.local>
2017-10-18 16:42:32 +0200

    On branch transfertCCRTCNG  Reactivation des regles transfert CCRT CNG

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,6 @@ object ApplicationBuild extends Build {
    val reagentsVersion = "1.3.0-SNAPSHOT" 
    val subVersion = "1.3.0-SNAPSHOT"  
 
-
    val libDatatableVersion = "1.2-SNAPSHOT"
    val libFrameworkWebVersion = "1.1-SNAPSHOT"
    
