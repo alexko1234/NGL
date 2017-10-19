738f6737ead035dc361bbe043435a0a2948e6e9c
Fernando Dos Santos <fdsantos@cng.fr>
2017-10-19 18:30:43 +0200

    sample-prep creation OK

--- app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToMany/cng/sample-prep-ctrl.js
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToMany/cng/sample-prep-ctrl.js
@@ -5,47 +5,44 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
                                                                function($scope, $parse, $filter, commonAtomicTransfertMethod, mainService, datatable ) {
 	
 	
-   var nbOutputSupport=1;  // essai d'initialisation
-
+   $scope.values=[1,2,3];
+   $scope.nbOutputSupport=$scope.values[0];
+	
    // créer un tableau sur lequel pourra boucler ng-repeat
    // ce tableau est modifié sur onChange de "nbOutputSupport"
    $scope.initOutputContainerSupportCodes = function(nbOutputSupport){
-	   if(nbOutputSupport){
-		    $scope.nbOutputSupport = nbOutputSupport; // GA: a cause d'un probleme de rafraichissement...
-		    
-			if(!$scope.isCreationMode()){
-				 
-			    //récupérer les codes des outputContainers et reinjecter si possible ce qu'il y avait avant
-				previousOutputContainerSupportCodes=$scope.$eval("atomicTransfertMethods|flatArray:'outputContainerUseds'|getArray:'locationOnContainerSupport.code'|unique",$scope.experiment);
-				previousStorageCodes=$scope.$eval("atomicTransfertMethods|flatArray:'outputContainerUseds'|getArray:'locationOnContainerSupport.storageCode'|unique",$scope.experiment);
-				if(previousOutputContainerSupportCodes.length >= nbOutputSupport){
-					//tronquer le tableau
-					$scope.outputContainerSupportCodes = previousOutputContainerSupportCodes.splice(0, nbOutputSupport);
-					// idem pour storageCodes
-					$scope.storageCodes = previousStorageCodes.splice(0, nbOutputSupport);
-					
-				}else if(previousOutputContainerSupportCodes.length < nbOutputSupport){
-					// completer le tableau  //idem pour storageCodes
-					$scope.outputContainerSupportCodes=previousOutputContainerSupportCodes;
-					$scope.storageCodes=previousStorageCodes;
-					for (var j=previousOutputContainerSupportCodes.length ; j<  nbOutputSupport; j++){
-						$scope.outputContainerSupportCodes.push(null);
-						$scope.storageCodes.push(null);
-					}
+	   
+	   //$scope.nbOutputSupport=nbOutputSupport;//??
+	   
+		if($scope.isCreationMode() ){
+			$scope.outputContainerSupportCodes= new Array(nbOutputSupport*1);// *1 pour forcer en numerique nbOutputSupport qui est est un input type text
+		} else {
+		    //en mode edition récupérer les codes des outputContainers et reinjecter si possible ce qu'il y avait avant
+			previousOutputContainerSupportCodes=$scope.$eval("atomicTransfertMethods|flatArray:'outputContainerUseds'|getArray:'locationOnContainerSupport.code'|unique",$scope.experiment);
+			previousStorageCodes=$scope.$eval("atomicTransfertMethods|flatArray:'outputContainerUseds'|getArray:'locationOnContainerSupport.storageCode'|unique",$scope.experiment);
+			if(previousOutputContainerSupportCodes.length >= nbOutputSupport){
+				//tronquer le tableau
+				$scope.outputContainerSupportCodes = previousOutputContainerSupportCodes.splice(0, nbOutputSupport);
+				// idem pour storageCodes
+				$scope.storageCodes = previousStorageCodes.splice(0, nbOutputSupport);
+				
+			}else if(previousOutputContainerSupportCodes.length < nbOutputSupport){
+				// completer le tableau  //idem pour storageCodes
+				$scope.outputContainerSupportCodes=previousOutputContainerSupportCodes;
+				$scope.storageCodes=previousStorageCodes;
+				for (var j=previousOutputContainerSupportCodes.length ; j<  nbOutputSupport; j++){
+					$scope.outputContainerSupportCodes.push(null);
+					$scope.storageCodes.push(null);
 				}
-			} else {
-				$scope.outputContainerSupportCodes= new Array(nbOutputSupport*1);// *1 pour forcer en numerique nbOutputSupport qui est est un input type text
 			}
-		}	
+		} 
 	}
 
-   if(!$scope.isCreationMode()){
-	   getExperimentData();
-    }else{
+   if($scope.isCreationMode()){
 	   // trouver LE/LES codes des supports de tous les containers en entree de l'experience (il peut y en avoir plusieurs..)
 	   $scope.inputSupportCodes = $scope.$eval("getBasket().get()|getArray:'support.code'|unique", mainService); 
 	   
-	   $scope.initOutputContainerSupportCodes (1);// test ajout pour initaliser mais chge rien 
+	   $scope.initOutputContainerSupportCodes(0);// test ajout pour initaliser mais chge rien 
 	   
 	   if ($scope.inputSupportCodes.length > 1){
 		   $scope.messages.clear();
@@ -58,129 +55,10 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 		   $scope.outputContainerSupportCodes=['']; // essai ajout ''
 		   $scope.storageCodes=[''];// essai ajout ''
 	   }
-	}	
-   
-   /* ancienne methode avant ajout datatable  A PURGER SI TOUT OK.....
-    
-   // il faut la callbackFunction pour le $emit 
-   function generateATM(callbackFunction){
-	    console.log ('ouput supports='+ $scope.outputContainerSupportCodes);
-	    console.log ('ouput storages='+ $scope.storageCodes);
-	    $scope.messages.clear();
-	    
-	    if($scope.isCreationMode()){
-	    	console.log ('creation mode...');
-	    	
-	    	//1 initialiser
-	    	$scope.experiment.atomicTransfertMethods = [];
-	    	
-	    	//2 récupérer les inputContainers depuis le basket
-	    	//Each promise object will have a "then" function that can take two arguments, a "success" handler and an "error" handler.
-	    	$commonATM.loadInputContainerFromBasket(mainService.getBasket().get())
-			  .then(function(containers) {	
-				 containers.forEach(function(inputContainer){
-					 
-					//2.1 création de l'ATM
-					var atm = newAtomicTransfertMethod(inputContainer.support.line, inputContainer.support.column);
-					
-					//2.2 création d'1 inputContainerUsed
-					var inputContainerUsed=$commonATM.convertContainerToInputContainerUsed(inputContainer);
-					atm.inputContainerUseds.push(inputContainerUsed);
-					//console.log('inputContainerUsed='+atm.inputContainerUseds[0].code);
-							
-					//2.3 création de j outputContainerUsed
-					for(var j = 0; j < $scope.outputContainerSupportCodes.length ; j++){
-						// si l'utilisateur a bien entré des supportCodes
-						if ($scope.outputContainerSupportCodes[j] !== undefined && $scope.outputContainerSupportCodes[j] !== null && $scope.outputContainerSupportCodes[j] !== ''){
-							var outputContainerUsed = $commonATM.newOutputContainerUsed(defaultOutputUnit, defaultOutputValue, atm.line, atm.column, inputContainer);
-							//affectation du SupportCode
-							outputContainerUsed.locationOnContainerSupport.code=  $scope.outputContainerSupportCodes[j];
-							//affectation du storageCode si defini
-							if ( $scope.storageCodes[j] !== undefined && $scope.storageCodes[j] !== null){			  
-								  outputContainerUsed.locationOnContainerSupport.storageCode=  $scope.storageCodes[j];
-							}
-							atm.outputContainerUseds.push(outputContainerUsed);
-						}
-					}
-					
-					//2.4 mettre l'atm dans l'expérience
-					$scope.experiment.atomicTransfertMethods.push(atm);
-					console.log('atm pushed into experiment');
-				});
-				 
-				// !!! promise=asynchronisme. mettre le $emit('childSaved') ici et plus dans $scope.$on('save',
-				// tester avec le premier ATM
-				if ( $scope.experiment.atomicTransfertMethods[0].outputContainerUseds.length === 0){
-					$scope.$emit('childSavedError', callbackFunction);
-
-				    $scope.messages.clazz = "alert alert-danger";
-				    $scope.messages.text = Messages('experiments.output.error.minSupports',1);
-				    $scope.messages.showDetails = false;
-					$scope.messages.open();   
-					
-				} else {	
-					$scope.$emit('childSaved', callbackFunction);
-			    } 
-			    
-			});	    
-	   } else {
-	    	console.log ('modification mode...');
-	    	// l'utilisateur peut modifier - le nombre de supports en output et/ou les codes barres en output
-	    	
-	    	// TODO  chg algo: se contenter de supprimer les outputContainerUseds au lieu de tout refaire !!
-	    	
-	    	//0 copier les anciens ATMs (on n'a plus le basket...)
-	    	var previousATMs=$scope.experiment.atomicTransfertMethods;
-	    	
-	    	//1 supprimer les ATM de l'expérience (on va les recréer)
-	    	$scope.experiment.atomicTransfertMethods = [];
-	    	
-	    	//2 boucler sur les previousATMs
-	    	previousATMs.forEach(function(prevatm){
-	    		
-			    	//2.1 création de l'ATM 
-					var atm = newAtomicTransfertMethod(prevatm.line, prevatm.column);
-			    
-					//2.2 création d'1 inputContainerUsed (récupérer l'ancien)
-					atm.inputContainerUseds.push (prevatm.inputContainerUseds[0]);
-					
-					//2.3 création de j outputContainerUsed
-					for(var j = 0; j < $scope.outputContainerSupportCodes.length ; j++){
-						// si l'utilisateur a bien entré des supportCodes
-						if ($scope.outputContainerSupportCodes[j] !== undefined && $scope.outputContainerSupportCodes[j] !== null && $scope.outputContainerSupportCodes[j] !== ''){
-							var outputContainerUsed = $commonATM.newOutputContainerUsed(defaultOutputUnit, defaultOutputValue, atm.line, atm.column, prevatm.inputContainerUseds[0]);
-							//affectation du SupportCode 
-							outputContainerUsed.locationOnContainerSupport.code=  $scope.outputContainerSupportCodes[j];
-							//affectation du storageCode si defini
-							if ( $scope.storageCodes[j] !== undefined && $scope.storageCodes[j] !== null){
-							  outputContainerUsed.locationOnContainerSupport.storageCode= $scope.storageCodes[j];
-							}
-							atm.outputContainerUseds.push(outputContainerUsed);
-						}
-					}
-			
-					
-					
-					//2.4 mettre l'atm dans l'expérience
-					$scope.experiment.atomicTransfertMethods.push(atm);
-	    	});
-	    	
-	    	// pas de promise dans ce cas mais obligé de faire le $emit('childSaved de facon similaire...
-	    	// tester avec le premier ATM
-			if ( $scope.experiment.atomicTransfertMethods[0].outputContainerUseds.length === 0){
-				$scope.$emit('childSavedError', callbackFunction);
-				
-			    $scope.messages.clazz = "alert alert-danger";
-			    $scope.messages.text = Messages('experiments.output.error.minSupports',1);
-			    $scope.messages.showDetails = false;
-				$scope.messages.open(); 
-			} else {
-				$scope.$emit('childSaved', callbackFunction);
-			}
-			
-	    }
+	} else {
+		 getExperimentData();
 	}
-	*/
+  
 	
 	function getExperimentData(){	
 		//1 récupérer LE locationOnContainerSupport.code des containers (il ne peux y en avoir qu'un seul)
@@ -188,7 +66,6 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 	     
 		//2 récupérer le nbre de nbOutputSupport en se basant sur atomic[0]: tjrs vrai ??? 
         //  => oui si on bloque les cas de sauvegarde sans  nbOutputSupport
-
 	    $scope.nbOutputSupport=$scope.experiment.atomicTransfertMethods[0].outputContainerUseds.length;
 			  
 	    //3 récupérer les codes des outputContainers  
@@ -203,10 +80,7 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 	$scope.$on('save', function(e, callbackFunction) {	
 		console.log("call event save");
 
-		//  ancienne methode avant ajout Datatable.... generateATM(callbackFunction);
-		
 		$scope.atmService.viewToExperimentOneToMany($scope.experiment);
-		
 		if ( $scope.experiment.atomicTransfertMethods[0].outputContainerUseds.length === 0){
 			$scope.$emit('childSavedError', callbackFunction);
 			
@@ -217,7 +91,6 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 		} else {
 			$scope.$emit('childSaved', callbackFunction);
 		}
-		
 	});
 	
 	$scope.$on('refresh', function(e) {
@@ -436,7 +309,6 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 								 }
 		
 								plateCells[code][ln][col] = sampleCodeAndTags;
-								console.log (">>>> plateCells "+code+"/"+ln+"/"+col+"="+sampleCodeAndTags);
 							}
 							
 							atomicIndex++;
@@ -456,6 +328,7 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 							var allData = [], i = 0;
 							angular.forEach(containers, function(container){
 								var line = {};
+								line.atomicIndex=i++;
 								line.atomicTransfertMethod =  $that.newAtomicTransfertMethod(container.support.line, container.support.column);
 								line.inputContainer = container;
 								line.inputContainerUsed = $that.$commonATM.convertContainerToInputContainerUsed(line.inputContainer);
@@ -497,7 +370,7 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 						this.$commonATM.removeNullProperties(inputContainerUsed.experimentProperties);
 						atm.inputContainerUseds.push(inputContainerUsed);	
 						
-						// Ne recreer les outputs que dans les etats Nouveau ou en cours  ( mais PAS si elle est terminee)
+						// Ne recreer les outputs que dans les etats Nouveau ou en cours (mais PAS si elle est terminee)
 						if('F' !== experiment.state.code){
 							experiment.atomicTransfertMethods[atomicIndex].outputContainerUseds = new Array(0);
 							
@@ -511,11 +384,10 @@ angular.module('home').controller('SamplePrepCtrl',['$scope', '$parse', '$filter
 									if ( $scope.storageCodes[j] !== undefined && $scope.storageCodes[j] !== null){			  
 										  outputContainerUsed.locationOnContainerSupport.storageCode=  $scope.storageCodes[j];
 									}
-									atm.outputContainerUseds.push(outputContainerUsed);
+									atm.outputContainerUseds.push(outputContainerUsed);		
 								}
 							}
 						}
-						
 					}
 					
 					//remove atomic null
