5b801a38060ac17a45ef6bf98e3015bc8f1fa0a3
Jean Verdier <vrd@genoscope.cns.fr>
2017-11-07 14:40:01 +0100

    before rebase

---
+++ lib-ngl-common/app/fr/cea/ig/play/NGLTools.java
@@ -0,0 +1,79 @@
+package fr.cea.ig.play;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import play.i18n.Lang;
+import play.i18n.MessagesApi;
+import play.i18n.Messages;
+
+import play.data.DynamicForm;
+import play.data.FormFactory;
+import play.data.Form;
+
+import fr.cea.ig.play.NGLConfig;
+
+
+// TODO: clean, comment
+
+/**
+ * Class to help transition from the old style globals to new style
+ * DI. An instance of this class is supposed to be injected instead of
+ * using IGGlobals.
+ * 
+ * @author vrd
+ *
+ */
+@Singleton
+public class NGLTools {
+
+	private final NGLConfig config;
+	private final MessagesApi messagesApi;
+	private final FormFactory formFactory;
+	
+	@Inject
+	public NGLTools(NGLConfig config,
+			MessagesApi messagesApi,
+			FormFactory formFactory) {
+		this.config   = config;
+		this.messagesApi = messagesApi;
+		this.formFactory = formFactory;
+	}
+		
+	public NGLConfig config() { 
+		return config;
+	}
+	
+	public String message(String key) {
+		return messages().at(key);
+	}
+	
+	public String messageEnv(String key) {
+		if (config.isNGLEnvProd()) 
+			return message(key);
+		return message(key) + "-" + config.nglEnv();
+	}
+	
+	public String currentUser() {
+		return fr.cea.ig.authentication.Helper.username(play.mvc.Http.Context.current().session());
+	}
+	
+	public Messages messages() {
+		return messagesApi.preferred(new ArrayList<Lang>());
+	}
+
+	public FormFactory formFactory() {
+		return formFactory;
+	}
+	
+	public <T> Form<T> form(Class<T> clazz) {
+		return formFactory().form(clazz);
+	}
+
+	public DynamicForm form() {
+		return formFactory().form();
+	}
+
+}
