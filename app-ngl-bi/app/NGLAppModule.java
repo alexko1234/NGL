6bcb9787931c026dc6aef2ae3dbeaa0b23e6879e
Guillaume Albini <galbini@genoscope.cns.fr>
2017-12-08 14:03:59 +0100

    add missing NGLAppModule

--- app-ngl-bi/app/NGLAppModule.java
+++ app-ngl-bi/app/NGLAppModule.java
@@ -1,7 +1,7 @@
 import play.api.Configuration;
 import play.api.Environment;
 
-public class NGLAppModule extends NGLSQStarterModule {
+public class NGLAppModule extends NGLBIStarterModule {
 	
 	public NGLAppModule(Environment environment, Configuration configuration) {
 		super(environment,configuration);
