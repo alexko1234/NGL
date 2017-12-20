522732a18cf89fde4b398d545604f38e89440421
Fernando Dos Santos <fdsantos@cng.fr>
2017-12-20 15:54:52 +0100

    fluo-quantification: ajout test spectramax sur cas admin

--- app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToVoid/cng/fluo-quantification-ctrl.js
+++ app-ngl-sq/public/ngl-sq/javascripts/experiments/oneToVoid/cng/fluo-quantification-ctrl.js
@@ -52,7 +52,7 @@ angular.module('home').controller('OneToVoidFluoQuantificationCNGCtrl',['$scope'
 	//NGL-1761: pour l'instant seul le fichier spectramax est disponible (masquer le bouton pour qbit)
 	$scope.button = {
 		isShow:function(){
-			return ($scope.isInProgressState() && !$scope.mainService.isEditMode() && ($scope.experiment.instrument.typeCode === "spectramax") || Permissions.check("admin"))
+			return ($scope.isInProgressState() && !$scope.mainService.isEditMode() && ($scope.experiment.instrument.typeCode === "spectramax") || Permissions.check("admin") && ($scope.experiment.instrument.typeCode === "spectramax"))
 			},
 		isFileSet:function(){
 			return ($scope.file === undefined)?"disabled":"";
