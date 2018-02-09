252e3fe7123c16c662899a5a65e3a8e1a43d6cc3
ejacoby <ejacoby@genoscope.cns.fr>
2018-02-09 08:51:58 +0100

    Init version 2.1.1  On branch master-isoprod-bi-2.1.X

--- project/Build.scala
+++ project/Build.scala
@@ -61,7 +61,7 @@ object ApplicationBuild extends Build {
   val appVersion             = "2.0.0"    + distSuffix
 	
 	val sqVersion              = "2.1.0" + distSuffix
-	val biVersion              = "2.2.0" + distSuffix
+	val biVersion              = "2.1.1" + distSuffix
 
 	val projectsVersion        = "2.2.0"  + distSuffix
 	val reagentsVersion        = "2.1.0"  + distSuffix
