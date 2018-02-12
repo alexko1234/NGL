e9d1c931d2bb4d797b705be0704f620f2bf5b7dd
Shahinaz Gas <sgas@genoscope.cns.fr>
2018-02-12 15:52:30 +0100

    Retirer requetes inutiles dans sra.configuration.consultation

--- app-ngl-sub/public/ngl-sub/javascripts/configurations/services.js
+++ app-ngl-sub/public/ngl-sub/javascripts/configurations/services.js
@@ -8,6 +8,7 @@
 		var initListService = function(){
 			if(!isInit){
 				createService.lists.refresh.projects();
+/*				
 				$http.get(jsRoutes.controllers.sra.api.Variables.list().url, {params:{type:'strategySample'}})
 				.success(function(data) {
 					// initialisation de la variable createService.sraVariables.strategySample utilisée dans create.scala.html
@@ -30,7 +31,7 @@
 				.success(function(data) {
 					createService.sraVariables.librarySource = data;																					
 				});
-				
+*/				
 				isInit=true;
 			}
 		};
@@ -178,7 +179,7 @@ var getColumns = function(){
 		if(!isInit){
 			consultationService.lists.refresh.projects();
 			//lists.refresh.states({objectTypeCode:"SRASubmission"});
-			
+			/*
 			$http.get(jsRoutes.controllers.sra.api.Variables.get('strategySample').url)
 				.success(function(data) {
 					// initialisation de la variable sraVariables.strategySample utilisée dans consultation.scala.html
@@ -201,7 +202,7 @@ var getColumns = function(){
 			.success(function(data) {
 				consultationService.sraVariables.librarySource = data;																					
 			});
-			
+			*/
 			isInit=true;
 		}
 	};
