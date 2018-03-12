084cf4b43070dc75afeb7f73af327bc5f70fd4b5
ejacoby <ejacoby@genoscope.cns.fr>
2018-03-12 14:06:43 +0100

    NGL-1923 : Correction bug rules nbCycles Utiles# On branch master-isoprod-bi-2.1.X

--- lib-ngl-common/app/controllers/migration/MigrationUpdateReadSetNbCycles.java
+++ lib-ngl-common/app/controllers/migration/MigrationUpdateReadSetNbCycles.java
@@ -1,39 +1,32 @@
 package controllers.migration;		
 
-//import static fr.cea.ig.play.IGGlobals.akkaSystem;
-
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.regex.Pattern;
 
-import models.laboratory.run.instance.ReadSet;
-import models.laboratory.run.instance.SampleOnContainer;
-import models.utils.InstanceConstants;
-import models.utils.InstanceHelpers;
+import javax.inject.Inject;
 
 import org.mongojack.DBCursor;
 import org.mongojack.DBQuery;
 import org.mongojack.DBUpdate;
 
-import play.Logger;
-import play.Play;
-import play.libs.Akka;
-import play.mvc.Result;
-import rules.services.RulesActor6;
-import rules.services.RulesMessage;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoException;
+
 import akka.actor.ActorRef;
 import akka.actor.Props;
-
-import com.mongodb.BasicDBObject;
-
-import controllers.CommonController;
 import controllers.DocumentController;
 import fr.cea.ig.MongoDBDAO;
 import fr.cea.ig.MongoDBResult;
-import fr.cea.ig.MongoDBResult.Sort;
 import fr.cea.ig.play.NGLContext;
+import models.laboratory.run.instance.ReadSet;
+import models.laboratory.run.instance.Run;
+import models.laboratory.run.instance.Treatment;
+import models.utils.InstanceConstants;
+import play.Logger;
+import play.Play;
+import play.mvc.Result;
+import rules.services.RulesActor6;
+import rules.services.RulesMessage;
 
 /**
  * Update SampleOnContainer on ReadSet
@@ -45,17 +38,27 @@ public class MigrationUpdateReadSetNbCycles extends DocumentController<ReadSet> 
 	//private static ActorRef rulesActor = Akka.system().actorOf(Props.create(RulesActor6.class));
 	private /*static*/ ActorRef rulesActor;// = akkaSystem().actorOf(Props.create(RulesActor6.class));
 	
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+	
+	@Inject
 	public MigrationUpdateReadSetNbCycles(NGLContext ctx) {
 		super(ctx, InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class);
 		rulesActor = ctx.akkaSystem().actorOf(Props.create(RulesActor6.class));
 	}
 	
-	public /*static*/ Result migration(){
+	public /*static*/ Result migration() throws MongoException, ParseException{
 		
 		Logger.info("Start MigrationUpdateReadSetNbCycles");
 		
+		BasicDBObject keys = new BasicDBObject();
+		keys.put("code", 1);
+		keys.put("treatments.ngsrg", 1);
+		keys.put("runSequencingStartDate", 1);
+		keys.put("laneNumber", 1);
+		keys.put("runCode", 1);
+		
 		MongoDBResult<ReadSet> results = MongoDBDAO.find(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, 
-				DBQuery.notExists("treatments.ngsrg.default.nbUsefulCycleRead1"));
+				DBQuery.greaterThan("runSequencingStartDate", sdf.parse("2017/12/01")).notExists("treatments.ngsrg.default.nbUsefulCycleRead1").exists("treatments.ngsrg"),keys);
 		
 		Logger.info("Update nb readsets = "+results.count());
 		
@@ -63,7 +66,45 @@ public class MigrationUpdateReadSetNbCycles extends DocumentController<ReadSet> 
 		
 		while(cursor.hasNext()){
 			ReadSet rs = cursor.next();
-			rulesActor.tell(new RulesMessage(Play.application().configuration().getString("rules.key"),"F_RG_1", rs),null);
+			Logger.debug("rs "+rs.code+" "+rs.runSequencingStartDate+" "+rs.laneNumber+" "+rs.runCode);
+			Logger.debug("rs trt "+rs.treatments.get("ngsrg"));
+			//rulesActor.tell(new RulesMessage(Play.application().configuration().getString("rules.key"),"F_RG_1", rs),null);
+			keys = new BasicDBObject();
+			keys.put("code", 1);
+			keys.put("lanes", 1);
+			Run run = MongoDBDAO.findOne(InstanceConstants.RUN_ILLUMINA_COLL_NAME, Run.class, DBQuery.is("code", rs.runCode), keys);
+			if(null != run){
+				Logger.debug("Load RUN "+run.code);
+				Treatment laneNGSRG = run.getLane(rs.laneNumber).treatments.get("ngsrg");
+				Treatment readSetNGSRG = rs.treatments.get("ngsrg");
+				Logger.debug("laneNGSRG "+laneNGSRG+" "+"readSetNGSRG "+readSetNGSRG);
+				if(laneNGSRG!=null && readSetNGSRG!=null){
+					boolean isUpdate = false;
+					Logger.debug("start copy");
+					if(null != laneNGSRG.results.get("default").get("nbUsefulCycleReadIndex2")){
+						readSetNGSRG.results.get("default").put("nbUsefulCycleReadIndex2", laneNGSRG.results.get("default").get("nbUsefulCycleReadIndex2"));
+						isUpdate = true;
+					}
+					if(null != laneNGSRG.results.get("default").get("nbUsefulCycleRead2")){
+						readSetNGSRG.results.get("default").put("nbUsefulCycleRead2", laneNGSRG.results.get("default").get("nbUsefulCycleRead2"));
+						isUpdate = true;
+					}
+					if(null != laneNGSRG.results.get("default").get("nbUsefulCycleRead1")){
+						readSetNGSRG.results.get("default").put("nbUsefulCycleRead1", laneNGSRG.results.get("default").get("nbUsefulCycleRead1"));
+						isUpdate = true;
+					}
+					if(null != laneNGSRG.results.get("default").get("nbUsefulCycleReadIndex1")){
+						readSetNGSRG.results.get("default").put("nbUsefulCycleReadIndex1", laneNGSRG.results.get("default").get("nbUsefulCycleReadIndex1"));
+					isUpdate = true;
+					}
+					Logger.debug("isUpdate "+isUpdate);
+					if(isUpdate){
+						MongoDBDAO.update(InstanceConstants.READSET_ILLUMINA_COLL_NAME, ReadSet.class, DBQuery.is("code", rs.code),  DBUpdate.set("treatments.ngsrg", readSetNGSRG));
+					}
+				}
+			}else{
+				Logger.error("ReadSet Does not exist: "+rs.runCode);
+			}
 		}
 		
 		return ok("Migration Finish");
