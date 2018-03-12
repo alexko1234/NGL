19446eedf8bfcf113f97cf3bd0603b3093e8be4c
Guillaume Albini <galbini@genoscope.cns.fr>
2018-03-12 16:33:49 +0100

    rollbakc on new getTemplateClass version

--- app-ngl-sq/app/controllers/experiments/tpl/Experiments.java
+++ app-ngl-sq/app/controllers/experiments/tpl/Experiments.java
@@ -129,7 +129,7 @@ public class Experiments extends NGLController
 	 * then if the class doesn't exist it will search for views.html.experiments.@atomicType.default.@outputCategoryCode
 	 * if all classes doesn't exist, a badRequest result is returned with the message "not implemented"
 	 * */
-	/*
+	
 	public Result getTemplate(String atomicType, String outputCategoryCode, String experimentCategoryCode, String experimentType){
 		
 		
@@ -167,8 +167,8 @@ public class Experiments extends NGLController
 		}
 		
 		return badRequest("Not implemented");
-	}*/
-
+	}
+/* ==> GA NOT WORK !!! See with sample-prep / CNG
 	public Result getTemplate(final String atomicType, String outputCategoryCode, final String experimentCategoryCode, final String experimentType){
 		if (ExperimentCategory.CODE.qualitycontrol.toString().equals(experimentCategoryCode))
 			outputCategoryCode = outputCategoryCode + "_" + experimentCategoryCode; // + "_qualitycontrol" ? 	
@@ -200,13 +200,13 @@ public class Experiments extends NGLController
 			return Optional.empty();
 		}		
 	}
-	
+*/	
 	/*This method return the HTML if the class exist, or NULL if not
 	 * First we search for the class, and if it exist, we load the render method
 	 * the template need to have an empty signature like @() because we call the method
 	 * without args
 	 * */
-	/*
+	
 	private Html getTemplateClass(String atomicType, String outputCategoryCode, String experimentType, String institute) {
 		
 		String keyWord = null;
@@ -229,7 +229,7 @@ public class Experiments extends NGLController
 				//TODO for template with @Inject need improvement
 				Class<?> clazz = Class.forName("views.html.experiments."+atomicType.toLowerCase()+"."+institute.toLowerCase()+"."+keyWord.toLowerCase());//package in java are always in lower case
 				// Object o = IGGlobals.injector().instanceOf(clazz);
-				Object o = ctx.injector().instanceOf(clazz);
+				Object o = getInjector().instanceOf(clazz);
 				Method m = clazz.getDeclaredMethod("render");
 				Html html = (Html)m.invoke(o);
 				return html;
@@ -238,83 +238,84 @@ public class Experiments extends NGLController
 			}
 		}
 	}
-	*/
+	
 	
 	public Result javascriptRoutes() {
   	    return jsRoutes(controllers.experiments.tpl.routes.javascript.Experiments.searchContainers(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.search(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.graph(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.listContainers(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.getTemplate(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.home(),  	    		
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.details(),
-  	    				controllers.experiments.tpl.routes.javascript.Experiments.get(),
-  	    				controllers.printing.tpl.routes.javascript.Printing.home(),
-
-  	    				controllers.instruments.api.routes.javascript.InstrumentUsedTypes.get(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.list(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.get(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.save(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.delete(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.update(),
-  	    				controllers.experiments.api.routes.javascript.Experiments.updateState(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentComments.save(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentComments.update(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentComments.delete(),
-
-  	    				controllers.experiments.api.routes.javascript.ExperimentTypeNodes.get(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentTypeNodes.list(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentReagents.list(),
-
-  	    				controllers.containers.api.routes.javascript.Containers.updateState(),
-  	    				controllers.containers.api.routes.javascript.Containers.updateStateBatch(),
-  	    				controllers.containers.api.routes.javascript.ContainerSupports.updateState(),
-  	    				controllers.containers.api.routes.javascript.ContainerSupports.updateStateBatch(),
-
-  	    				controllers.processes.api.routes.javascript.Processes.updateState(),
-  	    				controllers.processes.api.routes.javascript.Processes.updateStateBatch(),
-
-  	    				controllers.instruments.io.routes.javascript.IO.generateFile(),
-  	    				controllers.instruments.io.routes.javascript.IO.importFile(),
-
-  	    				controllers.containers.api.routes.javascript.Containers.list(),
-  	    				controllers.containers.api.routes.javascript.ContainerSupports.list(),
-  	    				controllers.containers.api.routes.javascript.Containers.get(),
-  	    				controllers.processes.api.routes.javascript.Processes.list(),
-  	    				controllers.processes.api.routes.javascript.ProcessTypes.list(),
-  	    				controllers.processes.api.routes.javascript.ProcessTypes.get(),
-  	    				controllers.processes.api.routes.javascript.ProcessCategories.list(),
-  	    				controllers.containers.api.routes.javascript.ContainerSupportCategories.list(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentTypes.list(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentTypes.get(),  	    		
-  	    				controllers.experiments.api.routes.javascript.ExperimentCategories.list(),
-  	    				controllers.instruments.api.routes.javascript.Instruments.list(),
-  	    				controllers.instruments.api.routes.javascript.InstrumentUsedTypes.list(),
-  	    				controllers.instruments.api.routes.javascript.InstrumentCategories.list(),
-  	    				controllers.protocols.api.routes.javascript.Protocols.list(),
-  	    				//instruments.io.routes.javascript.Outputs.generateFile(),
-  	    				//instruments.io.routes.javascript.Inputs.importFile(),
-  	    				controllers.resolutions.api.routes.javascript.Resolutions.list(),
-  	    				controllers.commons.api.routes.javascript.States.list(),
-  	    				controllers.reporting.api.routes.javascript.FilteringConfigurations.list(),
-  	    				controllers.reporting.api.routes.javascript.ReportingConfigurations.list(),
-  	    				controllers.reporting.api.routes.javascript.ReportingConfigurations.get(),
-  	    				controllers.reagents.api.routes.javascript.KitCatalogs.list(),
-  	    				controllers.reagents.api.routes.javascript.BoxCatalogs.list(),
-  	    				controllers.reagents.api.routes.javascript.ReagentCatalogs.list(),
-  	    				controllers.reagents.api.routes.javascript.Kits.list(),
-  	    				controllers.reagents.api.routes.javascript.Boxes.list(),
-  	    				controllers.reagents.api.routes.javascript.Reagents.list(),
-  	    				controllers.commons.api.routes.javascript.Values.list(),
-  	    				controllers.commons.api.routes.javascript.CommonInfoTypes.list(),
-  	    				controllers.experiments.api.routes.javascript.ExperimentCategories.list(),
-  	    				controllers.projects.api.routes.javascript.Projects.list(),
-  	    				controllers.samples.api.routes.javascript.Samples.list(),
-  	    				controllers.commons.api.routes.javascript.Users.list(),  	    
-  	    				controllers.commons.api.routes.javascript.Parameters.list(),
-  	    				controllers.valuation.api.routes.javascript.ValuationCriterias.get(),
-  	    				controllers.valuation.api.routes.javascript.ValuationCriterias.list(),
-  	    				controllers.runs.api.routes.javascript.Runs.list());
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.search(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.graph(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.listContainers(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.getTemplate(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.home(),  	    		
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.details(),
+  	    		controllers.experiments.tpl.routes.javascript.Experiments.get(),
+  	    		controllers.printing.tpl.routes.javascript.Printing.home(),
+  	    		
+  	    		controllers.instruments.api.routes.javascript.InstrumentUsedTypes.get(),
+	      		controllers.experiments.api.routes.javascript.Experiments.list(),
+	      		controllers.experiments.api.routes.javascript.Experiments.get(),
+	      		controllers.experiments.api.routes.javascript.Experiments.save(),
+	      		controllers.experiments.api.routes.javascript.Experiments.delete(),
+	      		controllers.experiments.api.routes.javascript.Experiments.update(),
+	      		controllers.experiments.api.routes.javascript.Experiments.updateState(),
+	      		controllers.experiments.api.routes.javascript.ExperimentComments.save(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentComments.update(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentComments.delete(),
+  	    		
+  	    		controllers.experiments.api.routes.javascript.ExperimentTypeNodes.get(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentTypeNodes.list(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentReagents.list(),
+  	    		
+  	    		controllers.containers.api.routes.javascript.Containers.updateState(),
+  	    		controllers.containers.api.routes.javascript.Containers.updateStateBatch(),
+  	    		controllers.containers.api.routes.javascript.ContainerSupports.updateState(),
+  	    		controllers.containers.api.routes.javascript.ContainerSupports.updateStateBatch(),
+  	    		
+  	    		controllers.processes.api.routes.javascript.Processes.updateState(),
+  	    		controllers.processes.api.routes.javascript.Processes.updateStateBatch(),
+  	    		
+  	    		controllers.instruments.io.routes.javascript.IO.generateFile(),
+  	    		controllers.instruments.io.routes.javascript.IO.importFile(),
+  	    		
+  	    		controllers.containers.api.routes.javascript.Containers.list(),
+  	    		controllers.containers.api.routes.javascript.ContainerSupports.list(),
+  	    		controllers.containers.api.routes.javascript.ContainerSupports.get(), // ajout FDS 16/02/2018
+  	    		controllers.containers.api.routes.javascript.Containers.get(),
+  	    		controllers.processes.api.routes.javascript.Processes.list(),
+  	    		controllers.processes.api.routes.javascript.ProcessTypes.list(),
+  	    		controllers.processes.api.routes.javascript.ProcessTypes.get(),
+  	    		controllers.processes.api.routes.javascript.ProcessCategories.list(),
+  	    		controllers.containers.api.routes.javascript.ContainerSupportCategories.list(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentTypes.list(),
+  	    		controllers.experiments.api.routes.javascript.ExperimentTypes.get(),  	    		
+  	    		controllers.experiments.api.routes.javascript.ExperimentCategories.list(),
+  	    		controllers.instruments.api.routes.javascript.Instruments.list(),
+  	    		controllers.instruments.api.routes.javascript.InstrumentUsedTypes.list(),
+  	    		controllers.instruments.api.routes.javascript.InstrumentCategories.list(),
+  	    		controllers.protocols.api.routes.javascript.Protocols.list(),
+  	    		//instruments.io.routes.javascript.Outputs.generateFile(),
+  	    		//instruments.io.routes.javascript.Inputs.importFile(),
+  	    		controllers.resolutions.api.routes.javascript.Resolutions.list(),
+  	    		controllers.commons.api.routes.javascript.States.list(),
+  	      		controllers.reporting.api.routes.javascript.FilteringConfigurations.list(),
+  	      		controllers.reporting.api.routes.javascript.ReportingConfigurations.list(),
+	      		controllers.reporting.api.routes.javascript.ReportingConfigurations.get(),
+	      		controllers.reagents.api.routes.javascript.KitCatalogs.list(),
+	      		controllers.reagents.api.routes.javascript.BoxCatalogs.list(),
+	      		controllers.reagents.api.routes.javascript.ReagentCatalogs.list(),
+  	      		controllers.reagents.api.routes.javascript.Kits.list(),
+  	      		controllers.reagents.api.routes.javascript.Boxes.list(),
+  	      		controllers.reagents.api.routes.javascript.Reagents.list(),
+  	      		controllers.commons.api.routes.javascript.Values.list(),
+				controllers.commons.api.routes.javascript.CommonInfoTypes.list(),
+				controllers.experiments.api.routes.javascript.ExperimentCategories.list(),
+				controllers.projects.api.routes.javascript.Projects.list(),
+  	    		controllers.samples.api.routes.javascript.Samples.list(),
+  	    		controllers.commons.api.routes.javascript.Users.list(),  	    
+	      		controllers.commons.api.routes.javascript.Parameters.list(),
+  	    		controllers.valuation.api.routes.javascript.ValuationCriterias.get(),
+  	    		controllers.valuation.api.routes.javascript.ValuationCriterias.list(),
+  	    		controllers.runs.api.routes.javascript.Runs.list());
   	}
 
 	/*
