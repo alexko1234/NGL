5b741736852d14cf99fdc39ac3f937e8496edec4
Guillaume Albini <galbini@genoscope.cns.fr>
2018-03-12 16:34:09 +0100

    add missing tpl routes

--- app-ngl-sq/app/controllers/samples/tpl/Samples.java
+++ app-ngl-sq/app/controllers/samples/tpl/Samples.java
@@ -69,40 +69,41 @@ public class Samples extends NGLController implements NGLJavascript  { // NGLBas
 	// tpl
 	public Result javascriptRoutes() {
 		return jsRoutes(controllers.processes.api.routes.javascript.ProcessTypes.list(),
-						controllers.containers.api.routes.javascript.Containers.list(),
-						controllers.processes.api.routes.javascript.Processes.list(),
-						controllers.processes.api.routes.javascript.ProcessCategories.list(),
-						controllers.commons.api.routes.javascript.CommonInfoTypes.list(),
-						controllers.commons.api.routes.javascript.Values.list(),
-						controllers.projects.api.routes.javascript.Projects.list(),
+				controllers.containers.api.routes.javascript.Containers.list(),
+				controllers.processes.api.routes.javascript.Processes.list(),
+				controllers.processes.api.routes.javascript.ProcessCategories.list(),
+				controllers.commons.api.routes.javascript.CommonInfoTypes.list(),
+				controllers.commons.api.routes.javascript.Values.list(),
+				controllers.projects.api.routes.javascript.Projects.list(),
 
-						controllers.samples.api.routes.javascript.Samples.list(),
-						controllers.samples.api.routes.javascript.Samples.get(),
-						controllers.samples.api.routes.javascript.Samples.update(),
+				controllers.samples.api.routes.javascript.Samples.list(),
+				controllers.samples.api.routes.javascript.Samples.get(),
+				controllers.samples.api.routes.javascript.Samples.update(),
 
-						controllers.samples.tpl.routes.javascript.Samples.get(),
-						controllers.samples.tpl.routes.javascript.Samples.search(),
-						controllers.samples.tpl.routes.javascript.Samples.details(),
-						controllers.samples.tpl.routes.javascript.Samples.home(),
+				controllers.samples.tpl.routes.javascript.Samples.get(),
+				controllers.samples.tpl.routes.javascript.Samples.search(),
+				controllers.samples.tpl.routes.javascript.Samples.details(),
+				controllers.samples.tpl.routes.javascript.Samples.home(),
 
-						controllers.containers.api.routes.javascript.Containers.get(),
 
-						controllers.experiments.api.routes.javascript.Experiments.list(),
-						controllers.commons.api.routes.javascript.States.list(),
-						controllers.commons.api.routes.javascript.Users.list(),
-						controllers.reporting.api.routes.javascript.FilteringConfigurations.list(),
-						controllers.containers.api.routes.javascript.ContainerSupportCategories.list(),
-						controllers.experiments.api.routes.javascript.ExperimentTypes.list(),
-						controllers.reporting.api.routes.javascript.ReportingConfigurations.list(),
-						controllers.reporting.api.routes.javascript.ReportingConfigurations.get(),			      		
-						controllers.commons.api.routes.javascript.Values.list(),
-						controllers.commons.api.routes.javascript.Parameters.list(),
-						controllers.resolutions.api.routes.javascript.Resolutions.list(),
-						controllers.commons.api.routes.javascript.PropertyDefinitions.list(),
-						controllers.processes.tpl.routes.javascript.Processes.home(),
-						controllers.experiments.tpl.routes.javascript.Experiments.get(),
+				controllers.containers.api.routes.javascript.Containers.get(),
 
-						controllers.protocols.api.routes.javascript.Protocols.list());
+				controllers.experiments.api.routes.javascript.Experiments.list(),
+				controllers.commons.api.routes.javascript.States.list(),
+				controllers.commons.api.routes.javascript.Users.list(),
+				controllers.reporting.api.routes.javascript.FilteringConfigurations.list(),
+				controllers.containers.api.routes.javascript.ContainerSupportCategories.list(),
+				controllers.experiments.api.routes.javascript.ExperimentTypes.list(),
+				controllers.reporting.api.routes.javascript.ReportingConfigurations.list(),
+				controllers.reporting.api.routes.javascript.ReportingConfigurations.get(),			      		
+				controllers.commons.api.routes.javascript.Values.list(),
+				controllers.commons.api.routes.javascript.Parameters.list(),
+				controllers.resolutions.api.routes.javascript.Resolutions.list(),
+				controllers.commons.api.routes.javascript.PropertyDefinitions.list(),
+				controllers.processes.tpl.routes.javascript.Processes.home(),
+				controllers.experiments.tpl.routes.javascript.Experiments.get(),
+
+				controllers.protocols.api.routes.javascript.Protocols.list());
 	}
 
 	/*
