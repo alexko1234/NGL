1195b1b4274fb17121700003adb5600807dde3ce
Guillaume Albini <galbini@genoscope.cns.fr>
2018-03-14 11:34:18 +0100

    change auth version

--- project/Build.scala
+++ project/Build.scala
@@ -113,7 +113,7 @@ object ApplicationBuild extends Build {
 
 	// IG libraries
 
-  val ceaAuth     = "fr.cea.ig.modules"   %% "authentication"     % "2.6-2.0.7"
+  val ceaAuth     = "fr.cea.ig.modules"   %% "authentication"     % "2.0.11-SNAPSHOT"
 	val ceaSpring   = "fr.cea.ig"           %% "play-spring-module" % "2.6-1.4.2-SNAPSHOT"
 	val ceaMongo    = "fr.cea.ig"           %% "mongodbplugin"      % "2.6-1.7.4-SNAPSHOT"
   // External libraries versions
