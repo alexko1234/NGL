2eb658772f6c19031d30a11c5d70910813b6d107
Adrien Josso <ajosso@genoscope.cns.fr>
2018-03-20 15:08:31 +0100

    use TestAppAuthFactory to start unit tests

--- app-ngl-bi/test/ngl/bi/Global.java
+++ app-ngl-bi/test/ngl/bi/Global.java
@@ -2,13 +2,15 @@ package ngl.bi;
 
 import fr.cea.ig.ngl.test.TestAppAuthFactory;
 import play.Application;
+import rules.services.Rules6Component;
 
 public class Global {
 	
 	public static final TestAppAuthFactory af = new TestAppAuthFactory("ngl-bi.test.conf"); 
 	
 	public static Application devapp() { 
-		return fr.cea.ig.play.test.DevAppTesting.devapp("ngl-bi.test.conf");
+//		return fr.cea.ig.play.test.DevAppTesting.devapp("ngl-bi.test.conf");
+		return af.createApplication();
 	}
 
 }
