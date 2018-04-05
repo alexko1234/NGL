7d7074bdb4a04b0c6b06b5cf2b6f650ce64dcddb
gsamson <gsamson@genoscope.cns.fr>
2018-04-05 17:32:00 +0200

    ajout spec FDR MiSeq pour URGV

--- app-ngl-sq/app/controllers/instruments/io/common/miseq/MiSeqOutput.java
+++ app-ngl-sq/app/controllers/instruments/io/common/miseq/MiSeqOutput.java
@@ -19,27 +19,23 @@ public class MiSeqOutput extends AbstractOutput {
 	@Override
 	public File generateFile(Experiment experiment, ContextValidation contextValidation) {
 		List<Container> containers = OutputHelper.getInputContainersFromExperiment(experiment);
-		System.out.println("testtttt ok");
-		
+	
 		TagModel tagModel = OutputHelper.getTagModel(containers);
 		String content = null;
 
-	/*if ((Boolean)experiment.instrumentProperties.get("customPrimers").value){
-			System.out.println("test ok");
+		if ((Boolean)experiment.instrumentProperties.get("customPrimers").value){
 			if(!"DUAL-INDEX".equals(tagModel.tagType)){
 				content = OutputHelper.format(sampleSheet_1_customPrimers.render(experiment,containers).body());			
 			}else{
 				content = OutputHelper.format(sampleSheet_2_customPrimers.render(experiment,containers).body());	
 			}
-		}else{*/
-		System.out.println("test ok");
-		
+		}else{
 			if(!"DUAL-INDEX".equals(tagModel.tagType)){
 				content = OutputHelper.format(sampleSheet_1.render(experiment,containers).body());			
 			}else{
 				content = OutputHelper.format(sampleSheet_2.render(experiment,containers).body());	
 			}
-	//	}
+		}
 		String filename = OutputHelper.getInstrumentPath(experiment.instrument.code)+experiment.instrumentProperties.get("miseqReagentCassette").value+".csv";
 		File file = new File(filename, content);
 
