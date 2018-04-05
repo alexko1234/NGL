eaedd2f83658a3daeee61fb5ad6348a56de46e30
gsamson <gsamson@genoscope.cns.fr>
2018-04-05 16:44:57 +0200

    tmp

--- app-ngl-sq/app/controllers/instruments/io/common/miseq/MiSeqOutput.java
+++ app-ngl-sq/app/controllers/instruments/io/common/miseq/MiSeqOutput.java
@@ -4,6 +4,7 @@ package controllers.instruments.io.common.miseq;
 
 import java.util.List;
 
+
 import models.laboratory.container.instance.Container;
 import models.laboratory.experiment.instance.Experiment;
 import validation.ContextValidation;
@@ -18,18 +19,31 @@ public class MiSeqOutput extends AbstractOutput {
 	@Override
 	public File generateFile(Experiment experiment, ContextValidation contextValidation) {
 		List<Container> containers = OutputHelper.getInputContainersFromExperiment(experiment);
+		System.out.println("testtttt ok");
+		
 		TagModel tagModel = OutputHelper.getTagModel(containers);
 		String content = null;
-		if(!"DUAL-INDEX".equals(tagModel.tagType)){
-			content = OutputHelper.format(sampleSheet_1.render(experiment,containers).body());			
-		}else{
-			content = OutputHelper.format(sampleSheet_2.render(experiment,containers).body());	
-		}
+
+	/*if ((Boolean)experiment.instrumentProperties.get("customPrimers").value){
+			System.out.println("test ok");
+			if(!"DUAL-INDEX".equals(tagModel.tagType)){
+				content = OutputHelper.format(sampleSheet_1_customPrimers.render(experiment,containers).body());			
+			}else{
+				content = OutputHelper.format(sampleSheet_2_customPrimers.render(experiment,containers).body());	
+			}
+		}else{*/
+		System.out.println("test ok");
+		
+			if(!"DUAL-INDEX".equals(tagModel.tagType)){
+				content = OutputHelper.format(sampleSheet_1.render(experiment,containers).body());			
+			}else{
+				content = OutputHelper.format(sampleSheet_2.render(experiment,containers).body());	
+			}
+	//	}
 		String filename = OutputHelper.getInstrumentPath(experiment.instrument.code)+experiment.instrumentProperties.get("miseqReagentCassette").value+".csv";
 		File file = new File(filename, content);
-		
+
 		OutputHelper.writeFile(file);
 		return file;
 	}
-
 }
