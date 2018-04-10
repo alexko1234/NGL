4d3d8db50b61cf36726a0e7ea96a97d957c882bb
Adrien Josso <ajosso@genoscope.cns.fr>
2018-04-10 10:15:24 +0200

    minimal refactoring

--- lib-ngl-common/app/controllers/containers/api/ContainerCategories.java
+++ lib-ngl-common/app/controllers/containers/api/ContainerCategories.java
@@ -1,60 +1,67 @@
 package controllers.containers.api;
 
-// import static play.data.Form.form;
-//import static fr.cea.ig.play.IGGlobals.form;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import controllers.APICommonController;
-//import controllers.CommonController;
-import controllers.authorisation.Permission;
-import fr.cea.ig.play.migration.NGLContext;
+import fr.cea.ig.authentication.Authenticated;
+import fr.cea.ig.authorization.Authorized;
+import fr.cea.ig.lfw.Historized;
+import fr.cea.ig.mongo.DBObjectConvertor;
+import fr.cea.ig.ngl.NGLApplication;
+import fr.cea.ig.ngl.NGLController;
+import fr.cea.ig.ngl.support.NGLForms;
 import models.laboratory.container.description.ContainerCategory;
 import models.utils.ListObject;
 import models.utils.dao.DAOException;
 import play.Logger;
 import play.data.Form;
-import play.libs.Json;
 import play.mvc.Result;
 import play.mvc.Results;
 import views.components.datatable.DatatableResponse;
 
 // Could provide implementation through CRUD base class.
-public class ContainerCategories extends APICommonController<ContainerCategoriesSearchForm> { //CommonController{
-	
-	private /*static*/ final Form<ContainerCategoriesSearchForm> containerCategoriesTypeForm; // = form(ContainerCategoriesSearchForm.class);
-	
+@Historized
+public class ContainerCategories extends NGLController implements NGLForms, DBObjectConvertor {
+
+	private final Form<ContainerCategoriesSearchForm> containerCategoriesTypeForm; 
+
 	@Inject
-	public ContainerCategories(NGLContext ctx) {
-		super(ctx, ContainerCategoriesSearchForm.class);
-		this.containerCategoriesTypeForm = ctx.form(ContainerCategoriesSearchForm.class);
+	public ContainerCategories(NGLApplication app) {
+		super(app);
+		this.containerCategoriesTypeForm = app.formFactory().form(ContainerCategoriesSearchForm.class);
 	}
-		
-	@Permission(value={"reading"})
-	public Result list() throws DAOException {
-		Form<ContainerCategoriesSearchForm>  containerCategoryFilledForm = filledFormQueryString(containerCategoriesTypeForm,ContainerCategoriesSearchForm.class);
-		ContainerCategoriesSearchForm containerCategoriesSearch = containerCategoryFilledForm.get();
+
+	@Authenticated
+	@Authorized.Read
+	public Result list() {
 		try {
-			List<ContainerCategory> containerCategories = ContainerCategory.find.findAll();
-			
-			if (containerCategoriesSearch.datatable) {
-				return ok(Json.toJson(new DatatableResponse<>(containerCategories, containerCategories.size()))); 
-			} else if(containerCategoriesSearch.list) {
-				List<ListObject> lop = new ArrayList<>();
-				for(ContainerCategory et:containerCategories){
-					lop.add(new ListObject(et.code, et.name));
+			Form<ContainerCategoriesSearchForm>  containerCategoryFilledForm = filledFormQueryString(containerCategoriesTypeForm, ContainerCategoriesSearchForm.class);
+			ContainerCategoriesSearchForm containerCategoriesSearch = containerCategoryFilledForm.get();
+			try {
+				List<ContainerCategory> containerCategories = ContainerCategory.find.findAll();
+
+				if (containerCategoriesSearch.datatable) {
+					return okAsJson(new DatatableResponse<ContainerCategory>(containerCategories, containerCategories.size())); 
+				} else if(containerCategoriesSearch.list) {
+					List<ListObject> lo = new ArrayList<ListObject>();
+					for(ContainerCategory cc : containerCategories){
+						lo.add(new ListObject(cc.code, cc.name));
+					}
+					return okAsJson(lo);
+				} else {
+					return okAsJson(containerCategories);
 				}
-				return Results.ok(Json.toJson(lop));
-			} else {
-				return Results.ok(Json.toJson(containerCategories));
+			} catch (DAOException e) {
+				Logger.error("DAO error: " + e.getMessage(),e);
+				return  Results.internalServerError(e.getMessage());
 			}
-		} catch (DAOException e) {
-			Logger.error("DAO error: " + e.getMessage(),e);
-			return  Results.internalServerError(e.getMessage());
-		}	
+		} catch (Exception e) {
+			getLogger().error(e.getMessage());
+			return nglGlobalBadRequest();
+		}
 	}
-	
+
 }
