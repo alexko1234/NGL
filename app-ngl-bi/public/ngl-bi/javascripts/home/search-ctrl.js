f41e35988293972b40ec32052ab36d6f0de2c22d
ejacoby <ejacoby@genoscope.cns.fr>
2018-04-12 15:58:43 +0200

    NGL-1943 optimisation requete promise par année On branch master-isoprod-bi-2.2.X

--- app-ngl-bi/public/ngl-bi/javascripts/home/search-ctrl.js
+++ app-ngl-bi/public/ngl-bi/javascripts/home/search-ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('home').controller('SearchCtrl', ['$scope', '$http','$httpParamSerializer', 'datatable' , function($scope, $http,$httpParamSerializer,datatable) {
+angular.module('home').controller('SearchCtrl', ['$scope', '$http','$httpParamSerializer', '$q','datatable' , function($scope, $http,$httpParamSerializer,$q,datatable) {
 
 	var datatableConfig = {
 			order :{by:'sequencingStartDate', reverse:true, mode:'remote'},
@@ -63,19 +63,45 @@ angular.module('home').controller('SearchCtrl', ['$scope', '$http','$httpParamSe
 		
 		
 		//Init list runCodes
+		var datatableConfigRunsNoValid = datatableConfig;
+		datatableConfigRunsNoValid.order={by:'sequencingStartDate', reverse:false, mode:'remote'},
+		$scope.runsNoValid = datatable(datatableConfigRunsNoValid);		
+		$scope.runsNoValid.config.spinner.start=true;
+		
+		var queries = [];
 		var form = {includes : []};
 		form.includes.push("runCode");
 		form.productionValidCode="UNSET";
 		form.typeCode="rsillumina";
 		//filter for CNG
 		form.fromDate=moment("14/12/2013", Messages("date.format").toUpperCase()).valueOf();
-		var datatableConfigRunsNoValid = datatableConfig;
-		datatableConfigRunsNoValid.order={by:'sequencingStartDate', reverse:false, mode:'remote'},
-		$scope.runsNoValid = datatable(datatableConfigRunsNoValid);		
-		$scope.runsNoValid.config.spinner.start=true;
+		form.toDate=moment("31/12/2013", Messages("date.format").toUpperCase()).valueOf();
+		queries.push( $http.get(jsRoutes.controllers.readsets.api.ReadSets.list().url, {params : form}) );
 		
+		var actualYear = new Date().getFullYear();
+		for (var i = 2014; i <= actualYear; i++){
+			var form = {includes : []};
+			form.includes.push("runCode");
+			form.productionValidCode="UNSET";
+			form.typeCode="rsillumina";
+			form.fromDate=moment("01/01/"+i, Messages("date.format").toUpperCase()).valueOf();
+			form.toDate=moment("31/12/"+i, Messages("date.format").toUpperCase()).valueOf();
+			queries.push( $http.get(jsRoutes.controllers.readsets.api.ReadSets.list().url, {params : form}) );
+		}
 		
-		$http.get(jsRoutes.controllers.readsets.api.ReadSets.list().url,{params:form}).then(function(result){
+		 $q.all(queries).then(function(results) {
+			 var allData = [];
+				results.forEach(function(result){
+					//$scope.runCodes.concat(result.data.map(function(readSet){return readSet.runCode;}));
+					allData = allData.concat(result.data);
+				});
+				$scope.runCodes = allData.map(function(readSet){return readSet.runCode;}).filter((value, index, self) => self.indexOf(value) === index);
+				$scope.runsNoValid.config.spinner.start=false;
+				$scope.runsNoValid.search({codes:$scope.runCodes});
+				$scope.runCodesUrl = $httpParamSerializer({codes:$scope.runCodes});
+		 });
+		
+		/*$http.get(jsRoutes.controllers.readsets.api.ReadSets.list().url,{params:form}).then(function(result){
 			$scope.runCodes =result.data.map(function(readSet){return readSet.runCode;}).filter((value, index, self) => self.indexOf(value) === index);
 			//var datatableConfigRunsNoValid = datatableConfig;
 			//datatableConfigRunsNoValid.order={by:'sequencingStartDate', reverse:false, mode:'remote'},
@@ -83,7 +109,7 @@ angular.module('home').controller('SearchCtrl', ['$scope', '$http','$httpParamSe
 			$scope.runsNoValid.config.spinner.start=false;
 			$scope.runsNoValid.search({codes:$scope.runCodes});
 			$scope.runCodesUrl = $httpParamSerializer({codes:$scope.runCodes});
-		});
+		});*/
 	}
 		
 }]);
