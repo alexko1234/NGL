42392abdd286dc4f821123034de0187c150ad45e
Guillaume Albini <galbini@genoscope.cns.fr>
2018-05-16 11:22:44 +0200

    solve error on build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -78,7 +78,8 @@ object ApplicationBuild extends Build {
 	val buildVersion           = "2.1"    + distSuffix
 	val nglVersion             = "2.0"    + distSuffix
 	
-	val sqVersion              = "2.2.2" + distSuffix	val biVersion              = "2.3.0" + distSuffix
+	val sqVersion              = "2.2.2" + distSuffix	
+	val biVersion              = "2.3.0" + distSuffix
 
 	val projectsVersion        = "2.4.0"  + distSuffix
 	val reagentsVersion        = "2.2.0"  + distSuffix
